6661df238bbbd2fc84f5de38e8265290a1e81506
Rostislav Masniuk <rostislav.masnyuk.98@gmail.com>
2019-06-19 15:56:49 +0300

    solution

---
+++ list-of-posts/build/precache-manifest.d48c7cbf34b5c4e4faf46c5502e00b00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ad859d876a82842c863eee5ed1703c",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "4b5d1642225e513b4c0d",
+    "url": "/react_dynamic-list-of-posts/static/css/main.deaf6b2d.chunk.css"
+  },
+  {
+    "revision": "9ab39bf724d219c5220e",
+    "url": "/react_dynamic-list-of-posts/static/js/2.4ae0c7af.chunk.js"
+  },
+  {
+    "revision": "4b5d1642225e513b4c0d",
+    "url": "/react_dynamic-list-of-posts/static/js/main.d3954628.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
