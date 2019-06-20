84b67241d9af65fc9f103990c473ebbdecff97d0
Batsenko <48134243+Batsenko@users.noreply.github.com>
2019-06-20 07:26:37 +0300

    Update CommentList.js

--- src/CommentList.js
+++ src/CommentList.js
@@ -2,12 +2,9 @@ import React from 'react';
 import Comment from "./Comment.js";
 
 function CommentList(props) {
-  return props.comments.map((value) => (
-    <Comment key={value.id} comment={value}/>
-    ));
-  return <Comment key={value.id} comment={value}/>
+  return props.comments.map((value) => {
+    return <Comment key={value.id} comment={value}/>;
   });
 }
 
 export default CommentList;
-                            
