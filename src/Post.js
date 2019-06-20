4f07dd4464ce06420a58fa1d7994d2d8b1037746
Batsenko <48134243+Batsenko@users.noreply.github.com>
2019-06-20 07:32:02 +0300

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -5,17 +5,15 @@ import "./post.css";
 
 function Post(props) {
   return (
-  <div className="post">
-    <h1>
-    {props.post.title}
-    </h1>
-  
-  <p> {props.post.body} </p>
-  <User user={props.user} />
-  
-  <p className="commentsText"> COMMENTS: </p>
-  <CommentList comments={props.comments} />
-  </div>
+    <div className="post">
+      <h1>
+      {props.post.title}
+      </h1>
+      <p> {props.post.body} </p>
+      <User user={props.user} />
+      <p className="commentsText"> COMMENTS: </p>
+      <CommentList comments={props.comments} />
+    </div>
   );
 }
 
