f4fe3d9c70c904ad3bc36f96c3251e2214be3f17
Batsenko <48134243+Batsenko@users.noreply.github.com>
2019-06-20 07:33:11 +0300

    Update User.js

--- src/User.js
+++ src/User.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function User(props) {
   return (
-  <div className="user">
-    <p> {props.user.name} </p>
-  <p> {props.user.email} </p>
-  <p> {`${props.user.address.city} ${props.user.address.street}`}</p>
-  </div>
+    <div className="user">
+      <p> {props.user.name} </p>
+      <p> {props.user.email} </p>
+      <p> {`${props.user.address.city} ${props.user.address.street}`}</p>
+    </div>
   );
 }
 
