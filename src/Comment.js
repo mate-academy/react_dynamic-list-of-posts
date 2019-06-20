3f88c52ba1adf6a38cb448ba34fe3b78c953739a
Batsenko <48134243+Batsenko@users.noreply.github.com>
2019-06-20 07:04:02 +0300

    Update src/Comment.js
    Co-Authored-By: Остап П'ятковський <44294945+gestap@users.noreply.github.com>

--- src/Comment.js
+++ src/Comment.js
@@ -4,7 +4,7 @@ function Comment(props) {
   return (
     <div className="comment">
     <h3> {props.comment.name} </h3>
-  <p> {props.comment.email} </p>
+    <p> {props.comment.email} </p>
     <p> {props.comment.body} </p>
   </div>
   );
