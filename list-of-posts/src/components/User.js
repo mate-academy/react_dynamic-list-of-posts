52c46fda485b3ce16a83e0862b80cd663540c1d2
AlexHladun <gefest.gl@gmail.com>
2019-06-27 19:35:51 +0300

    Fixed User.js

--- list-of-posts/src/components/User.js
+++ list-of-posts/src/components/User.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class User extends Component {
-  render() {
-    return (
-      <div className="author-email">
-        <span> - <strong>{this.props.user.name}</strong></span>
-        <span> {this.props.user.email}</span>
+export default function User(props) {
+    return <div className="author-email">
+        <span> - <strong>{props.user.name}</strong></span>
+        <span> {props.user.email}</span>
       </div>
-    )
-  }
 
 }
