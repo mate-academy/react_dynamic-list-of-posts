cb88401dfdb6ce2c16b74fe5d014f5798ffa91a0
vpolets <vpolets@gmail.com>
2019-07-02 13:20:03 +0300

    add task

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['airbnb', '@mate-academy/eslint-config'],
+  env: {
+    commonjs: true,
+    node: true,
+    es6: true,
+    browser: true
+  },
+  parserOptions: {
+    sourceType: "module"
+  },
+  "globals": {
+    it: false
+  },
+  rules: {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    'no-console': 'off',
+    "no-param-reassign": 0,
+
+    "no-shadow": ["error", { "builtinGlobals": false }],
+    "react/destructuring-assignment": 0,
+  }
+};
