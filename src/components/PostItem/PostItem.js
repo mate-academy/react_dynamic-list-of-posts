89017426edb4bd8d549b3eda5d04cb0b7f3ca599
alex <earthhuman41@gmail.com>
2019-07-08 17:59:09 +0300

    solution

--- src/components/PostItem/PostItem.js
+++ src/components/PostItem/PostItem.js
@@ -5,25 +5,28 @@ import './PostItem.css';
 import CommentList from '../CommentsList/CommentsList';
 import User from '../User/User';
 
-const PostItem = ({ postItem, postAuthor, postComments }) => (
+const PostItem = ({ post }) => (
   <div className="post__container">
+    {console.log(post.postItem)}
     <article className="post">
-      <h2 className="post__title">{postItem.title}</h2>
-      <p>{postItem.body}</p>
+      <h2 className="post__title">{post.postItem.title}</h2>
+      <p>{post.postItem.body}</p>
 
-      <User user={postAuthor} />
+      <User user={post.postAuthor} />
 
     </article>
-    <CommentList comments={postComments} />
+    <CommentList comments={post.postComments} />
   </div>
 );
 
 PostItem.propTypes = {
-  postItem: PropTypes.shape({
-    title: PropTypes.string,
-    body: PropTypes.string,
+  post: PropTypes.shape({
+    postItem: PropTypes.shape({
+      title: PropTypes.string,
+      body: PropTypes.string,
+    }),
+    postAuthor: PropTypes.object,
+    postComments: PropTypes.object,
   }).isRequired,
-  postAuthor: PropTypes.object.isRequired,
-  postComments: PropTypes.object.isRequired,
 };
 export default PostItem;
