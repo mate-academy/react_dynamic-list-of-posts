20a92841c59d711c0515f5bcae248a162c905a11
Пользователь <Sviatoslv>
2019-07-15 22:12:48 +0300

    API to separate file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
 import React from 'react';
 import './App.css';
 import PostList from './components/PostList';
-
-const getPosts = async() => {
-  const url = 'https://jsonplaceholder.typicode.com';
-
-  const postsFetch = await fetch(`${url}/posts`);
-  const posts = await postsFetch.json();
-
-  const usersFetch = await fetch(`${url}/users`);
-  const users = await usersFetch.json();
-
-  const commentsFetch = await fetch(`${url}/comments`);
-  const comments = await commentsFetch.json();
-
-  const postWithUsersAndComments = posts.map(post => (
-    {
-      ...post,
-      user: users.find(user => user.id === post.userId),
-      postComments: comments.filter(comment => comment.postId === post.id),
-    }
-  ));
-
-  return postWithUsersAndComments;
-};
+import getPosts from './API_DATA';
 
 class App extends React.Component {
   state={
