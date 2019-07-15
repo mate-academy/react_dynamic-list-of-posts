5b458f3e79999ac7553d1cd70433faf7088d7e9d
Serhii <davydko.serhiy@gmail.com>
2019-07-15 19:22:51 +0300

    add comment hidder

--- src/CommentList.js
+++ src/CommentList.js
@@ -2,13 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Comment from './Comment';
 
-const CommentList = ({ comments }) => (
-  <div className="CommentList__items">
-    {comments.map(comment => (
-      <Comment comment={comment} />
-    ))}
-  </div>
-);
+class CommentList extends React.Component {
+  state = {
+    isHidden: true,
+  }
+
+  showComments = () => {
+    this.setState(prevState => ({
+      isHidden: !prevState.isHidden,
+    }));
+  };
+
+  render() {
+    return (
+      <div className="CommentList__items">
+        <button type="button" onClick={this.showComments}>
+          {this.state.isHidden ? 'Show Comment' : 'Hide comments'}
+        </button>
+        {this.state.isHidden
+          ? <span> </span>
+          : this.props.comments.map(comment => (
+            <Comment comment={comment} />
+          ))}
+      </div>
+    );
+  }
+}
 
 CommentList.propTypes = {
   comments: PropTypes.shape({
