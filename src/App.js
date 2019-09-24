576cf68b8f9dfa4010f63db31d2be9ac579cbaaa
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-09-24 21:30:13 +0300

    improve

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import PostList from './components/PostList/PostList';
 
-const postsUrl = 'https://jsonplaceholder.typicode.com/posts';
-const usersUrl = 'https://jsonplaceholder.typicode.com/users';
-const commentsUrl = 'https://jsonplaceholder.typicode.com/comments';
+const BASE_URL = `https://jsonplaceholder.typicode.com`;
 
 export default class App extends Component {
   state = {
@@ -14,6 +12,7 @@ export default class App extends Component {
     posts: [],
     users: [],
     comments: [],
+    errorMessage: null,
   };
 
   async onLoadClick() {
@@ -21,9 +20,9 @@ export default class App extends Component {
 
     try {
       const [fetchPosts, fetchUsers, fecthComments] = await Promise.all([
-        fetch(postsUrl),
-        fetch(usersUrl),
-        fetch(commentsUrl),
+        fetch(`${BASE_URL}/posts`),
+        fetch(`${BASE_URL}/users`),
+        fetch(`${BASE_URL}/comments`),
       ]);
 
       const posts = await fetchPosts.json();
@@ -34,8 +33,9 @@ export default class App extends Component {
         posts, users, comments, isLoaded: true,
       });
     } catch (error) {
-      // eslint-disable-next-line no-console
-      console.warn('error');
+      this.setState({
+        errorMessage: error.message,
+      });
     }
   }
 
@@ -63,12 +63,13 @@ export default class App extends Component {
 
   render() {
     const {
-      isLoaded, posts, users, comments,
+      isLoaded, posts, users, comments, errorMessage,
     } = this.state;
 
     return (
       <div className="App">
         <h1>Dynamic list of posts</h1>
+        <h2>{errorMessage}</h2>
 
         {isLoaded
           ? <PostList posts={posts} users={users} comments={comments} />
