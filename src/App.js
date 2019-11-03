f9e3d5a611d6f4a0421737fcd58db8be83ee0c92
VadimK <djvadya16@gmail.com>
2019-11-03 12:55:58 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,87 @@
 import React from 'react';
 import './App.css';
+import { Button } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Dynamic list of posts</h1>
-    </div>
-  );
+import { getPosts, getUsers, getComments } from './Api';
+import PostsList from './components/postslist/PostsList';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+      users: [],
+      comments: [],
+      isLoading: false,
+      hasError: false,
+      dataReceived: false,
+    }
+  }
+
+  loadData = async () => {
+    this.setState({ isLoading: true })
+
+    const [ posts, users, comments ] = await Promise.all([getPosts(), getUsers(), getComments()])
+      .catch(() => this.setState({ hasError: true }))
+      .finally(() => this.setState({ isLoading: false }))
+
+    this.setState(prev => {
+      return {
+        ...prev,
+        posts: posts,
+        users: users,
+        comments: comments,
+        isLoading: false,
+        dataReceived: true,
+        hasError: false,
+      }
+    })
+  }
+
+  render() {
+    const {
+      dataReceived,
+      isLoading,
+      hasError,
+      posts,
+      users,
+      comments,
+    } = this.state;
+
+    if (!dataReceived && !hasError) {
+
+      return (
+        <div className="start-button">
+          {
+            isLoading
+            ? <Button loading></Button>
+            : <Button onClick={this.loadData}>Load</Button>
+          }
+        </div>
+      );
+    } else if (hasError) {
+
+      return (
+        <>
+          <div className="start-button">
+            <h3>Sorry! We have some problem with connection to server, please load data again.</h3>
+              {
+                isLoading
+                ? <Button loading></Button>
+                : <Button onClick={this.loadData}>Load</Button>
+              }
+          </div>
+        </>
+      );
+    } else {
+
+      return (
+        <PostsList posts={posts} users={users} comments={comments}/>
+      )
+    }
+  }
 }
-
+//
 export default App;
+
---
+++ src/components/comment/Comment.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Comment({comment:{name, body, email}}) {
+  return (
+    <p>
+      <strong>{name}</strong> ({email}): <br />{body}
+    </p>
+  )
+}
+
+Comment.propTypes = {
+  comment: PropTypes.object.isRequired,
+  id: PropTypes.number.isRequired,
+}
+//
+export default Comment;
