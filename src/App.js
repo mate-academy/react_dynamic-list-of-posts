dfe5d102053634611d285e295cbb985074fd121c
Viktor Lukovets <nuerio63@gmail.com>
2019-11-04 10:52:53 +0200

    added catch method

--- src/App.js
+++ src/App.js
@@ -8,24 +8,43 @@ class App extends React.Component {
   state = {
     posts: [],
     isLoading: false,
+    hasError: false,
   }
 
   loadPosts = () => {
     this.setState({
       isLoading: true,
+      hasError: false,
     });
 
     getPostsWithDependencies()
       .then((posts) => {
-        this.setState({ posts, isLoading: false });
+        this.setState({ posts });
+      })
+      .catch(() => {
+        this.setState({ hasError: true });
+      })
+      .finally(() => {
+        this.setState({ isLoading: false });
       });
   }
 
   render() {
-    const { posts, isLoading } = this.state;
+    const { posts, isLoading, hasError } = this.state;
 
     if (isLoading) {
       return <button type="button">Loading...</button>;
+    }
+
+    if (hasError) {
+      return (
+        <>
+          <p>Error occurred</p>
+          <button type="button" onClick={this.loadPosts}>
+            Try again
+          </button>
+        </>
+      );
     }
 
     if (posts.length === 0) {
