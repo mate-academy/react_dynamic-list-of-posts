44a72be732032652208df465edbb9a82a40ba340
Viktor Lukovets <nuerio63@gmail.com>
2019-11-05 10:22:09 +0200

    Solution

--- src/api.js
+++ src/api.js
@@ -2,13 +2,14 @@ const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
 const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
 const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments';
 
+async function loadData(url) {
+  return fetch(url).then(response => response.json());
+}
+
 const getPostsWithDependencies = async () => {
-  const postList = await fetch(POSTS_URL)
-    .then(response => response.json());
-  const userList = await fetch(USERS_URL)
-    .then(response => response.json());
-  const commentList = await fetch(COMMENTS_URL)
-    .then(response => response.json());
+  const postList = await loadData(POSTS_URL);
+  const userList = await loadData(USERS_URL);
+  const commentList = await loadData(COMMENTS_URL);
 
   return postList.map(post => (
     {
