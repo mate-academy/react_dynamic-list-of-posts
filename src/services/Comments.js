2236810224372b21e362440acc16523902c97b0e
dsrtF0x-git <54433971+dsrtF0x-git@users.noreply.github.com>
2019-11-05 13:21:12 +0200

    Delete Comments.js

--- src/services/Comments.js
+++
@@ -1,8 +0,0 @@
-const COMMENT_API = `https://jsonplaceholder.typicode.com/comments`;
-
-function getComments() {
-  return fetch(COMMENT_API)
-    .then(comments => comments.json());
-}
-
-export default getComments;
