0e7cd70d0b87e87c0ee4954ab37cdfca9bdc7d32
Liliya Taran <liliyataran2017@gmail.com>
2019-12-23 15:43:36 +0200

    fix

--- src/PostList.js
+++ src/PostList.js
@@ -1,35 +1,36 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { debounce } from 'debounce';
 import Post from './Post';
 
 const PostList = ({ posts }) => {
+  const [searchPosts, setSearchPosts] = useState(posts);
   const [searchingItem, setSearchingItem] = useState('');
 
-  const handleInput = (event) => {
-    setSearchingItem(event.target.value);
+  const handleInput = ({ target }) => {
+    setSearchingItem(target.value.toLowerCase());
+    getSearchedPosts(target.value.toLowerCase());
   };
 
-  const getSearchedPosts = (ArrOfPosts, inputValue) => (
-    ArrOfPosts.filter(post => (
-      post.title.toLowerCase().includes(inputValue.toLowerCase())
-      || post.body.toLowerCase().includes(inputValue.toLowerCase())
-    )));
-
-  const visiblePosts = searchingItem
-    ? getSearchedPosts(posts, searchingItem)
-    : posts;
+  const getSearchedPosts = debounce(inputValue => setSearchPosts(
+    posts.filter(post => (
+      post.title.toLowerCase().includes(inputValue)
+      || post.body.toLowerCase().includes(inputValue)
+    ))
+  ), 1000);
 
   return (
     <>
       <input
         onChange={handleInput}
+        value={searchingItem}
         className="search"
         type="text"
         placeholder="Search"
       />
-      {(visiblePosts.length < 1)
+      {(searchPosts.length < 1)
         ? (<div className="result">No results...</div>)
-        : (visiblePosts
+        : (searchPosts
           .map(currentPost => (
             <Post
               post={currentPost}
