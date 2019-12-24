81552c6c270b273e454076d500afaef5895de8cd
bandura <banduraweb@ukr.net>
2019-12-24 08:21:46 +0200

    solutuion

---
+++ src/components/PostsList/PostsLists.constants.js
@@ -0,0 +1,5 @@
+export default {
+  POSTS: 'https://jsonplaceholder.typicode.com/posts',
+  USERS: 'https://jsonplaceholder.typicode.com/users',
+  COMMENTS: 'https://jsonplaceholder.typicode.com/comments',
+};
