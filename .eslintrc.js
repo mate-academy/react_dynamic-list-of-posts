e6424eb3f9a13cddf3880dfcce024e1dc7cee532
mgrinko <glukym@gmail.com>
2020-01-22 17:49:14 +0200

    Typescript was added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,38 @@
 module.exports = {
-  extends: '@mate-academy/eslint-config-react',
+  "extends": [
+    "@mate-academy/eslint-config-react",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "plugins": [
+    "@typescript-eslint",
+    "react-hooks"
+  ],
+  "rules": {
+    // typescript
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/camelcase": ["error", { "properties": "never" }],
+    "no-unused-expressions": ["warn", {
+      "allowShortCircuit": true,
+      "allowTernary": true
+    }],
+    "@typescript-eslint/no-unused-vars": ["error"],
+
+    // react
+    "react/jsx-filename-extension": ["warn", {
+      "extensions": [".jsx", ".tsx"]
+    }],
+    "react/prop-types": "off", // Incompatible with TS props type?
+    "react-hooks/rules-of-hooks": "error",
+    "react/no-danger": "off",
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  },
 };
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <h1>Dynamic list of posts</h1>
+);
+
+export default App;
