9e5693586d5d0aef64460299d0ae5f2b6c1c306d
morfeii <morfeii88@gmail.com>
2020-03-05 11:10:47 +0200

    delete emty lines

--- src/api/getData.ts
+++ src/api/getData.ts
@@ -1,7 +1,6 @@
 import { API_URL } from '../utils/constants';
 import { PostType, UserType, CommentType } from '../utils/interfaces';
 
-
 const getData = async <T>(url: string): Promise<T> => {
   const response = await fetch(url);
   const data = await response.json();
