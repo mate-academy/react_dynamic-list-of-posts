014a6eb0b14ebff582d3ef500f09e204ca4f2737
ShapovalDenys <denys.igorevich@gmail.com>
2020-05-14 16:37:24 +0300

    add debounce

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import React, { useState, FormEvent } from 'react';
+import React, {
+  useState, FormEvent, useMemo, useCallback,
+} from 'react';
 import './App.css';
+import debounce from 'lodash.debounce';
 import { getPosts, getUsers, getComments } from './API';
 import PostList from './PostList';
 
@@ -8,6 +11,7 @@ const App = () => {
   const [isLoading, setIsLoadind] = useState(false);
   const [isLoaded, setIsLoaded] = useState(false);
   const [query, setQuery] = useState('');
+  const [filterQuery, setFilterQuery] = useState('.');
 
   const loadPosts = async () => {
     setIsLoadind(true);
@@ -25,18 +29,24 @@ const App = () => {
     setPreparedPosts(preparedPosts);
     setIsLoadind(false);
     setIsLoaded(true);
+    setFilterQuery('');
   };
+
+  const setFilterQueryWithDebounce = useCallback(debounce(setFilterQuery, 1000), []);
 
   const handleInput = (event: FormEvent<HTMLInputElement>) => {
     const { value } = event.target as HTMLInputElement;
 
     setQuery(value);
+    setFilterQueryWithDebounce(value);
   };
 
-  const filteredPosts = posts
-    .filter((post: PostProps) => (post.title + post.body)
-      .toLowerCase()
-      .includes(query.toLowerCase()));
+  const filteredPosts = useMemo(
+    () => posts
+      .filter((post: PostProps) => (post.title + post.body)
+        .toLowerCase()
+        .includes(query.toLowerCase())), [filterQuery],
+  );
 
   return (
     <>
@@ -53,7 +63,14 @@ const App = () => {
           </button>
         )}
         {isLoaded
-        && <input type="text" className="form-control mrl" onChange={handleInput} />}
+        && (
+          <input
+            type="text"
+            value={query}
+            className="form-control mrl"
+            onChange={handleInput}
+          />
+        )}
       </div>
       {isLoading
         && <div className="spinner-border text-primary" role="status" />}
