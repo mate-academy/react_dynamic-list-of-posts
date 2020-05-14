79cabe793b42326711c2d452ead85e57b6b80ec3
Nick <nikolyaoooyartchuk@gmail.com>
2020-05-14 10:53:30 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,18 @@ const App: React.FC = () => {
   const [posts, setPosts] = useState<PostType[]>([]);
   const [users, setUsers] = useState<UserType[]>([]);
   const [comments, setComments] = useState<CommentType[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isInitialized, setIsInitialized] = useState(false);
 
   function LoadData() {
+    setIsInitialized(true);
     getElements().then(resolve => {
       const [tempPosts, tempUsers, TempComments] = resolve;
 
       setPosts(tempPosts);
       setUsers(tempUsers);
       setComments(TempComments);
+      setIsLoaded(true);
     });
   }
 
@@ -27,9 +31,16 @@ const App: React.FC = () => {
           comments={comments}
         />
       )}
-      <button type="button" onClick={LoadData}>
-        Load
-      </button>
+      {!isInitialized && !isLoaded && (
+        <button type="button" onClick={LoadData}>
+          Load
+        </button>
+      )}
+      {(isInitialized && (
+        <p>
+          Loading...
+        </p>
+      ))}
     </div>
   );
 };
