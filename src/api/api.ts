e254bde6f95c1293270db92f740430793aaf2f6b
Iurii Petrov <yurapetrov91@gmail.com>
2020-05-18 18:32:49 +0300

    Solution

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,23 +1,10 @@
-const BASE_URL = 'https://jsonplaceholder.typicode.com/';
-const POSTS_URL = 'posts';
-const USERS_URL = 'users';
-const COMMENTS_URL = 'comments';
+const API_URL = 'https://jsonplaceholder.typicode.com/';
 
-const getData = <T>(url: string): Promise<T> => {
-  return fetch(url)
-    .then(response => response.json());
-};
+const getData = async (url: string) => {
+  const response =  await fetch(url);
+  const data = await response.json();
 
-const getPosts = (postsUrl: string): Promise<Post[]> => {
-  return getData(`${BASE_URL}${postsUrl}`);
-};
-
-const getComments = (commentUrl: string): Promise<Comment[]> => {
-  return getData(`${BASE_URL}${commentUrl}`);
-};
-
-const getUsers = (usersUrl: string): Promise<User[]> => {
-  return getData(`${BASE_URL}${usersUrl}`);
+  return data;
 };
 
 export const getPrepearedPosts = async (): Promise<PostWithUser[]> => {
@@ -26,15 +13,15 @@ export const getPrepearedPosts = async (): Promise<PostWithUser[]> => {
     comments,
     users,
   ] = await Promise.all([
-    getPosts(POSTS_URL),
-    getComments(COMMENTS_URL),
-    getUsers(USERS_URL),
+    getData(`${API_URL}posts`),
+    getData(`${API_URL}comments`),
+    getData(`${API_URL}users`),
   ]);
 
-  const PrepearedPosts = posts.map(post => ({
+  const PrepearedPosts = posts.map((post: Post) => ({
     ...post,
-    user: users.find(user => user.id === post.userId) as User,
-    comments: comments.filter(comment => comment.postId === post.id),
+    user: users.find((user: User) => user.id === post.userId),
+    comments: comments.filter((comment: Comment) => comment.postId === post.id),
   }));
 
   return PrepearedPosts;
