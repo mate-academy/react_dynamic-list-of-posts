19f863320f78afdb773774223401d0886e973994
Illia Oliynik <illiaoliynik@gmail.com>
2020-05-19 08:47:23 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import {
+  getComments,
+  getPosts,
+  getUsers,
+  Post,
+} from './api';
 
-const App = () => (
-  <h1>Dynamic list of posts</h1>
-);
+const App = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [query, setQuery] = useState<string>('');
+
+  const handleLoadClick = async () => {
+    setLoading(true);
+
+    try {
+      const postsFromServer = await getPosts();
+      const commentsFromServer = await getComments();
+      const usersFromServer = await getUsers();
+
+      const postsWithUsersAndComments: Post[] = postsFromServer.map(post => ({
+        ...post,
+        user: usersFromServer.find(user => user.id === post.userId),
+        comment: commentsFromServer.filter(comment => comment.postId === post.id),
+      }));
+
+      setPosts(postsWithUsersAndComments);
+    } catch (e) {
+      setError('Loading error');
+    }
+
+    setLoading(false);
+  };
+
+  const filterMovieList = posts
+    .filter(({ title, body }) => (title + body)
+      .toLowerCase()
+      .includes(query.toLowerCase()));
+
+  return (
+    <>
+      <h1>Dynamic list of posts</h1>
+      {!posts.length ? (
+        <>
+          <button type="button" onClick={handleLoadClick} disabled={loading}>
+            {loading ? 'Loading...' : 'load'}
+          </button>
+          {error && (
+            <>
+              <span>{error}</span>
+              <button type="button" onClick={handleLoadClick} disabled={loading}>
+                try again
+              </button>
+            </>
+          )}
+        </>
+      ) : (
+        <ul>
+          <input type="text" onChange={event => (setQuery(event.target.value))} />
+          {filterMovieList.map(post => (
+            <li key={post.id}>
+              <p>{`${post.id}. ${post.title}`}</p>
+              <p>{`body: ${post.body}`}</p>
+              <p>{`user: ${post.user ? post.user.name : 'Unknown'}`}</p>
+              <ul>
+                {post.comment ? post.comment.map(comment => (
+                  <li key={comment.id}>
+                    <p>{`name: ${comment.name}`}</p>
+                    <p>{`body: ${comment.body}`}</p>
+                  </li>
+                )) : 'Unknown'}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
 
 export default App;
---
+++ src/api.ts
@@ -0,0 +1,31 @@
+const API_URL = 'https://jsonplaceholder.typicode.com';
+
+export interface Post {
+  id: number;
+  userId: number;
+  body: string;
+  title: string;
+  user?: User;
+  comment?: Comment[];
+}
+
+export interface User {
+  id: number;
+  name: string;
+}
+
+export interface Comment {
+  id: number;
+  postId: number;
+  body: string;
+  name: string;
+}
+
+const getDataFromServer = <T>(url: string): Promise<T[]> => {
+  return fetch(API_URL + url)
+    .then(response => response.json());
+};
+
+export const getPosts = () => getDataFromServer<Post>('/posts');
+export const getUsers = () => getDataFromServer<User>('/users');
+export const getComments = () => getDataFromServer<Comment>('/comments');
