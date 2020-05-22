cc0464c50b7fda0f2d67fef28832461a29b9edc5
euxos <eugen.ostrozhinsky@gmail.com>
2020-05-22 15:06:30 +0300

    filter was added

--- src/App.tsx
+++ src/App.tsx
@@ -15,15 +15,21 @@ export const App = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [comments, setCommnets] = useState<Comment[]>([]);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [erroeMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [filteredPosts, setFilteredPosts] = useState<Post[]>([]);
+  const [filterQuery, setFilterQuery] = useState('');
 
   const handleClick = async () => {
+    setIsLoading(true);
+
     try {
       const postsFromServer = await getPosts();
       const usersFromServer = await getUsers();
       const commentsFromServer = await getComments();
 
       setPosts(postsFromServer);
+      setFilteredPosts(postsFromServer);
       setUsers(usersFromServer);
       setCommnets(commentsFromServer);
       setIsLoaded(true);
@@ -31,7 +37,25 @@ export const App = () => {
     } catch (exeption) {
       setErrorMessage('Error');
     }
+
+    setIsLoading(false);
   }
+
+  const changeFilterInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterQuery(event.target.value);
+
+    setTimeout(() => {
+      setFilteredPosts(posts.filter(({ title, body }) => (title + body)
+        .toLowerCase()
+        .includes(filterQuery.toLowerCase())));
+    }, 1000);
+  }
+
+  const resetFilter = () => {
+    setFilteredPosts(posts);
+    setFilterQuery('');
+  }
+
 
   return (
     <section className="post-list">
@@ -42,17 +66,36 @@ export const App = () => {
         <button
           type="button"
           onClick={handleClick}
+          disabled={isLoading}
         >
-          Load
+          {isLoading ? `Loading...` : `Load`}
         </button>
-        {erroeMessage && (<p>{erroeMessage}</p>)}
+        {errorMessage && (<p>{errorMessage}</p>)}
       </>
       ) : (
-        <PostList
-          posts={posts}
-          users={users}
-          comments={comments}
-        />
+        <>
+          <div>
+            <input
+              type="textarea"
+              className="post-list__filter"
+              value={filterQuery}
+              onChange={changeFilterInput}
+            />
+            <button
+              type="button"
+              onClick={resetFilter}
+            >
+              Reset
+            </button>
+          </div>
+
+          <PostList
+            posts={filteredPosts}
+            users={users}
+            comments={comments}
+          />
+        </>
+
       )}
     </section>
   )
