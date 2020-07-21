846098896e0efb9324c17a2c1c85c4eea7982c29
Andrew Nickyforov <nik_andrey@ukr.net>
2020-07-21 13:58:16 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { PostList } from './components/PostList/PostList';
+import { getPreparedPosts } from './api/api';
 
-const App = () => (
-  <h1>Dynamic list of posts</h1>
-);
+const App: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [searchValue, setSearchValue] = useState<string>('');
+
+  const handleStart = async () => {
+    setIsLoaded(true);
+
+    await getPreparedPosts().then(data => setPosts(data));
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    setSearchValue(value);
+  };
+
+  const handleEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.keyCode === 13) {
+      setPosts(prev => (
+        [...prev].sort((a, b) => (
+          Number(b.title.includes(searchValue)) - Number(a.title.includes(searchValue))
+        ))
+      ));
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Static list of posts</h1>
+
+      <p>
+        <span>posts: </span>
+        {posts.length}
+      </p>
+
+      { posts.length === 0
+        ? (
+          <button
+            disabled={isLoaded}
+            type="button"
+            onClick={handleStart}
+          >
+            {!isLoaded ? 'Load' : 'Loading...'}
+          </button>
+        )
+        : (
+          <>
+            <input
+              type="text"
+              onChange={handleChange}
+              value={searchValue}
+              onKeyUp={handleEnter}
+              placeholder="Type word(s) and press Enter"
+            />
+            <PostList posts={posts} />
+          </>
+        )}
+    </div>
+  );
+};
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,34 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+  user: User;
+  comments: Comment[];
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  addres: Address;
+  [prop: string]: any;
+}
+
+interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+interface Address {
+  city: string;
+  street: string;
+  [prop: string]: any;
+}
