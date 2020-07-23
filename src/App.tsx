8dcb1c80eeaa99e27e754e2011e97f9f5d0fc2d0
DimaDP <dimadp95@gmail.com>
2020-07-23 23:56:00 +0300

    added styles

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,7 @@ const App: React.FC = () => {
           <>
             <button
               type="button"
+              className="btn btn-secondary"
               onClick={handleButtonClick}
               disabled={isLoading}
             >
@@ -52,7 +53,7 @@ const App: React.FC = () => {
               <input
                 type="text"
                 placeholder="search..."
-                className="input"
+                className="form-control form-control-lg"
                 onChange={handleQuery}
               />
             </div>
--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { UserProps } from '../types';
 
-export const User: React.FC<UserProps> = ({ user }) => (
-  <div>
-    <b>{user ? user.name : ''}</b>
-    <br />
-    <span>{user ? user.email : ''}</span>
-    <br />
-    <span>
-      {user ? user.address.city : ''}
-      ,
-      {user ? user.address.street : ''}
-      <i> st.</i>
-      <br />
-      {user ? user.address.zipcode : ''}
-    </span>
-  </div>
-);
+export const User: React.FC<UserProps> = ({ user }) => {
+  if (user) {
+    return (
+      <div>
+        <b>{user.name}</b>
+        <br />
+        <span>{user.email}</span>
+        <br />
+        <span>
+          {user.address.city}
+          ,
+          {user.address.street}
+          <i> st.</i>
+          <br />
+          {user.address.zipcode}
+        </span>
+      </div>
+    );
+  }
+
+  return <></>;
+};
