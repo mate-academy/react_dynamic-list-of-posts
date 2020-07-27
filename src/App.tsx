2af3bf1253a4635a63249c449259820412f96600
Andrew Nickyforov <nik_andrey@ukr.net>
2020-07-27 18:18:17 +0300

    fixed working of app

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,15 @@ const App: React.FC = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [searchValue, setSearchValue] = useState<string>('');
+  const [filteredPosts, setFilteredPosts] = useState<Post[]>([]);
 
   const handleStart = async () => {
     setIsLoaded(true);
 
-    await getPreparedPosts().then(data => setPosts(data));
+    await getPreparedPosts().then(data => {
+      setPosts(data);
+      setFilteredPosts(data);
+    });
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,11 +26,11 @@ const App: React.FC = () => {
 
   const handleEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.keyCode === 13) {
-      setPosts(prev => (
-        [...prev].sort((a, b) => (
-          Number(b.title.includes(searchValue)) - Number(a.title.includes(searchValue))
-        ))
-      ));
+      const filter = posts.filter(post => (
+        post.title.includes(searchValue) || post.body.includes(searchValue)));
+
+      setFilteredPosts(filter);
+      setSearchValue('');
     }
   };
 
@@ -36,7 +40,7 @@ const App: React.FC = () => {
 
       <p>
         <span>posts: </span>
-        {posts.length}
+        {filteredPosts.length}
       </p>
 
       { posts.length === 0
@@ -58,7 +62,7 @@ const App: React.FC = () => {
               onKeyUp={handleEnter}
               placeholder="Type word(s) and press Enter"
             />
-            <PostList posts={posts} />
+            <PostList posts={filteredPosts} />
           </>
         )}
     </div>
