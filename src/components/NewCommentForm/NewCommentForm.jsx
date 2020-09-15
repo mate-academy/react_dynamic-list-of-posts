9376927c453d63b8bf88034b5e7634c38722f5ad
Alisa <aliceblunt8@gmail.com>
2020-09-15 17:46:14 +0300

    finish task

--- src/components/NewCommentForm/NewCommentForm.jsx
+++ src/components/NewCommentForm/NewCommentForm.jsx
@@ -1,39 +1,63 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './NewCommentForm.scss';
 
-export const NewCommentForm = () => (
-  <form className="NewCommentForm">
-    <div className="form-field">
-      <input
-        type="text"
-        name="name"
-        placeholder="Your name"
-        className="NewCommentForm__input"
-      />
-    </div>
+export const NewCommentForm = ({ postId, addNewComment }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    <div className="form-field">
-      <input
-        type="text"
-        name="email"
-        placeholder="Your email"
-        className="NewCommentForm__input"
-      />
-    </div>
+    const { name, email, body } = event.target;
 
-    <div className="form-field">
-      <textarea
-        name="body"
-        placeholder="Type comment here"
-        className="NewCommentForm__input"
-      />
-    </div>
+    const newComment = {
+      postId,
+      name: name.value,
+      email: email.value,
+      body: body.value,
+    };
 
-    <button
-      type="submit"
-      className="NewCommentForm__submit-button button"
-    >
-      Add a comment
-    </button>
-  </form>
-);
+    addNewComment(newComment);
+    event.target.reset();
+  };
+
+  return (
+    <form className="NewCommentForm" onSubmit={handleSubmit}>
+      <div className="form-field">
+        <input
+          type="text"
+          name="name"
+          placeholder="Your name"
+          className="NewCommentForm__input"
+        />
+      </div>
+
+      <div className="form-field">
+        <input
+          type="text"
+          name="email"
+          placeholder="Your email"
+          className="NewCommentForm__input"
+        />
+      </div>
+
+      <div className="form-field">
+        <textarea
+          name="body"
+          placeholder="Type comment here"
+          className="NewCommentForm__input"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="NewCommentForm__submit-button button"
+      >
+        Add a comment
+      </button>
+    </form>
+  );
+};
+
+NewCommentForm.propTypes = {
+  postId: PropTypes.number.isRequired,
+  addNewComment: PropTypes.func.isRequired,
+};
--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,45 +1,89 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { NewCommentForm } from '../NewCommentForm';
+import { getPostDetails } from '../../api/posts';
+import { getPostComments, deleteComment, addComment } from '../../api/comments';
 import './PostDetails.scss';
 
-export const PostDetails = () => (
-  <div className="PostDetails">
-    <h2>Post details:</h2>
+export const PostDetails = ({ postId }) => {
+  const [post, addPost] = useState([]);
+  const [comments, addComments] = useState([]);
+  const [areVisible, changeVisibility] = useState(true);
 
-    <section className="PostDetails__post">
-      <p>sunt aut facere repellat provident occaecati excepturi optio</p>
-    </section>
+  useEffect(() => {
+    const fetchPostDetails = async() => {
+      const postfromServer = await getPostDetails(postId);
+      const commentsfromServer = await getPostComments(postId);
 
-    <section className="PostDetails__comments">
-      <button type="button" className="button">Hide 2 comments</button>
+      addPost(postfromServer);
+      addComments(commentsfromServer);
+    };
 
-      <ul className="PostDetails__list">
-        <li className="PostDetails__list-item">
+    fetchPostDetails();
+  }, [postId]);
+
+  const handleDelete = (commentId) => {
+    deleteComment(commentId)
+      .then(() => getPostComments(postId))
+      .then(result => addComments(result));
+  };
+
+  const updateComments = (newComment) => {
+    addComment(newComment)
+      .then(() => getPostComments(postId))
+      .then(result => addComments(result));
+  };
+
+  return (
+    <div className="PostDetails">
+      <h2>Post details:</h2>
+
+      <section className="PostDetails__post">
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </section>
+
+      {comments.length > 0 && (
+        <section className="PostDetails__comments">
           <button
             type="button"
-            className="PostDetails__remove-button button"
+            className="button"
+            onClick={() => changeVisibility(!areVisible)}
           >
-            X
+            {areVisible
+              ? (`Hide ${comments.length} comments`)
+              : (`Show ${comments.length} comments`)}
           </button>
-          <p>My first comment</p>
-        </li>
 
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
-          >
-            X
-          </button>
-          <p>sad sds dfsadf asdf asdf</p>
-        </li>
-      </ul>
-    </section>
+          {areVisible && (
+            <ul className="PostDetails__list">
+              {comments.map(comment => (
+                <li key={comment.id} className="PostDetails__list-item">
+                  <button
+                    type="button"
+                    className="PostDetails__remove-button button"
+                    onClick={() => handleDelete(comment.id)}
+                  >
+                    X
+                  </button>
+                  <h4>{comment.name}</h4>
+                  <p>{comment.body}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
+      )}
 
-    <section>
-      <div className="PostDetails__form-wrapper">
-        <NewCommentForm />
-      </div>
-    </section>
-  </div>
-);
+      <section>
+        <div className="PostDetails__form-wrapper">
+          <NewCommentForm postId={postId} addNewComment={updateComments} />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+PostDetails.propTypes = {
+  postId: PropTypes.number.isRequired,
+};
