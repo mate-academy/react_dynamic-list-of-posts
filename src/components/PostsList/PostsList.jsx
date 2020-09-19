87a0531527fe81b66787811a905495f5c53fe345
oivannikov <alex0502ivannikov@gmail.com>
2020-09-19 20:35:04 +0300

    solution

--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -1,37 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './PostsList.scss';
 
-export const PostsList = () => (
-  <div className="PostsList">
-    <h2>Posts:</h2>
+export const PostsList = ({
+  posts,
+  viewPostUser,
+  setPostDetails,
+  postId,
+  setPostId
+}) => {
+  function handleClick(id) {
+    if (postId === id) {
+      setPostId(0);
+      setPostDetails([]);
+    } else {
+      setPostId(id);
+      viewPostUser(id);
+    }
+  }
 
-    <ul className="PostsList__list">
-      <li className="PostsList__item">
-        <div>
-          <b>[User #1]: </b>
-          sunt aut facere repellat provident occaecati excepturi optio
-        </div>
-        <button
-          type="button"
-          className="PostsList__button button"
-        >
-          Close
-        </button>
-      </li>
+  return (
+    <div className="PostsList">
+      <h2>Posts:</h2>
+      <ul className="PostsList__list">
+      {
+        posts.map(({userId, title, id}) => {
+          return (
+            <li
+              className="PostsList__item"
+              key={id}
+            >
+              <div>
+                <b>[User #{userId}]:</b>
+                {title}
+              </div>
+              <button
+                onClick={() => {
+                  handleClick(id);
+                }}
+                type="button"
+                className="PostsList__button button"
+              >
+                {id === postId ? "Close" : "Open"}
+              </button>
+            </li>
+          )
+        })
+       
+      }
+      </ul>
+    </div>
+  )
+};
 
-      <li className="PostsList__item">
-        <div>
-          <b>[User #2]: </b>
-          et ea vero quia laudantium autem
-        </div>
-
-        <button
-          type="button"
-          className="PostsList__button button"
-        >
-          Open
-        </button>
-      </li>
-    </ul>
-  </div>
-);
+PostsList.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    userId: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  })).isRequired,
+  viewPostUser: PropTypes.func.isRequired,
+  postId: PropTypes.number.isRequired,
+  setPostId: PropTypes.func.isRequired,
+  setPostDetails: PropTypes.func.isRequired,
+};
