f9b0df5866e9125971aca8d0c024771f0481c862
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-11-11 14:27:27 +0300

    changes

--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -2,33 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './PostsList.scss';
 import { ButtonOpenClose } from '../ButtonOpenClose';
+import { Loader } from '../Loader';
 
 export const PostsList = ({
   posts,
   changePostId,
 }) => (
-  <div className="PostsList">
-    <h2>Posts:</h2>
+  <>
+    {!posts.length ? (
+      <Loader />
+    ) : (
+      <div className="PostsList">
+        <h2>Posts:</h2>
 
-    <ul className="PostsList__list">
-      {posts.map(post => (
-        <li
-          key={post.id}
-          className="PostsList__item"
-        >
-          <div>
-            <b>{`User #${post.userId}`}</b>
-            <br />
-            {post.title}
-          </div>
-          <ButtonOpenClose
-            changePostId={changePostId}
-            postId={post.id}
-          />
-        </li>
-      ))}
-    </ul>
-  </div>
+        <ul className="PostsList__list">
+          {posts.map(post => (
+            <li
+              key={post.id}
+              className="PostsList__item"
+            >
+              <div>
+                <b>{`User #${post.userId}`}</b>
+                <br />
+                {post.title}
+              </div>
+              <ButtonOpenClose
+                changePostId={changePostId}
+                postId={post.id}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    )}
+  </>
 );
 
 PostsList.propTypes = {
