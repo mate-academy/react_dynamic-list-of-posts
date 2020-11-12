90b287f88318e2330a42153598184ff424b0f019
KirillLutsenko <mnrovski68@gmail.com>
2020-11-12 19:49:52 +0200

    React Dynamic List of Posts 100%

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Comment = ({ comment, remove }) => {
-  const handleClickRemove = () => remove(comment.id);
-
-  return (
-    <>
-      <li className="PostDetails__list-item">
-        <button
-          type="button"
-          className="PostDetails__remove-button button"
-          onClick={handleClickRemove}
-        >
-          X
-        </button>
-      </li>
-    </>
-  );
-};
+export const Comment = ({
+  comment,
+  remove,
+}) => (
+  <li className="PostDetails__list-item">
+    <button
+      type="button"
+      className="PostDetails__remove-button button"
+      onClick={() => remove(comment.id)}
+    >
+      X
+    </button>
+    <p>{comment.body}</p>
+  </li>
+);
 
 Comment.propTypes = {
   remove: PropTypes.func.isRequired,
--- src/components/CommentsList/CommentsList.jsx
+++ src/components/CommentsList/CommentsList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Comment } from '../Comment';
 
-export const CommentList = ({ remove, comments }) => (
+export const CommentsList = ({ remove, comments }) => (
   <ul className="PostDetails__list">
     {comments.map(comment => (
       <Comment
@@ -14,7 +14,7 @@ export const CommentList = ({ remove, comments }) => (
   </ul>
 );
 
-CommentList.propTypes = {
+CommentsList.propTypes = {
   remove: PropTypes.func.isRequired,
   comments: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number,
