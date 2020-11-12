e220b39ae445ac6ebd943176db2f6270888f8e5f
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-11-12 17:00:28 +0200

    implemented 1 - 6

---
+++ src/api/posts.js
@@ -0,0 +1,13 @@
+const BASE_URL = 'https://mate-api.herokuapp.com';
+
+export async function getUserPosts(userId) {
+  const response = await fetch(`${BASE_URL}/posts`);
+  const data = await response.json();
+  const posts = await data.data;
+
+  if (userId === '0') {
+    return posts;
+  }
+
+  return posts.filter(post => post.userId === +userId);
+}
