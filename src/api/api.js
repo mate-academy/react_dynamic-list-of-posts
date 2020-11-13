1cb7e986e8d3c7a37a8ef30225bc8fcc7d6a9889
goncharukop <goncharukop@gmail.com>
2020-11-13 20:46:45 +0200

    add comments from server

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,23 @@
 export const BASE_URL = 'https://mate-api.herokuapp.com';
+
+export async function request(url, options) {
+  const response = await fetch(`${BASE_URL}${url}`, options);
+
+  if (!response.ok) {
+    throw new Error(`${response.status} - ${response.statusText}`);
+  }
+
+  const result = await response.json();
+
+  return result.data;
+}
+
+export const post = (url, data) => request(url, {
+  method: 'POST',
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+  body: JSON.stringify(data),
+});
+
+export const remove = url => request(url, { method: 'DELETE' });
--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -1,37 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './PostsList.scss';
+import { getUserPosts } from '../../api/posts';
 
-export const PostsList = () => (
-  <div className="PostsList">
-    <h2>Posts:</h2>
+export const PostsList = () => {
+  const [posts, setPosts] = useState([]);
 
-    <ul className="PostsList__list">
-      <li className="PostsList__item">
-        <div>
-          <b>[User #1]: </b>
-          sunt aut facere repellat provident occaecati excepturi optio
-        </div>
-        <button
-          type="button"
-          className="PostsList__button button"
+  const loadPosts = async() => {
+    const loadedPosts = await getUserPosts();
+
+    setPosts(loadedPosts);
+  };
+
+  useEffect(() => {
+    loadPosts();
+  }, []);
+
+  return (
+    <ul>
+      {posts.map(post => (
+        <li
+          className="PostsList__item"
+          key={post.id}
         >
-          Close
-        </button>
-      </li>
-
-      <li className="PostsList__item">
-        <div>
-          <b>[User #2]: </b>
-          et ea vero quia laudantium autem
-        </div>
-
-        <button
-          type="button"
-          className="PostsList__button button"
-        >
-          Open
-        </button>
-      </li>
+          <div>
+            <b>{`[User #${post.userId}]:`}</b>
+            {post.title}
+          </div>
+          <button
+            type="button"
+            className="PostsList__button button"
+          >
+            Close
+          </button>
+        </li>
+      ))}
     </ul>
-  </div>
-);
+  );
+};
