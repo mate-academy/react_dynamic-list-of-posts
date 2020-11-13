608ccfd3d0e2b552c17f8fad51c33cb69ce3aed3
Liza Krasnoshchok <nlight1122@gmail.com>
2020-11-13 13:27:14 +0200

    solution 30%

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,13 @@
 export const BASE_URL = 'https://mate-api.herokuapp.com';
+
+export async function getAllPosts() {
+  const response = await fetch(`${BASE_URL}/posts`);
+
+  return (await response.json()).data;
+}
+
+export async function getPostDetails(postId) {
+  const response = await fetch(`${BASE_URL}/posts/${postId}`);
+
+  return (await response.json()).data;
+}
