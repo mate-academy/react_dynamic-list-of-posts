9a29944d7a5571061de5fa2816c28f5f28ebb024
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-11-22 14:07:13 +0200

    Solution

--- src/components/NewCommentForm/NewCommentForm.jsx
+++ src/components/NewCommentForm/NewCommentForm.jsx
@@ -1,39 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './NewCommentForm.scss';
+import { addComment, getPostComments } from '../../api/comments';
 
-export const NewCommentForm = () => (
-  <form className="NewCommentForm">
-    <div className="form-field">
-      <input
-        type="text"
-        name="name"
-        placeholder="Your name"
-        className="NewCommentForm__input"
-      />
-    </div>
+export const NewCommentForm = ({ selectedPostId, setComments }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [body, setBody] = useState('');
 
-    <div className="form-field">
-      <input
-        type="text"
-        name="email"
-        placeholder="Your email"
-        className="NewCommentForm__input"
-      />
-    </div>
+  const handleSendComment = (event) => {
+    event.preventDefault();
 
-    <div className="form-field">
-      <textarea
-        name="body"
-        placeholder="Type comment here"
-        className="NewCommentForm__input"
-      />
-    </div>
+    if (name && email && body) {
+      addComment(selectedPostId, name, email, body)
+        .then(() => getPostComments(selectedPostId))
+        .then(setComments);
 
-    <button
-      type="submit"
-      className="NewCommentForm__submit-button button"
+      setName('');
+      setEmail('');
+      setBody('');
+    }
+  };
+
+  return (
+    <form
+      className="NewCommentForm"
+      onSubmit={handleSendComment}
     >
-      Add a comment
-    </button>
-  </form>
-);
+      <div className="form-field">
+        <input
+          type="text"
+          name="name"
+          placeholder="Your name"
+          className="NewCommentForm__input"
+          value={name}
+          onChange={event => setName(event.target.value.trimLeft())}
+        />
+      </div>
+
+      <div className="form-field">
+        <input
+          type="text"
+          name="email"
+          placeholder="Your email"
+          className="NewCommentForm__input"
+          value={email}
+          onChange={event => setEmail(event.target.value.trimLeft())}
+        />
+      </div>
+
+      <div className="form-field">
+        <textarea
+          name="body"
+          placeholder="Type comment here"
+          className="NewCommentForm__input"
+          value={body}
+          onChange={event => setBody(event.target.value.trimLeft())}
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="NewCommentForm__submit-button button"
+      >
+        Add a comment
+      </button>
+    </form>
+  );
+};
+
+NewCommentForm.propTypes = {
+  selectedPostId: PropTypes.number.isRequired,
+  setComments: PropTypes.func.isRequired,
+};
--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -1,37 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { getUserPosts } from '../../api/posts';
 import './PostsList.scss';
 
-export const PostsList = () => (
-  <div className="PostsList">
-    <h2>Posts:</h2>
+export const PostsList = (
+  { selectedUser, selectedPostId, setSelectedPostId },
+) => {
+  const [posts, setPosts] = useState([]);
 
-    <ul className="PostsList__list">
-      <li className="PostsList__item">
-        <div>
-          <b>[User #1]: </b>
-          sunt aut facere repellat provident occaecati excepturi optio
-        </div>
-        <button
-          type="button"
-          className="PostsList__button button"
-        >
-          Close
-        </button>
-      </li>
+  useEffect(() => {
+    getUserPosts(selectedUser).then(setPosts);
+  }, [selectedUser]);
 
-      <li className="PostsList__item">
-        <div>
-          <b>[User #2]: </b>
-          et ea vero quia laudantium autem
-        </div>
+  return (
+    <div className="PostsList">
+      <h2>Posts:</h2>
 
-        <button
-          type="button"
-          className="PostsList__button button"
-        >
-          Open
-        </button>
-      </li>
-    </ul>
-  </div>
-);
+      <ul className="PostsList__list">
+        {posts.map(post => (
+          <li
+            key={post.id}
+            className="PostsList__item"
+          >
+            <div>
+              <b>{`[User ${post.userId}]: `}</b>
+              {post.title}
+            </div>
+            <button
+              type="button"
+              className="PostsList__button button"
+              onClick={() => {
+                selectedPostId === post.id
+                  ? setSelectedPostId(null)
+                  : setSelectedPostId(post.id);
+              }}
+            >
+              {
+                selectedPostId === post.id
+                  ? 'Close'
+                  : 'Open'
+              }
+            </button>
+          </li>
+        ))}
+
+      </ul>
+    </div>
+  );
+};
+
+PostsList.propTypes = {
+  selectedUser: PropTypes.number,
+  selectedPostId: PropTypes.number,
+  setSelectedPostId: PropTypes.func.isRequired,
+};
+
+PostsList.defaultProps = {
+  selectedUser: null,
+  selectedPostId: null,
+};
