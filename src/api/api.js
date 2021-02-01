70d64e85892c5ed479422f4d64147f918b032d08
Alex <alexmogwaiii@gmail.com>
2021-02-01 23:27:57 +0200

    made task

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,17 @@
 export const BASE_URL = 'https://mate-api.herokuapp.com';
+
+export const request = async(url, option) => {
+  try {
+    const response = await fetch(`${BASE_URL}${url}`, option);
+
+    if (!response.ok) {
+      throw new Error(`
+          ${response.status} - ${response.statusText}
+        `);
+    }
+
+    return response.json();
+  } catch (error) {
+    throw new Error(error);
+  }
+};
--- src/components/NewCommentForm/NewCommentForm.jsx
+++ src/components/NewCommentForm/NewCommentForm.jsx
@@ -1,39 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './NewCommentForm.scss';
 
-export const NewCommentForm = () => (
-  <form className="NewCommentForm">
-    <div className="form-field">
-      <input
-        type="text"
-        name="name"
-        placeholder="Your name"
-        className="NewCommentForm__input"
-      />
-    </div>
+export const NewCommentForm = ({ postComment }) => {
+  const [nameQuery, setName] = useState('');
+  const [emailQuery, setEmail] = useState('');
+  const [bodyQuery, setBody] = useState('');
 
-    <div className="form-field">
-      <input
-        type="text"
-        name="email"
-        placeholder="Your email"
-        className="NewCommentForm__input"
-      />
-    </div>
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-    <div className="form-field">
-      <textarea
-        name="body"
-        placeholder="Type comment here"
-        className="NewCommentForm__input"
-      />
-    </div>
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'body':
+        setBody(value);
+        break;
+      default:
+        break;
+    }
+  };
 
-    <button
-      type="submit"
-      className="NewCommentForm__submit-button button"
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (nameQuery && emailQuery && bodyQuery) {
+      postComment([nameQuery, emailQuery, bodyQuery]);
+    }
+  };
+
+  return (
+    <form
+      className="NewCommentForm"
+      onSubmit={handleSubmit}
     >
-      Add a comment
-    </button>
-  </form>
-);
+      <div className="form-field">
+        <input
+          type="text"
+          name="name"
+          placeholder="Your name"
+          className="NewCommentForm__input"
+          value={nameQuery}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-field">
+        <input
+          type="text"
+          name="email"
+          placeholder="Your email"
+          className="NewCommentForm__input"
+          value={emailQuery}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div className="form-field">
+        <textarea
+          name="body"
+          placeholder="Type comment here"
+          className="NewCommentForm__input"
+          value={bodyQuery}
+          onChange={handleChange}
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="NewCommentForm__submit-button button"
+      >
+        Add a comment
+      </button>
+    </form>
+  );
+};
+
+NewCommentForm.propTypes = {
+  postComment: PropTypes.func.isRequired,
+};
