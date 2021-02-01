6dd5e279ca6b6f18564230972dcc0cff0d28e229
Khrulenko Vitalii <khrulenko.v@gmail.com>
2021-02-02 01:43:59 +0200

    solution

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,45 +1,113 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
 import { NewCommentForm } from '../NewCommentForm';
+import { getPostComments, removeComment } from '../../api/comments';
+
 import './PostDetails.scss';
 
-export const PostDetails = () => (
-  <div className="PostDetails">
-    <h2>Post details:</h2>
+export const PostDetails = ({
+  selectedPost,
+  areCommentsHidden,
+  setAreCommentsHidden,
+}) => {
+  const [postComments, setPostComments] = useState([]);
 
-    <section className="PostDetails__post">
-      <p>sunt aut facere repellat provident occaecati excepturi optio</p>
-    </section>
+  function loadPostComments() {
+    getPostComments().then((allComments) => {
+      setPostComments(
+        allComments.filter(com => com.postId === selectedPost.id),
+      );
+    });
+  }
 
-    <section className="PostDetails__comments">
-      <button type="button" className="button">Hide 2 comments</button>
+  useEffect(() => {
+    loadPostComments();
+  }, [selectedPost]);
 
-      <ul className="PostDetails__list">
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
+  return (
+    <div className="PostDetails">
+      <h2>Post details:</h2>
+
+      <section className="PostDetails__post">
+        <p>{selectedPost.title}</p>
+      </section>
+
+      {(postComments.length === 0)
+        ? (
+          <p
+            className="PostDetails__preloadText"
           >
-            X
-          </button>
-          <p>My first comment</p>
-        </li>
+            There are no comments yet. But you can add some:
+          </p>
+        )
+        : (
+          <section className="PostDetails__comments">
+            <button
+              type="button"
+              className="button PostDetails__hide-show-button"
+              onClick={() => setAreCommentsHidden(state => !state)}
+            >
+              {`${areCommentsHidden ? 'Hide' : 'Show'} ${postComments.length}`}
+              {' '}
+              comment
+              {
+                postComments.length > 1 ? 's' : ''
+              }
+            </button>
 
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
-          >
-            X
-          </button>
-          <p>sad sds dfsadf asdf asdf</p>
-        </li>
-      </ul>
-    </section>
+            {areCommentsHidden && (
+              <ul className="PostDetails__list">
+                {postComments.map(comment => (
+                  <li
+                    className="PostDetails__list-item"
+                    key={comment.id}
+                  >
+                    <button
+                      type="button"
+                      className="PostDetails__remove-button button"
+                      onClick={() => {
+                        removeComment(comment.id)
+                          .then((data) => {
+                            loadPostComments();
+                          });
+                      }}
+                    >
+                      X
+                    </button>
+                    <p>
+                      <strong>
+                        {comment.name}
+                        :
+                      </strong>
+                      {comment.body}
+                    </p>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </section>
+        )}
 
-    <section>
-      <div className="PostDetails__form-wrapper">
-        <NewCommentForm />
-      </div>
-    </section>
-  </div>
-);
+      <section>
+        <div className="PostDetails__form-wrapper">
+          <NewCommentForm
+            postId={selectedPost.id}
+            postComments={postComments}
+            setPostComments={setPostComments}
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+PostDetails.propTypes = {
+  selectedPost: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    userId: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  areCommentsHidden: PropTypes.bool.isRequired,
+  setAreCommentsHidden: PropTypes.func.isRequired,
+};
--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -1,37 +1,87 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './PostsList.scss';
 
-export const PostsList = () => (
+export const PostsList = ({
+  posts,
+  selectedUser,
+  selectedPost,
+  setSelectedPost,
+  isOpen,
+  setIsOpen,
+  setAreCommentsHidden,
+}) => (
   <div className="PostsList">
     <h2>Posts:</h2>
 
     <ul className="PostsList__list">
-      <li className="PostsList__item">
-        <div>
-          <b>[User #1]: </b>
-          sunt aut facere repellat provident occaecati excepturi optio
-        </div>
-        <button
-          type="button"
-          className="PostsList__button button"
-        >
-          Close
-        </button>
-      </li>
+      {posts.map((post) => {
+        if (
+          selectedUser === '0'
+            || +selectedUser === post.userId
+        ) {
+          return (
+            <li
+              className="PostsList__item"
+              key={post.id}
+            >
+              <div>
+                <b>
+                  [User #
+                  {post.userId}
+                  ]:
+                  {' '}
+                </b>
+                {post.title}
+              </div>
 
-      <li className="PostsList__item">
-        <div>
-          <b>[User #2]: </b>
-          et ea vero quia laudantium autem
-        </div>
+              <button
+                type="button"
+                className={`PostsList__button button ${
+                  (isOpen && +selectedPost.id === post.id)
+                    ? 'PostsList__close-button'
+                    : ''
+                }`}
 
-        <button
-          type="button"
-          className="PostsList__button button"
-        >
-          Open
-        </button>
-      </li>
+                onClick={() => {
+                  if (selectedPost.id !== post.id) {
+                    setSelectedPost(post);
+                    setIsOpen(true);
+                    setAreCommentsHidden(true);
+                  } else {
+                    setIsOpen(state => !state);
+                  }
+                }}
+              >
+                {(isOpen && +selectedPost.id === post.id) ? `Close` : `Open`}
+              </button>
+            </li>
+          );
+        }
+
+        return (<></>);
+      })}
     </ul>
   </div>
 );
+
+PostsList.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      userId: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+  selectedUser: PropTypes.string.isRequired,
+  selectedPost: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    userId: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  setSelectedPost: PropTypes.func.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  setIsOpen: PropTypes.func.isRequired,
+  setAreCommentsHidden: PropTypes.func.isRequired,
+};
