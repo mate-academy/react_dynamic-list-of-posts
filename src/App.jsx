325f8b750edb757da00a37c4c38dfc7b45fa6214
Iryna Kozhura <ivladimirovna713@gmail.com>
2021-02-02 18:19:31 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 
-const App = () => (
-  <div className="App">
-    <header className="App__header">
-      <label>
-        Select a user: &nbsp;
+class App extends Component {
+  state ={
+    userId: 0,
+    postId: 0,
+    isPostChosen: false,
+  };
 
-        <select className="App__user-selector">
-          <option value="0">All users</option>
-          <option value="1">Leanne Graham</option>
-          <option value="2">Ervin Howell</option>
-          <option value="3">Clementine Bauch</option>
-          <option value="4">Patricia Lebsack</option>
-          <option value="5">Chelsey Dietrich</option>
-          <option value="6">Mrs. Dennis Schulist</option>
-          <option value="7">Kurtis Weissnat</option>
-          <option value="8">Nicholas Runolfsdottir V</option>
-          <option value="9">Glenna Reichert</option>
-          <option value="10">Leanne Graham</option>
-        </select>
-      </label>
-    </header>
+  handleChoseUser = (event) => {
+    const { name, value } = event.target;
 
-    <main className="App__main">
-      <div className="App__sidebar">
-        <PostsList />
+    this.setState({
+      [name]: +value,
+    });
+  };
+
+  handleOpenPost = (id) => {
+    if (this.state.postId === id && this.state.isPostChosen) {
+      this.setState({ isPostChosen: false });
+    } else {
+      this.setState({
+        postId: id,
+        isPostChosen: true,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App__header">
+          <label>
+            Select a user: &nbsp;
+
+            <select
+              className="App__user-selector"
+              value={this.state.userId}
+              name="userId"
+              onChange={this.handleChoseUser}
+            >
+              <option value="0">All users</option>
+              <option value="1">Leanne Graham</option>
+              <option value="2">Ervin Howell</option>
+              <option value="3">Clementine Bauch</option>
+              <option value="4">Patricia Lebsack</option>
+              <option value="5">Chelsey Dietrich</option>
+              <option value="6">Mrs. Dennis Schulist</option>
+              <option value="7">Kurtis Weissnat</option>
+              <option value="8">Nicholas Runolfsdottir V</option>
+              <option value="9">Glenna Reichert</option>
+              <option value="10">Leanne Graham</option>
+            </select>
+          </label>
+        </header>
+
+        <main className="App__main">
+          <div className="App__sidebar">
+            <PostsList
+              onOpen={this.handleOpenPost}
+              user={this.state.userId}
+              isPostChosen={this.state.isPostChosen}
+              selectedPost={this.state.postId}
+            />
+          </div>
+
+          <div className="App__content">
+            {this.state.isPostChosen
+            && <PostDetails postId={this.state.postId} />}
+          </div>
+        </main>
       </div>
-
-      <div className="App__content">
-        <PostDetails />
-      </div>
-    </main>
-  </div>
-);
+    );
+  }
+}
 
 export default App;
--- src/components/NewCommentForm/NewCommentForm.jsx
+++ src/components/NewCommentForm/NewCommentForm.jsx
@@ -1,39 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './NewCommentForm.scss';
 
-export const NewCommentForm = () => (
-  <form className="NewCommentForm">
-    <div className="form-field">
-      <input
-        type="text"
-        name="name"
-        placeholder="Your name"
-        className="NewCommentForm__input"
-      />
-    </div>
+export const NewCommentForm = ({ addComment }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [body, setBody] = useState('');
 
-    <div className="form-field">
-      <input
-        type="text"
-        name="email"
-        placeholder="Your email"
-        className="NewCommentForm__input"
-      />
-    </div>
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
 
-    <div className="form-field">
-      <textarea
-        name="body"
-        placeholder="Type comment here"
-        className="NewCommentForm__input"
-      />
-    </div>
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
 
-    <button
-      type="submit"
-      className="NewCommentForm__submit-button button"
+  const handleBody = (event) => {
+    setBody(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addComment({
+      name, email, body,
+    });
+  };
+
+  return (
+    <form
+      className="NewCommentForm"
+      onSubmit={handleSubmit}
     >
-      Add a comment
-    </button>
-  </form>
-);
+      <div className="form-field">
+        <input
+          type="text"
+          name="name"
+          placeholder="Your name"
+          className="NewCommentForm__input"
+          value={name}
+          onChange={handleName}
+        />
+      </div>
+
+      <div className="form-field">
+        <input
+          type="text"
+          name="email"
+          placeholder="Your email"
+          className="NewCommentForm__input"
+          value={email}
+          onChange={handleEmail}
+        />
+      </div>
+
+      <div className="form-field">
+        <textarea
+          name="body"
+          placeholder="Type comment here"
+          className="NewCommentForm__input"
+          value={body}
+          onChange={handleBody}
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="NewCommentForm__submit-button button"
+      >
+        Add a comment
+      </button>
+    </form>
+  );
+};
+
+NewCommentForm.propTypes = {
+  addComment: PropTypes.func.isRequired,
+};
