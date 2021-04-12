5ba784738d4b5e09a639eb481e619d060e7c46c2
Ruslan Shmadchenko <ruslanwpua@gmail.com>
2021-04-13 00:00:12 +0300

    solution with TS

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,36 @@
+// export const BASE_URL = 'https://jsonplaceholder.typicode.com';
 export const BASE_URL = 'https://mate-api.herokuapp.com';
+
+export function loadPosts() {
+  return fetch(`${BASE_URL}/posts`)
+    .then(response => response.json())
+}
+
+export function getUserPosts(userId) {
+  return fetch(`${BASE_URL}/posts/${userId}`)
+    .then(response => response.json())
+}
+
+export function getPostDetails(postId) {
+  return fetch(`${BASE_URL}/posts/${postId}`)
+    .then(response => response.json())
+}
+
+export function getPostComments(postId) {
+  return fetch(`${BASE_URL}/comments/`)
+    .then(response => response.json())
+    .then(comments => comments.data.filter(comment => comment.postId === postId))
+}
+
+export function removeComment(commentId) {
+  return fetch(`${BASE_URL}/comments/${commentId}`, { method: 'DELETE' })
+    .then(res => res.json())
+}
+
+export function addComment(comment) {
+  return fetch(`${BASE_URL}/comments/`, {
+     method: 'POST',
+     body: JSON.stringify(comment) 
+  })
+    .then(res => res.json())
+}
--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,45 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NewCommentForm } from '../NewCommentForm';
+import { Loader } from '../Loader/Loader';
 import './PostDetails.scss';
+import { removeComment, getPostComments } from '../../api/api';
+import { Post } from '../../types';
 
-export const PostDetails = () => (
-  <div className="PostDetails">
+// interface Props {
+//   selectedPostId: number,
+//   posts: Array<Post>,
+// }
+
+// export const PostDetails: React.FC<Props> = ({ selectedPostId, posts }) => {
+  export const PostDetails = ({ selectedPostId, posts }) => {
+  const [comments, setComments] = useState([]);
+  const [buttonIsActive, setButtonIsActive] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const post = posts.find(post => post.id === selectedPostId);
+
+  console.log(post.id);
+  console.log('post');
+
+  useEffect(() => {
+    (async function anyNameFunction() {
+      setIsLoading(true);
+
+      const result = await getPostComments(selectedPostId);
+
+      setComments(result);
+
+      setIsLoading(false);
+    })();
+  }, [selectedPostId]);
+
+  const handleButton = () => {
+    if (buttonIsActive) {
+      setButtonIsActive(false);
+    } else {
+      setButtonIsActive(true);
+    }
+  }
+
+  const deleteComment = async (id) => {
+    await removeComment(id);
+    getPostComments(selectedPostId)
+      .then(setComments);
+  }
+
+  if (isLoading) {
+    return <Loader />
+  }
+
+  return (
+    <div className="PostDetails">
     <h2>Post details:</h2>
 
     <section className="PostDetails__post">
-      <p>sunt aut facere repellat provident occaecati excepturi optio</p>
+      <p>{post.title}</p>
     </section>
 
     <section className="PostDetails__comments">
-      <button type="button" className="button">Hide 2 comments</button>
+      <button
+        type="button"
+        className="button"
+        onClick={handleButton}
+      >
+        {buttonIsActive ? `Show ${comments.length} comments` : `Hide ${comments.length} comments`}
+      </button>
 
       <ul className="PostDetails__list">
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
-          >
-            X
-          </button>
-          <p>My first comment</p>
-        </li>
-
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
-          >
-            X
-          </button>
-          <p>sad sds dfsadf asdf asdf</p>
-        </li>
+        {comments.length > 0 && !buttonIsActive && comments.map(comment => (
+          <li className="PostDetails__list-item" key={comment.id}>
+            <button
+              type="button"
+              className="PostDetails__remove-button button"
+              onClick={() => deleteComment(comment.id)}
+            >
+              X
+            </button>
+            <p>{comment.name}</p>
+          </li>
+        ))}
       </ul>
     </section>
 
     <section>
       <div className="PostDetails__form-wrapper">
-        <NewCommentForm />
+        <NewCommentForm
+          postId={post.id}
+          onSetComments={setComments}
+        />
       </div>
     </section>
   </div>
-);
+  )
+}
