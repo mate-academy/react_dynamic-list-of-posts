752f3fe1fa1a6f265aacdf1e74f1944f0b6ef992
Ping <4ivrik@ukr.net>
2021-04-14 21:34:13 +0300

    solution

--- src/components/NewCommentForm/NewCommentForm.jsx
+++ src/components/NewCommentForm/NewCommentForm.jsx
@@ -1,39 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './NewCommentForm.scss';
+import { createComment } from '../../api/api';
 
-export const NewCommentForm = () => (
-  <form className="NewCommentForm">
-    <div className="form-field">
-      <input
-        type="text"
-        name="name"
-        placeholder="Your name"
-        className="NewCommentForm__input"
-      />
-    </div>
+const { uuid } = require('uuidv4');
 
-    <div className="form-field">
-      <input
-        type="text"
-        name="email"
-        placeholder="Your email"
-        className="NewCommentForm__input"
-      />
-    </div>
+export const NewCommentForm = ({ selectedPostId, addComment }) => {
+  const [userName, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [body, setBody] = useState('');
 
-    <div className="form-field">
-      <textarea
-        name="body"
-        placeholder="Type comment here"
-        className="NewCommentForm__input"
-      />
-    </div>
+  const handleInputChange = (event, callback) => {
+    const { value } = event.target;
 
-    <button
-      type="submit"
-      className="NewCommentForm__submit-button button"
+    callback(value);
+  };
+
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+    const newComment = {
+      id: uuid(),
+      postId: selectedPostId,
+      name: userName,
+      email,
+      body,
+    };
+
+    await createComment(newComment);
+    addComment(newComment);
+
+    setName('');
+    setEmail('');
+    setBody('');
+  };
+
+  return (
+    <form
+      className="NewCommentForm"
+      onSubmit={handleSubmit}
     >
-      Add a comment
-    </button>
-  </form>
-);
+      <div className="form-field">
+        <input
+          type="text"
+          name="name"
+          value={userName}
+          placeholder="Your name"
+          className="NewCommentForm__input"
+          onChange={event => handleInputChange(event, setName)}
+          required
+        />
+      </div>
+
+      <div className="form-field">
+        <input
+          type="text"
+          name="email"
+          value={email}
+          placeholder="Your email"
+          className="NewCommentForm__input"
+          onChange={event => handleInputChange(event, setEmail)}
+          required
+        />
+      </div>
+
+      <div className="form-field">
+        <textarea
+          name="body"
+          value={body}
+          placeholder="Type comment here"
+          className="NewCommentForm__input"
+          onChange={event => handleInputChange(event, setBody)}
+          required
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="NewCommentForm__submit-button button"
+      >
+        Add a comment
+      </button>
+    </form>
+  );
+};
+
+NewCommentForm.propTypes = {
+  selectedPostId: PropTypes.number.isRequired,
+  addComment: PropTypes.func.isRequired,
+};
--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,45 +1,121 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { getPostComments, getPostDetails, removeComment } from '../../api/api';
+import { Loader } from '../Loader';
 import { NewCommentForm } from '../NewCommentForm';
 import './PostDetails.scss';
 
-export const PostDetails = () => (
-  <div className="PostDetails">
-    <h2>Post details:</h2>
+export const PostDetails = ({ selectedPostId }) => {
+  const [postDetails, setPostDetails] = useState(null);
+  const [comments, setComments] = useState([]);
+  const [areCommentsVisible, setCommentsVisible] = useState(true);
 
-    <section className="PostDetails__post">
-      <p>sunt aut facere repellat provident occaecati excepturi optio</p>
-    </section>
+  useEffect(() => {
+    getPostDetails(selectedPostId).then(setPostDetails);
+  }, [selectedPostId]);
 
-    <section className="PostDetails__comments">
-      <button type="button" className="button">Hide 2 comments</button>
+  useEffect(() => {
+    getPostComments(selectedPostId).then(setComments);
+  }, [selectedPostId]);
 
-      <ul className="PostDetails__list">
-        <li className="PostDetails__list-item">
+  if (postDetails === null) {
+    return <Loader />;
+  }
+
+  const filteredComments = comments.filter(
+    comment => comment.postId === selectedPostId,
+  );
+
+  const addComment = (newComment) => {
+    setComments(prevComments => [...prevComments, newComment]);
+  };
+
+  const deleteComment = (commentId) => {
+    removeComment(commentId);
+    setComments(prevComments => prevComments.filter(
+      comment => comment.id !== commentId,
+    ));
+  };
+
+  return (
+    <div className="PostDetails">
+      <h2>Post details:</h2>
+
+      <section className="PostDetails__post">
+        <p>
+          Title:
+          {postDetails.title}
+        </p>
+        <p>
+          Created:
+          {postDetails.createdAt}
+        </p>
+        <p>
+          Last update:
+          {postDetails.updatedAt}
+        </p>
+      </section>
+
+      <section className="PostDetails__comments">
+
+        {areCommentsVisible ? (
+          <>
+            <button
+              type="button"
+              className="button"
+              onClick={() => setCommentsVisible(false)}
+            >
+              Hide
+              {' '}
+              {filteredComments.length}
+              {' '}
+              comments
+            </button>
+
+            <ul className="PostDetails__list">
+              {filteredComments.map(comment => (
+                <li key={comment.id} className="PostDetails__list-item">
+                  <button
+                    type="button"
+                    className="PostDetails__remove-button button"
+                    onClick={() => deleteComment(comment.id)}
+                  >
+                    X
+                  </button>
+                  <p>{comment.body}</p>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+
           <button
             type="button"
-            className="PostDetails__remove-button button"
+            className="button"
+            onClick={() => setCommentsVisible(true)}
           >
-            X
+            Show
+            {' '}
+            {filteredComments.length}
+            {' '}
+            comments
           </button>
-          <p>My first comment</p>
-        </li>
+        )}
+      </section>
 
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
-          >
-            X
-          </button>
-          <p>sad sds dfsadf asdf asdf</p>
-        </li>
-      </ul>
-    </section>
+      <section>
+        <div className="PostDetails__form-wrapper">
+          <NewCommentForm
+            selectedPostId={selectedPostId}
+            comments={filteredComments}
+            addComment={addComment}
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
 
-    <section>
-      <div className="PostDetails__form-wrapper">
-        <NewCommentForm />
-      </div>
-    </section>
-  </div>
-);
+PostDetails.propTypes = {
+  selectedPostId: PropTypes.number.isRequired,
+};
