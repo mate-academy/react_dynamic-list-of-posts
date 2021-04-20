7333e880f46f3c88dc45179a89b864ac81e21022
Olga Briukhanova <oaleksandrovva@gmail.com>
2021-04-20 17:09:08 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
+import { getUsers } from './api/users';
+// import { getUserPosts } from './api/posts';
 
-const App = () => (
-  <div className="App">
-    <header className="App__header">
-      <label>
-        Select a user: &nbsp;
+export const App = () => {
+  const [users, setUsers] = useState(null);
+  const [selectedUserId, setSelectedUserId] = useState('');
 
-        <select className="App__user-selector">
-          <option value="0">All users</option>
-          <option value="1">Leanne Graham</option>
-          <option value="2">Ervin Howell</option>
-          <option value="3">Clementine Bauch</option>
-          <option value="4">Patricia Lebsack</option>
-          <option value="5">Chelsey Dietrich</option>
-          <option value="6">Mrs. Dennis Schulist</option>
-          <option value="7">Kurtis Weissnat</option>
-          <option value="8">Nicholas Runolfsdottir V</option>
-          <option value="9">Glenna Reichert</option>
-          <option value="10">Leanne Graham</option>
-        </select>
-      </label>
-    </header>
+  useEffect(() => {
+    getUsers().then(setUsers);
+  }, []);
 
-    <main className="App__main">
-      <div className="App__sidebar">
-        <PostsList />
-      </div>
+  const handleChange = (clickEvent) => {
+    const { value } = clickEvent.target;
 
-      <div className="App__content">
-        <PostDetails />
-      </div>
-    </main>
-  </div>
-);
+    setSelectedUserId(value);
+  };
 
-export default App;
+  return (
+    <div className="App">
+      <header className="App__header">
+        {users ? (
+          <label>
+            Select a user: &nbsp;
+
+            <select
+              className="App__user-selector"
+              onChange={handleChange}
+            >
+              <option value="0">All users</option>
+              {users.map(({ name, id }) => (
+                <option value={id} key={id}>
+                  {name}
+                </option>
+              ))}
+            </select>
+          </label>
+        ) : (
+          <span>Users are not loaded</span>
+        )}
+      </header>
+
+      <main className="App__main">
+        <div className="App__sidebar">
+          <PostsList
+            selectedUserId={selectedUserId}
+          />
+        </div>
+
+        <div className="App__content">
+          <PostDetails />
+        </div>
+      </main>
+    </div>
+  );
+};
