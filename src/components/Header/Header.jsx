277118ccef7015c296831de44aef05c8e6bd1d04
Serhii Nikitin <serg.niki@i.ua>
2021-04-29 22:34:59 +0300

    Header.jsx and post.js were created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getUserPosts } from '../../api/posts';
+
+export const Header = ({ posts, userId, setUserId, setUserPosts }) => {
+  const handleSelect = (e) => {
+    if (e.target.value === '0') {
+      return setUserPosts(posts);
+    }
+
+    setUserId(+e.target.value);
+
+    return getUserPosts(e.target.value)
+      .then(userPosts => setUserPosts(userPosts));
+  };
+
+  return (
+    <header className="App__header">
+      <label>
+        Select a user: &nbsp;
+
+        <select
+          className="App__user-selector"
+          value={userId}
+          onChange={handleSelect}
+        >
+          <option value="0">All users</option>
+          <option value="1">Leanne Graham</option>
+          <option value="2">Ervin Howell</option>
+          <option value="3">Clementine Bauch</option>
+          <option value="4">Patricia Lebsack</option>
+          <option value="5">Chelsey Dietrich</option>
+          <option value="6">Mrs. Dennis Schulist</option>
+          <option value="7">Kurtis Weissnat</option>
+          <option value="8">Nicholas Runolfsdottir V</option>
+          <option value="9">Glenna Reichert</option>
+          <option value="10">Leanne Graham</option>
+        </select>
+      </label>
+    </header>
+  );
+};
+
+Header.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      userId: PropTypes.number.isRequired,
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string,
+      body: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  userId: PropTypes.number.isRequired,
+  setUserId: PropTypes.func.isRequired,
+  setUserPosts: PropTypes.func.isRequired,
+};
--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -1,37 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PostsList.scss';
 
-export const PostsList = () => (
+export const PostsList = ({ userPosts, setSelectedPostId, selectedPostId }) => (
   <div className="PostsList">
     <h2>Posts:</h2>
 
     <ul className="PostsList__list">
-      <li className="PostsList__item">
-        <div>
-          <b>[User #1]: </b>
-          sunt aut facere repellat provident occaecati excepturi optio
-        </div>
-        <button
-          type="button"
-          className="PostsList__button button"
+      {userPosts.map(post => (
+        <li
+          className="PostsList__item"
+          key={post.id}
         >
-          Close
-        </button>
-      </li>
-
-      <li className="PostsList__item">
-        <div>
-          <b>[User #2]: </b>
-          et ea vero quia laudantium autem
-        </div>
-
-        <button
-          type="button"
-          className="PostsList__button button"
-        >
-          Open
-        </button>
-      </li>
+          <div>
+            <b>{`[User ${post.userId}]: `}</b>
+            {post.title}
+          </div>
+          <button
+            type="button"
+            className="PostsList__button button"
+            onClick={() => {
+              setSelectedPostId(post.id);
+            }}
+          >
+            {selectedPostId === post.id
+              ? 'Open'
+              : 'Close'
+              }
+          </button>
+        </li>
+      ))}
     </ul>
   </div>
 );
