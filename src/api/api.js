800aec69b1e2a777d5a9960549508ce5276548fa
amithos <alex.theutos@gmail.com>
2021-06-14 00:50:26 +0300

    solution

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,23 @@
-export const BASE_URL = 'https://mate-api.herokuapp.com';
+const BASE_URL = 'https://mate-api.herokuapp.com/';
+
+export function request(endpoint, options) {
+  return fetch(`${BASE_URL}${endpoint}`, options)
+    .then((response) => {
+      if (!response.ok) {
+        return Promise.reject(new Error('Error: ', response.statusText));
+      }
+
+      return response.json();
+    })
+    .then(result => result.data);
+}
+
+export const remove = elementURL => request(elementURL, { method: 'DELETE' });
+
+export const post = (url, data) => request(url, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+  },
+  body: JSON.stringify(data),
+});
--- src/components/NewCommentForm/NewCommentForm.jsx
+++ src/components/NewCommentForm/NewCommentForm.jsx
@@ -1,39 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { addComment } from '../../api/comments';
 import './NewCommentForm.scss';
 
-export const NewCommentForm = () => (
-  <form className="NewCommentForm">
-    <div className="form-field">
-      <input
-        type="text"
-        name="name"
-        placeholder="Your name"
-        className="NewCommentForm__input"
-      />
-    </div>
+export const NewCommentForm = ({ selectedPostId, onRefreshDetails }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [body, setBody] = useState('');
 
-    <div className="form-field">
-      <input
-        type="text"
-        name="email"
-        placeholder="Your email"
-        className="NewCommentForm__input"
-      />
-    </div>
+  const resetForm = () => {
+    setName('');
+    setEmail('');
+    setBody('');
+  };
 
-    <div className="form-field">
-      <textarea
-        name="body"
-        placeholder="Type comment here"
-        className="NewCommentForm__input"
-      />
-    </div>
+  const setValue = (event, setter) => {
+    setter(event.target.value);
+  };
 
-    <button
-      type="submit"
-      className="NewCommentForm__submit-button button"
+  const submitComment = (event) => {
+    event.preventDefault();
+    addComment({
+      name,
+      email,
+      body,
+      postId: selectedPostId,
+    })
+      .then(() => onRefreshDetails());
+    resetForm();
+  };
+
+  return (
+    <form
+      className="NewCommentForm"
+      onSubmit={submitComment}
     >
-      Add a comment
-    </button>
-  </form>
-);
+      <div className="form-field">
+        <input
+          type="text"
+          name="name"
+          placeholder="Your name"
+          className="NewCommentForm__input"
+          value={name}
+          onChange={(event) => {
+            setValue(event, setName);
+          }}
+        />
+      </div>
+
+      <div className="form-field">
+        <input
+          type="text"
+          name="email"
+          placeholder="Your email"
+          className="NewCommentForm__input"
+          value={email}
+          onChange={(event) => {
+            setValue(event, setEmail);
+          }}
+        />
+      </div>
+
+      <div className="form-field">
+        <textarea
+          name="body"
+          placeholder="Type comment here"
+          className="NewCommentForm__input"
+          value={body}
+          onChange={(event) => {
+            setValue(event, setBody);
+          }}
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="NewCommentForm__submit-button button"
+      >
+        Add a comment
+      </button>
+    </form>
+  );
+};
+
+NewCommentForm.propTypes = {
+  selectedPostId: PropTypes.number.isRequired,
+  onRefreshDetails: PropTypes.func.isRequired,
+};
