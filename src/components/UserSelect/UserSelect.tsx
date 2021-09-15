a6e495933ad5cf6a9422300913149ae1f20337c1
lera-yaskevych <redrum.back@gmail.com>
2021-09-15 14:26:25 +0300

    added missing files

---
+++ src/components/UserSelect/UserSelect.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+type Props = {
+  setUser: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const UserSelect: React.FC<Props> = (props) => {
+  const { setUser } = props;
+
+  return (
+    <label>
+      Select a user: &nbsp;
+
+      <select
+        className="App__user-selector"
+        onChange={(event) => {
+          setUser(+event.target.value);
+        }}
+      >
+        <option value="0">All users</option>
+        <option value="1">Leanne Graham</option>
+        <option value="2">Ervin Howell</option>
+        <option value="3">Clementine Bauch</option>
+        <option value="4">Patricia Lebsack</option>
+        <option value="5">Chelsey Dietrich</option>
+        <option value="6">Mrs. Dennis Schulist</option>
+        <option value="7">Kurtis Weissnat</option>
+        <option value="8">Nicholas Runolfsdottir V</option>
+        <option value="9">Glenna Reichert</option>
+        <option value="10">Leanne Graham</option>
+      </select>
+    </label>
+  );
+};
