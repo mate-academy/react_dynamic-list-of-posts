8626dc7b1c79f37888942a7433336e8a16c3a79c
lera-yaskevych <redrum.back@gmail.com>
2021-09-15 13:58:11 +0300

    solved

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,12 @@
-export const BASE_URL = 'https://mate-api.herokuapp.com';
+export const BASE_URL = 'https://mate.academy/students-api';
+
+export const basicRequest = (url: string, option = {}) => {
+  return fetch(`${BASE_URL}${url}`, option)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+
+      return response.json();
+    });
+};
--- src/components/PostsList/PostsList.tsx
+++ src/components/PostsList/PostsList.tsx
@@ -1,37 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Loader } from '../Loader';
 import './PostsList.scss';
+import { getUserPosts } from '../../api/posts';
 
-export const PostsList: React.FC = () => (
-  <div className="PostsList">
-    <h2>Posts:</h2>
+type Props = {
+  selectedUserId: number;
+  setPost: React.Dispatch<React.SetStateAction<number>>;
+  selectedPostId: number;
+};
 
-    <ul className="PostsList__list">
-      <li className="PostsList__item">
-        <div>
-          <b>[User #1]: </b>
-          sunt aut facere repellat provident occaecati excepturi optio
-        </div>
-        <button
-          type="button"
-          className="PostsList__button button"
-        >
-          Close
-        </button>
-      </li>
+export const PostsList: React.FC<Props> = (props) => {
+  const { selectedUserId, setPost, selectedPostId } = props;
+  const [posts, setPosts] = useState([] as Post[]);
+  const [loading, setLoadingStatus] = useState(true);
 
-      <li className="PostsList__item">
-        <div>
-          <b>[User #2]: </b>
-          et ea vero quia laudantium autem
-        </div>
+  useEffect(() => {
+    getUserPosts(selectedUserId)
+      .then(response => {
+        setPosts(response);
+        setLoadingStatus(false);
+      });
+  }, [selectedUserId]);
 
-        <button
-          type="button"
-          className="PostsList__button button"
-        >
-          Open
-        </button>
-      </li>
-    </ul>
-  </div>
-);
+  const selectHandler = (id: number) => {
+    if (id === selectedPostId) {
+      setPost(0);
+    } else {
+      setPost(id);
+    }
+  };
+
+  return (
+    <div className="PostsList">
+      <h2>Posts:</h2>
+      {loading && (
+        <Loader />
+      )}
+      <ul className="PostsList__list">
+        {posts.map((post: Post) => (
+          <>
+            <li
+              className="PostsList__item"
+              key={post.id}
+            >
+              <div>
+                <b>{`[User #${post.userId}]: `}</b>
+                {post.title}
+              </div>
+              <button
+                type="button"
+                className="PostsList__button button"
+                onClick={() => selectHandler(post.id)}
+              >
+                {selectedPostId === post.id ? (
+                  'Close'
+                ) : (
+                  'Open'
+                )}
+              </button>
+            </li>
+          </>
+        ))}
+      </ul>
+    </div>
+  );
+};
