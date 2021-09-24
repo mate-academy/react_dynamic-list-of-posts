4bed148d8e5716f8d03b87575e1fc7e08ad07406
Kazkoviy <illyaboiko1807@gmail.com>
2021-09-24 15:35:26 +0300

    loader

--- src/components/PostsList/PostsList.tsx
+++ src/components/PostsList/PostsList.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import React from 'react';
+import { Loader } from '../Loader';
 import './PostsList.scss';
 
 type Props = {
@@ -14,34 +15,38 @@ export const PostsList: React.FC<Props> = (props) => {
   return (
     <div className="PostsList">
       <h2>Posts:</h2>
-      <ul className="PostsList__list">
-        {posts.map(post => (
-          <li
-            className="PostsList__item"
-            key={post.id}
-          >
-            <div>
-              <b>{`[User #${post.userId}]`}</b>
-              {post.title}
-            </div>
-            <button
-              type="button"
-              className={classNames(
-                {
-                  PostsList__button: true,
-                  button: true,
-                  'button--active': selectedPostId === post.id,
-                },
-              )}
-              onClick={() => onChangePostId(post.id)}
+      {posts.length > 0 ? (
+        <ul className="PostsList__list">
+          {posts.map(post => (
+            <li
+              className="PostsList__item"
+              key={post.id}
             >
-              {selectedPostId === post.id
-                ? 'Close'
-                : 'Open'}
-            </button>
-          </li>
-        ))}
-      </ul>
+              <div>
+                <b>{`[User #${post.userId}]`}</b>
+                {post.title}
+              </div>
+              <button
+                type="button"
+                className={classNames(
+                  {
+                    PostsList__button: true,
+                    button: true,
+                    'button--active': selectedPostId === post.id,
+                  },
+                )}
+                onClick={() => onChangePostId(post.id)}
+              >
+                {selectedPostId === post.id
+                  ? 'Close'
+                  : 'Open'}
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 };
