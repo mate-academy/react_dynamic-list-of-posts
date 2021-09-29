bd293f6d9dda8f93dd69a39cfd1ee42a3e946af2
Alexandr Somov <aleksandr.somov2011@gmail.com>
2021-09-29 21:00:04 +0300

    solution

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,47 @@
-export const BASE_URL = 'https://mate-api.herokuapp.com';
+export const BASE_URL = 'https://mate.academy/students-api';
+
+export const getPosts = async () => {
+  const res = await fetch(`${BASE_URL}/posts`);
+
+  return res.json();
+};
+
+export const getUserPosts = async (userId: number) => {
+  const res = await fetch(`${BASE_URL}/posts?userId=${userId}`);
+
+  return res.json();
+};
+
+export const getUser = async () => {
+  const res = await fetch(`${BASE_URL}/users`);
+
+  return res.json();
+};
+
+export const getPostDetails = async (postId: number) => {
+  const res = await fetch(`${BASE_URL}/posts/${postId}`);
+
+  return res.json();
+};
+
+export const getPostComments = async (postId: number) => {
+  const res = await fetch(`${BASE_URL}/comments?postId=${postId}`);
+
+  return res.json();
+};
+
+export const deletePostComments = (commentId: number) => {
+  return fetch(`${BASE_URL}/comments/${commentId}`, {
+    method: 'DELETE',
+  });
+};
+
+export const addCommentPost = async (newComment: Comment) => {
+  return fetch(`${BASE_URL}/comments`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(newComment),
+  });
+};
--- src/components/PostDetails/PostDetails.tsx
+++ src/components/PostDetails/PostDetails.tsx
@@ -1,45 +1,98 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NewCommentForm } from '../NewCommentForm';
 import './PostDetails.scss';
+import {
+  addCommentPost, deletePostComments, getPostComments, getPostDetails,
+} from '../../api/api';
 
-export const PostDetails: React.FC = () => (
-  <div className="PostDetails">
-    <h2>Post details:</h2>
+type Props = {
+  selectedPostId: number,
+};
 
-    <section className="PostDetails__post">
-      <p>sunt aut facere repellat provident occaecati excepturi optio</p>
-    </section>
+export const PostDetails: React.FC<Props> = (props) => {
+  const { selectedPostId } = props;
+  const [postDetails, setPostDetails] = useState<Post | null>(null);
+  const [postComments, setPostComments] = useState<Comment[]>([]);
+  const [isShowComments, setIsShowComments] = useState(true);
 
-    <section className="PostDetails__comments">
-      <button type="button" className="button">Hide 2 comments</button>
+  useEffect(() => {
+    (async () => {
+      const getPostDetailsFromApi = await getPostDetails(selectedPostId);
 
-      <ul className="PostDetails__list">
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
-          >
-            X
-          </button>
-          <p>My first comment</p>
-        </li>
+      setPostDetails(getPostDetailsFromApi);
+    })();
+  }, [postDetails]);
 
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
-          >
-            X
-          </button>
-          <p>sad sds dfsadf asdf asdf</p>
-        </li>
-      </ul>
-    </section>
+  useEffect(() => {
+    (async () => {
+      const getCommentsFromApi = await getPostComments(selectedPostId);
 
-    <section>
-      <div className="PostDetails__form-wrapper">
-        <NewCommentForm />
-      </div>
-    </section>
-  </div>
-);
+      setPostComments(getCommentsFromApi);
+    })();
+  }, [postComments]);
+
+  const addComment = async (newComment: Comment) => {
+    await addCommentPost(newComment);
+    const updateComments = await getPostComments(selectedPostId);
+
+    setPostComments(updateComments);
+  };
+
+  const removeComments = async (commentId: number) => {
+    await deletePostComments(commentId);
+    const updateComments = await getPostComments(selectedPostId);
+
+    setPostComments(updateComments);
+  };
+
+  return (
+    <div className="PostDetails">
+      <h2>Post details:</h2>
+
+      <section className="PostDetails__post">
+        <p>{postDetails?.body}</p>
+      </section>
+
+      <section className="PostDetails__comments">
+        <button
+          type="button"
+          className="button"
+          onClick={() => setIsShowComments(current => !current)}
+        >
+          {isShowComments ? 'Hide' : 'Show'}
+          {' '}
+          {postComments.length}
+          {' '}
+          comments
+        </button>
+
+        {isShowComments && (
+          <ul className="PostDetails__list">
+            {postComments.map(comment => (
+              <li className="PostDetails__list-item" key={comment.id}>
+                <button
+                  type="button"
+                  className="PostDetails__remove-button button"
+                  onClick={() => removeComments(comment.id)}
+                >
+                  X
+                </button>
+                <p>{comment.body}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+
+      </section>
+
+      <section>
+        <div className="PostDetails__form-wrapper">
+          <NewCommentForm
+            addComment={addComment}
+            postId={selectedPostId}
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
