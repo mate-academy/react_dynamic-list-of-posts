f3a015b992a15a5dcdf79047d8e21f4bf482737c
bohdan_kochetov <bodja1208@gmail.com>
2021-10-25 18:29:04 +0300

    solution

---
+++ src/api/posts.ts
@@ -0,0 +1,20 @@
+export async function getUserPosts(userId = 0): Promise<Post[]> {
+  let postsURL = 'https://mate.academy/students-api/posts';
+
+  if (userId !== 0) {
+    postsURL += `?userId=${userId}`;
+  }
+
+  return fetch(postsURL)
+    .then(response => response.json());
+}
+
+export async function getPostDetails(postId: number): Promise<Post> {
+  return fetch(`https://mate.academy/students-api/posts/${postId}`)
+    .then(response => response.json());
+}
+
+export async function getPostComments(postId: number): Promise<Comment[]> {
+  return fetch(`https://mate.academy/students-api/comments?postId=${postId}`)
+    .then(response => response.json());
+}
