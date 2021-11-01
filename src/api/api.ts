7aaf4e4607fbae7adf34d77ee17a9e4a20dc823e
Ruslan-Mykhalchak <ruslan1993x@gmail.com>
2021-11-01 15:16:09 +0200

    solution

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,7 @@
 export const BASE_URL = 'https://mate.academy/students-api';
+
+export const request = async (url:string, item = {}) => {
+  const response = await fetch(`${BASE_URL}${url}`, item);
+
+  return response.json();
+};
