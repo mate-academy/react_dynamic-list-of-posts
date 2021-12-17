f0683b6f497b68906cbfc368a8fe2d0191dccb54
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-12-17 12:28:01 +0200

    Task finally done

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
+import { getUserPosts } from './api/posts';
+import { getUsers } from './api/users';
+import { Post, User } from './types/types';
 
-const App: React.FC = () => (
-  <div className="App">
-    <header className="App__header">
-      <label>
-        Select a user: &nbsp;
+export const App: React.FC = () => {
+  const [posts, setPosts] = useState([] as Post[]);
+  const [selectedPostId, setPostId] = useState(0);
+  const [users, setUsers] = useState([] as User[]);
+  const [userId, setUserId] = useState(0);
 
-        <select className="App__user-selector">
-          <option value="0">All users</option>
-          <option value="1">Leanne Graham</option>
-          <option value="2">Ervin Howell</option>
-          <option value="3">Clementine Bauch</option>
-          <option value="4">Patricia Lebsack</option>
-          <option value="5">Chelsey Dietrich</option>
-          <option value="6">Mrs. Dennis Schulist</option>
-          <option value="7">Kurtis Weissnat</option>
-          <option value="8">Nicholas Runolfsdottir V</option>
-          <option value="9">Glenna Reichert</option>
-          <option value="10">Leanne Graham</option>
-        </select>
-      </label>
-    </header>
+  useEffect(() => {
+    getUserPosts(userId)
+      .then(result => setPosts(result));
+  }, [userId]);
 
-    <main className="App__main">
-      <div className="App__sidebar">
-        <PostsList />
-      </div>
+  useEffect(() => {
+    getUsers()
+      .then(result => setUsers(result));
+  }, []);
 
-      <div className="App__content">
-        <PostDetails />
-      </div>
-    </main>
-  </div>
-);
+  const selectPost = (postId: number) => {
+    if (postId === selectedPostId) {
+      setPostId(0);
+    } else {
+      setPostId(postId);
+    }
+  };
 
-export default App;
+  return (
+    <div className="App">
+      <header className="App__header">
+        <label htmlFor="select">
+          Select a user: &nbsp;
+
+          <select
+            className="App__user-selector"
+            onChange={(event) => {
+              setUserId(+event.target.value);
+            }}
+          >
+            <option value="0">All users</option>
+            {users.map(user => (
+              <option
+                key={user.id}
+                value={user.id}
+              >
+                {user.name}
+              </option>
+            ))}
+          </select>
+        </label>
+      </header>
+
+      <main className="App__main">
+        <div className="App__sidebar">
+          <PostsList
+            posts={posts}
+            selectPost={selectPost}
+            selectedId={selectedPostId}
+          />
+        </div>
+
+        {selectedPostId !== 0 && (
+          <div className="App__content">
+            <PostDetails postId={selectedPostId} />
+          </div>
+        )}
+      </main>
+    </div>
+  );
+};
--- src/components/PostDetails/PostDetails.tsx
+++ src/components/PostDetails/PostDetails.tsx
@@ -1,45 +1,106 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NewCommentForm } from '../NewCommentForm';
 import './PostDetails.scss';
+import { getPostDetails } from '../../api/posts';
+import { addComment, deleteComment, getPostComments } from '../../api/comments';
+import { Post, Comment } from '../../types/types';
 
-export const PostDetails: React.FC = () => (
-  <div className="PostDetails">
-    <h2>Post details:</h2>
+type Props = {
+  postId: number,
+};
 
-    <section className="PostDetails__post">
-      <p>sunt aut facere repellat provident occaecati excepturi optio</p>
-    </section>
+export const PostDetails: React.FC<Props> = ({ postId }) => {
+  const [postInfo, setPostInfo] = useState<Post>({} as Post);
+  const [comments, setComments] = useState<Comment[]>([] as Comment[]);
+  const [commentVisibility, setCommentVisibility] = useState(true);
 
-    <section className="PostDetails__comments">
-      <button type="button" className="button">Hide 2 comments</button>
+  useEffect(() => {
+    getPostDetails(postId)
+      .then(result => setPostInfo(result));
+  }, [postId]);
 
-      <ul className="PostDetails__list">
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
-          >
-            X
-          </button>
-          <p>My first comment</p>
-        </li>
+  useEffect(() => {
+    getPostComments(postId)
+      .then(result => setComments([...result]));
+  }, [postId]);
 
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
-          >
-            X
-          </button>
-          <p>sad sds dfsadf asdf asdf</p>
-        </li>
-      </ul>
-    </section>
+  const commentDisplay = () => {
+    return commentVisibility
+      ? setCommentVisibility(false)
+      : setCommentVisibility(true);
+  };
 
-    <section>
-      <div className="PostDetails__form-wrapper">
-        <NewCommentForm />
-      </div>
-    </section>
-  </div>
-);
+  const removeComment = (commentId: number) => {
+    deleteComment(commentId)
+      .then(() => getPostComments(postId)
+        .then(result => setComments(result)));
+  };
+
+  const addNewComment = (newComment: Comment) => {
+    addComment(newComment)
+      .then(() => getPostComments(postId)
+        .then(result => setComments(result)));
+  };
+
+  return (
+    <div className="PostDetails">
+      <h2>Post details:</h2>
+      <section className="PostDetails__post">
+        <p>
+          {postInfo.body}
+        </p>
+      </section>
+
+      <section className="PostDetails__comments">
+        {comments.length > 0
+          ? (
+            <button
+              type="button"
+              className="button"
+              onClick={commentDisplay}
+            >
+              {commentVisibility ? 'Hide' : 'Show'}
+              {` ${comments.length} `}
+              {comments.length === 1 ? 'comment' : 'comments'}
+            </button>
+          ) : (
+            <button
+              type="button"
+              className="button--no_comments"
+              disabled
+            >
+              No comments yet
+            </button>
+          )}
+
+        <ul className="PostDetails__list">
+          {commentVisibility && (
+            comments.map(comment => (
+              <li
+                key={comment.id}
+                className="PostDetails__list-item"
+              >
+                <button
+                  type="button"
+                  className="PostDetails__remove-button button"
+                  onClick={() => removeComment(comment.id)}
+                >
+                  X
+                </button>
+                <p>
+                  {comment.body}
+                </p>
+              </li>
+            ))
+          )}
+        </ul>
+      </section>
+
+      <section>
+        <div className="PostDetails__form-wrapper">
+          <NewCommentForm postId={postId} addNewComment={addNewComment} />
+        </div>
+      </section>
+    </div>
+  );
+};
