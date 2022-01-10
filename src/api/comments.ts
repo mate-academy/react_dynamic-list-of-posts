d5e90d819823d1547c77c43dae578202feb97a90
Andriy Bulbuk <bulbuk.andriy@gmail.com>
2022-01-10 16:24:08 +0200

    Dynamic-list-of-posts v1.0 added api file

---
+++ src/api/comments.ts
@@ -0,0 +1,38 @@
+import { BASE_URL, getData } from './api';
+import { Comment } from '../react-app-env';
+
+export const getPostComments = (postId: number) => {
+  const commentsUrl = `${BASE_URL}/comments?postId=${postId}`;
+
+  return getData(commentsUrl);
+};
+
+export const deleteComment = async (commentId: number) => {
+  const commentUrl = `${BASE_URL}/comments/${commentId}`;
+
+  const response = await fetch(commentUrl, { method: 'DELETE' });
+
+  if (!response.ok) {
+    return Promise.reject(new Error(`Status code - ${response.status}`));
+  }
+
+  return response.json();
+};
+
+export const postComment = async (comment: Omit<Comment, 'id'>) => {
+  const commentsUrl = 'https://mate.academy/students-api/comments';
+
+  const response = await fetch(commentsUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(comment),
+  });
+
+  if (!response.ok) {
+    return Promise.reject(new Error(`Status code - ${response.status}`));
+  }
+
+  return response.json();
+};
