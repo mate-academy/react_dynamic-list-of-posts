d19c3e8639714dc4f1e9395c1e6562c4002d5ee9
Nikolas <tes.kolia@gmail.com>
2022-02-04 22:34:17 +0200

    added to commit

---
+++ src/components/PostDetails/PostDetailsUi.tsx
@@ -0,0 +1,81 @@
+import classNames from 'classnames';
+import { Loader } from '../Loader';
+import { NewCommentForm } from '../NewCommentForm';
+
+type Props = {
+  postDetails: Post,
+  isHidden: boolean,
+  comments: Comment[],
+  initialize: boolean,
+  selectedPostId: number,
+  handleAdd: (comment: NewComment) => void,
+  handleDelete: (commentId: number) => void,
+  handleVisabiliti: () => void,
+};
+
+export const PostDetailsUi: React.FC<Props> = ({
+  handleAdd,
+  handleDelete,
+  handleVisabiliti,
+  selectedPostId,
+  postDetails,
+  initialize,
+  isHidden,
+  comments,
+}) => (
+  <div className="PostDetails">
+    {selectedPostId !== 0
+      ? (
+        <>
+          <h2>Post details:</h2>
+          {initialize
+            ? <Loader />
+            : (
+              <>
+                <section className="PostDetails__post">
+                  <p>{postDetails.title}</p>
+                </section>
+                <section className="PostDetails__comments">
+                  <button
+                    type="button"
+                    className="button"
+                    onClick={handleVisabiliti}
+                  >
+                    {`Hide ${comments.length} comments`}
+                  </button>
+
+                  <ul className="PostDetails__list">
+                    {comments.map(comment => (
+                      <li
+                        key={comment.id}
+                        className={classNames(
+                          'PostDetails__list-item',
+                          {
+                            hide: isHidden,
+                          },
+                        )}
+                      >
+                        <button
+                          type="button"
+                          className="PostDetails__remove-button button"
+                          onClick={() => handleDelete(comment.id)}
+                        >
+                          X
+                        </button>
+                        <p>{comment.body}</p>
+                      </li>
+                    ))}
+                  </ul>
+                </section>
+              </>
+            )}
+          <section>
+            <div className="PostDetails__form-wrapper">
+              <NewCommentForm handleAdd={handleAdd} selectedPostId={selectedPostId} />
+            </div>
+          </section>
+        </>
+      )
+      : 'No post selected'}
+  </div>
+);
---
+++ src/components/PostsList/PostListUi.tsx
@@ -0,0 +1,40 @@
+import { Loader } from '../Loader';
+
+type Props = {
+  posts: Post[]
+  initialize:boolean
+  selectedPostId: number,
+  setSelectePostId: (postId: number) => void
+};
+
+export const PostsListUi: React.FC<Props> = ({
+  posts,
+  initialize,
+  selectedPostId,
+  setSelectePostId,
+}) => (
+  <div className="PostsList">
+    <h2>Posts:</h2>
+    <div>{!initialize ? <Loader /> : null}</div>
+    <ul className="PostsList__list">
+      {posts.map(post => (
+        <li className="PostsList__item" key={post.id}>
+          <div>
+            <b>
+              {`User #${post.userId}: `}
+            </b>
+            sunt aut facere repellat provident occaecati excepturi optio
+          </div>
+
+          <button
+            type="button"
+            className="PostsList__button button"
+            onClick={() => setSelectePostId(post.id)}
+          >
+            {selectedPostId === post.id ? 'Close' : 'Open'}
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
