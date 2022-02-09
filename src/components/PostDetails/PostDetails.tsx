dfe87d231fd438096c2ee956d8edb94466a79a9f
Marina Tilniak <marinatilnak@gmail.com>
2022-02-09 22:03:06 +0200

    Solution

--- src/components/PostDetails/PostDetails.tsx
+++ src/components/PostDetails/PostDetails.tsx
@@ -1,45 +1,107 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { getPostsDetails } from '../../api/posts';
+import { getPostComments, deleteComment } from '../../api/comments';
 import { NewCommentForm } from '../NewCommentForm';
 import './PostDetails.scss';
 
-export const PostDetails: React.FC = () => (
-  <div className="PostDetails">
-    <h2>Post details:</h2>
+type Props = {
+  selectedPostId: number,
+};
 
-    <section className="PostDetails__post">
-      <p>sunt aut facere repellat provident occaecati excepturi optio</p>
-    </section>
+export const PostDetails: React.FC<Props> = ({ selectedPostId }) => {
+  const [post, setPost] = useState<Post | null>(null);
+  const [comments, setComments] = useState<PostComment[]>([]);
+  const [isHideComments, setIsHideComments] = useState(false);
 
-    <section className="PostDetails__comments">
-      <button type="button" className="button">Hide 2 comments</button>
+  const loadComments = async () => {
+    const postComments = await getPostComments(selectedPostId);
 
-      <ul className="PostDetails__list">
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
-          >
-            X
-          </button>
-          <p>My first comment</p>
-        </li>
+    setComments(postComments);
+  };
 
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
-          >
-            X
-          </button>
-          <p>sad sds dfsadf asdf asdf</p>
-        </li>
-      </ul>
-    </section>
+  useEffect(
+    () => {
+      const getSelectedPost = async () => {
+        const postDetails = await getPostsDetails(selectedPostId);
 
-    <section>
-      <div className="PostDetails__form-wrapper">
-        <NewCommentForm />
-      </div>
-    </section>
-  </div>
-);
+        setPost(postDetails);
+      };
+
+      if (selectedPostId) {
+        getSelectedPost();
+        loadComments();
+      }
+    },
+    [selectedPostId],
+  );
+
+  const handleTogglerComments = () => {
+    setIsHideComments(!isHideComments);
+  };
+
+  const deleteSelectedComment = async (commentId: number) => {
+    await deleteComment(commentId);
+    loadComments();
+  };
+
+  return (
+    <div className="PostDetails">
+      <h2>Post details:</h2>
+
+      <section className="PostDetails__post">
+        <p>{post?.title}</p>
+      </section>
+
+      <section className="PostDetails__comments">
+        {isHideComments
+          ? (
+            <button
+              type="button"
+              className="button PostDetails__show-button"
+              onClick={handleTogglerComments}
+            >
+              {`Show ${comments.length === 1
+                ? '1 comment'
+                : `${comments.length} comments`
+              }`}
+            </button>
+          )
+          : (
+            <>
+              <button
+                type="button"
+                className="button"
+                onClick={handleTogglerComments}
+              >
+                {`Hide ${comments.length === 1
+                  ? '1 comment'
+                  : `${comments.length} comments`
+                }`}
+              </button>
+
+              <ul className="PostDetails__list">
+                {comments.map(comment => (
+                  <li className="PostDetails__list-item">
+                    <button
+                      type="button"
+                      className="PostDetails__remove-button button"
+                      onClick={() => deleteSelectedComment(comment.id)}
+                    >
+                      X
+                    </button>
+                    <p>{comment.body}</p>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+      </section>
+
+      <section>
+        <div className="PostDetails__form-wrapper">
+          <NewCommentForm selectedPostId={selectedPostId} loadComments={loadComments} />
+        </div>
+      </section>
+    </div>
+  );
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,19 @@
-/// <reference types="react-scripts" />
+/// <reference types="react-scripts" /
+interface Post {
+  id: number,
+  userId: number,
+  completed: boolean,
+  title: string,
+  createdAT: string,
+  updatedAt: string,
+}
+
+interface PostComment {
+  id: number,
+  postId: number,
+  name: string,
+  email: string,
+  body: string,
+  createdAT: string,
+  updatedAt: string,
+}
