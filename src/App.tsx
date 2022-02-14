ee242a61b4345abaaa5dac172ca75805d632528e
Serhii Haidukov <sergeyglazgo@gmail.com>
2022-02-14 16:22:49 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -63,14 +63,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-/* <option value="1">Leanne Graham</option>
-            <option value="2">Ervin Howell</option>
-            <option value="3">Clementine Bauch</option>
-            <option value="4">Patricia Lebsack</option>
-            <option value="5">Chelsey Dietrich</option>
-            <option value="6">Mrs. Dennis Schulist</option>
-            <option value="7">Kurtis Weissnat</option>
-            <option value="8">Nicholas Runolfsdottir V</option>
-            <option value="9">Glenna Reichert</option>
-            <option value="10">Leanne Graham</option> */
