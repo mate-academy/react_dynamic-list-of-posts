8185bf1ef8061ad5170c7a743eb2b1de9bb23f5e
Fiann(Larin) <dimiklarin420@gmail.com>
2022-03-21 14:13:23 +0200

    solution

---
+++ src/api/posts.ts
@@ -0,0 +1,44 @@
+import { BASE_URL } from './api';
+
+export const getPosts = () => {
+  return fetch(`${BASE_URL}/posts`)
+    .then(response => response.json());
+};
+
+export const getUserPosts = (userId: number) => {
+  return fetch(`${BASE_URL}/posts?userId=${userId}`)
+    .then(response => response.json());
+};
+
+export const getUser = () => {
+  return fetch(`${BASE_URL}/users`)
+    .then(response => response.json());
+};
+
+export const getPostDetails = (postId: number) => {
+  return fetch(`${BASE_URL}/posts/${postId}`)
+    .then(response => response.json());
+};
+
+export const getPostComments = (postId: number) => {
+  return fetch(`${BASE_URL}/comments?postId=${postId}`)
+    .then(response => response.json());
+};
+
+export const addComment = (newComment: Partial<Comment>) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(newComment),
+  };
+
+  return fetch(`${BASE_URL}/comments`, options)
+    .then(response => response.json());
+};
+
+export const deleteComment = (commentId: number): Promise<Comment> => {
+  return fetch(`${BASE_URL}/comments/${commentId}`, { method: 'DELETE' })
+    .then(response => response.json());
+};
--- src/components/NewCommentForm/NewCommentForm.tsx
+++ src/components/NewCommentForm/NewCommentForm.tsx
@@ -1,39 +1,76 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import './NewCommentForm.scss';
 
-export const NewCommentForm: React.FC = () => (
-  <form className="NewCommentForm">
-    <div className="form-field">
-      <input
-        type="text"
-        name="name"
-        placeholder="Your name"
-        className="NewCommentForm__input"
-      />
-    </div>
+type Props = {
+  selectedPostId: number;
+  addNewComment: (newComment: Partial<Comment>) => void;
+};
 
-    <div className="form-field">
-      <input
-        type="text"
-        name="email"
-        placeholder="Your email"
-        className="NewCommentForm__input"
-      />
-    </div>
+export const NewCommentForm: React.FC<Props> = (props) => {
+  const { selectedPostId, addNewComment } = props;
 
-    <div className="form-field">
-      <textarea
-        name="body"
-        placeholder="Type comment here"
-        className="NewCommentForm__input"
-      />
-    </div>
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [body, setBody] = useState('');
 
-    <button
-      type="submit"
-      className="NewCommentForm__submit-button button"
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    addNewComment({
+      postId: selectedPostId,
+      name,
+      email,
+      body,
+    });
+
+    setName('');
+    setEmail('');
+    setBody('');
+  };
+
+  return (
+    <form
+      className="NewCommentForm"
+      onSubmit={handleSubmit}
     >
-      Add a comment
-    </button>
-  </form>
-);
+      <div className="form-field">
+        <input
+          type="text"
+          name="name"
+          placeholder="Your name"
+          className="NewCommentForm__input"
+          value={name}
+          onChange={(event) => setName(event.currentTarget.value)}
+        />
+      </div>
+
+      <div className="form-field">
+        <input
+          type="text"
+          name="email"
+          placeholder="Your email"
+          className="NewCommentForm__input"
+          value={email}
+          onChange={(event) => setEmail(event.currentTarget.value)}
+        />
+      </div>
+
+      <div className="form-field">
+        <textarea
+          name="body"
+          placeholder="Type comment here"
+          className="NewCommentForm__input"
+          value={body}
+          onChange={(event) => setBody(event.currentTarget.value)}
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="NewCommentForm__submit-button button"
+      >
+        Add a comment
+      </button>
+    </form>
+  );
+};
