1dfcf425881c730b5de63549be2a3a0ae2bebe9f
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-04-06 19:16:55 +0300

    solution

--- src/components/PostsList/PostsList.tsx
+++ src/components/PostsList/PostsList.tsx
@@ -1,37 +1,51 @@
 import React from 'react';
+import { Post } from '../../types/Post';
 import './PostsList.scss';
 
-export const PostsList: React.FC = () => (
+export const PostsList: React.FC<Props> = ({
+  posts,
+  selectedPost,
+  selectPost,
+}) => (
   <div className="PostsList">
     <h2>Posts:</h2>
 
     <ul className="PostsList__list">
-      <li className="PostsList__item">
-        <div>
-          <b>[User #1]: </b>
-          sunt aut facere repellat provident occaecati excepturi optio
-        </div>
-        <button
-          type="button"
-          className="PostsList__button button"
-        >
-          Close
-        </button>
-      </li>
-
-      <li className="PostsList__item">
-        <div>
-          <b>[User #2]: </b>
-          et ea vero quia laudantium autem
-        </div>
-
-        <button
-          type="button"
-          className="PostsList__button button"
-        >
-          Open
-        </button>
-      </li>
+      {posts.map(post => (
+        <li key={post.id} className="PostsList__item">
+          <div>
+            <b>{`[User #${post.userId}:]`}</b>
+            {post.title}
+          </div>
+          {selectedPost === post.id
+            ? (
+              <button
+                type="button"
+                className="PostList__button button"
+                onClick={() => selectPost(0)}
+              >
+                close
+              </button>
+            )
+            : (
+              <button
+                type="button"
+                className="PostList__button button"
+                onClick={() => {
+                  selectPost(post.id);
+                }}
+              >
+                open
+              </button>
+            )}
+        </li>
+      ))}
     </ul>
   </div>
 );
+
+interface Props {
+  posts: Post[],
+  selectedPost: number,
+  selectPost: (postId: number) => void,
+}
