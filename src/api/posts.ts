3a74bb053dccf6db1bec7be9f5b67ae2b80803be
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-04-06 19:17:14 +0300

    first solution

---
+++ src/api/posts.ts
@@ -0,0 +1,32 @@
+import { Comment } from '../types/Comment';
+import { Post } from '../types/Post';
+import { BASE_URL } from './api';
+
+export const getUserPosts = (userId:number):Promise<Post[]> => {
+  if (!userId) {
+    return fetch(`${BASE_URL}/posts`).then(response => response.json());
+  }
+
+  return fetch(`${BASE_URL}/posts?userId=${userId}`).then(response => response.json());
+};
+
+export const getPostDetails = (postId:number):Promise<Post> => {
+  return fetch(`${BASE_URL}/posts/:${postId}`).then(response => response.json());
+};
+
+export const getPostComments = (postId: number): Promise<Comment[]> => {
+  return fetch(`${BASE_URL}/comments?postId=${postId}`).then(response => response.json());
+};
+
+export const deleteComment = (commentId:number) => {
+  return fetch(`${BASE_URL}/comments/${commentId}`, {
+    method: 'DELETE',
+  });
+};
+
+export const createComment = (newComment:Comment) => {
+  return fetch(`${BASE_URL}/comments`, {
+    method: 'POST',
+    body: JSON.stringify(newComment),
+  });
+};
