4e8df8ec9109b748a28000ad387a9c5e97b29cb3
Roman Pavlenko <proman9973@gmail.com>
2022-06-21 11:00:52 +0300

    solution fixes

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -11,3 +11,9 @@ export async function getUserPosts(userId : string) {
 
   return result;
 }
+
+export async function getPostDetails(postId : number) {
+  const result = await fetch(`${BASE_URL}/posts/:${postId}`);
+
+  return result;
+}
--- src/components/PostDetails/PostDetails.tsx
+++ src/components/PostDetails/PostDetails.tsx
@@ -64,7 +64,7 @@ export const PostDetails: React.FC<Props> = ({ post }) => {
                 >
                   X
                 </button>
-                <p>{post.body}</p>
+                <p>{singleComment.body}</p>
               </li>
             )))}
           </ul>
