6aeb5cdbd77aab2050eb8909c8a1b3ebf71ed197
Yaroslav Kononenko <konoyaroslav@gmail.com>
2022-06-22 08:15:13 +0300

    version_1

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.scss';
 import './styles/general.scss';
 import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
+import { API } from './api/api';
+import { Loader } from './components/Loader';
+import { Post } from './react-app-env';
 
-const App: React.FC = () => (
-  <div className="App">
-    <header className="App__header">
-      <label>
-        Select a user: &nbsp;
+const App: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [selectedUser, setSelectedUser] = useState(0);
+  const [selectedPostId, setSelectedPostId] = useState(0);
+  const [isLoading, seIstLoading] = useState(false);
 
-        <select className="App__user-selector">
-          <option value="0">All users</option>
-          <option value="1">Leanne Graham</option>
-          <option value="2">Ervin Howell</option>
-          <option value="3">Clementine Bauch</option>
-          <option value="4">Patricia Lebsack</option>
-          <option value="5">Chelsey Dietrich</option>
-          <option value="6">Mrs. Dennis Schulist</option>
-          <option value="7">Kurtis Weissnat</option>
-          <option value="8">Nicholas Runolfsdottir V</option>
-          <option value="9">Glenna Reichert</option>
-          <option value="10">Leanne Graham</option>
-        </select>
-      </label>
-    </header>
+  useEffect(() => {
+    seIstLoading(true);
 
-    <main className="App__main">
-      <div className="App__sidebar">
-        <PostsList />
-      </div>
+    if (selectedUser) {
+      API.getUserPosts(selectedUser)
+        .then(data => {
+          setPosts(data);
+          seIstLoading(false);
+        });
+    } else {
+      API.getPosts()
+        .then(data => {
+          setPosts(data);
+          seIstLoading(false);
+        });
+    }
+  }, [selectedUser]);
 
-      <div className="App__content">
-        <PostDetails />
-      </div>
-    </main>
-  </div>
-);
+  const onSelectedPost = useCallback((selectedId: number) => {
+    const id = selectedId === selectedPostId ? 0 : selectedId;
+
+    setSelectedPostId(id);
+  }, []);
+
+  return (
+    <div className="App">
+
+      <header className="App__header">
+        <label htmlFor="users">
+          Select a user: &nbsp;
+
+          <select
+            className="App__user-selector"
+            id="users"
+            value={selectedUser}
+            onChange={(e) => setSelectedUser(+e.target.value)}
+          >
+            <option value="0">All users</option>
+            <option value="1">Leanne Graham</option>
+            <option value="2">Ervin Howell</option>
+            <option value="3">Clementine Bauch</option>
+            <option value="4">Patricia Lebsack</option>
+            <option value="5">Chelsey Dietrich</option>
+            <option value="6">Mrs. Dennis Schulist</option>
+            <option value="7">Kurtis Weissnat</option>
+            <option value="8">Nicholas Runolfsdottir V</option>
+            <option value="9">Glenna Reichert</option>
+            <option value="10">Leanne Graham</option>
+          </select>
+        </label>
+      </header>
+
+      <main className="App__main">
+        <div className="App__sidebar">
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <PostsList
+              posts={posts}
+              selectedPostId={selectedPostId}
+              onSelectedPost={onSelectedPost}
+            />
+          )}
+        </div>
+
+        <div className="App__content">
+          {
+            selectedPostId
+              ? (
+                <PostDetails selectedPostId={selectedPostId} />
+              ) : (
+                <h2>Post not selected</h2>
+              )
+          }
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default App;
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,47 @@
+import { Comment } from '../react-app-env';
+
 export const BASE_URL = 'https://mate.academy/students-api';
+
+type Params = {
+  method: string,
+  headers?: {
+    'Content-Type': string,
+  },
+  body?: string,
+};
+
+const request = async (endPoint = '', params: Params = { method: 'GET' }) => {
+  try {
+    const response = await fetch(`${BASE_URL}${endPoint}`, params);
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error('Error with request');
+    }
+
+    return data;
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+
+    return null;
+  }
+};
+
+export const API = {
+  getPosts: () => request('/posts'),
+  getUserPosts: (userId: number) => request(`/posts?userId=${userId}`),
+  getPostDetails: (postId: number) => request(`/posts/${postId}`),
+  getPostComments: (postId: number) => {
+    return request('/comments')
+      .then(data => data.filter((el: Comment) => el.postId === postId));
+  },
+  deleteComments: (id: number) => request(`/comments/${id}`, { method: 'DELETE' }),
+  addComment: (comment: Partial<Comment>) => request('/comments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(comment),
+  }),
+};
