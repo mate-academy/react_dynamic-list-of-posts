51407eb98f6adbbe34757c2af46cc6f4f80ef9f8
RomekIvantsiv <ivantsiv.romek@gmail.com>
2022-06-22 16:59:33 +0200

    First try

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,7 @@
 export const BASE_URL = 'https://mate.academy/students-api';
+
+export const getUsers = () => {
+  return fetch(`${BASE_URL}/users/`)
+    .then(response => response.json())
+    .then(result => result.splice(0, 10));
+};
