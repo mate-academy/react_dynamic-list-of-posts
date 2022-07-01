18b0b4a3206a956794aff60bc3dbce8f938995ca
efdalyalcin <efdalyalcin@hotmail.com>
2022-07-01 18:22:28 +0300

    initial commit, get metods done. Post method is needed

---
+++ src/api/comments.ts
@@ -0,0 +1,7 @@
+import { BASE_URL } from './api';
+
+export const getPostComments = async (postId: number) => {
+  const response = await fetch(`${BASE_URL}/comments?postId=${postId}`);
+
+  return response.json();
+};
--- src/components/PostDetails/PostDetails.tsx
+++ src/components/PostDetails/PostDetails.tsx
@@ -1,45 +1,100 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getPostComments } from '../../api/comments';
+import { getPostDetails } from '../../api/posts';
 import { NewCommentForm } from '../NewCommentForm';
 import './PostDetails.scss';
 
-export const PostDetails: React.FC = () => (
-  <div className="PostDetails">
-    <h2>Post details:</h2>
+type Props = {
+  selectedPostId: number;
+  // filteredPost: Post | null;
+};
 
-    <section className="PostDetails__post">
-      <p>sunt aut facere repellat provident occaecati excepturi optio</p>
-    </section>
+export const PostDetails: React.FC<Props> = ({
+  selectedPostId,
+  // filteredPost,
+}) => {
+  const [postDetails, setPostDetails] = useState<Post | null>(null);
+  const [postComments, setPostComments] = useState<Comment[]>([]);
 
-    <section className="PostDetails__comments">
-      <button type="button" className="button">Hide 2 comments</button>
+  const loadPostDetails = async () => {
+    try {
+      let postDetailsFromServer;
 
-      <ul className="PostDetails__list">
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
-          >
-            X
-          </button>
-          <p>My first comment</p>
-        </li>
+      if (selectedPostId) {
+        postDetailsFromServer = await getPostDetails(selectedPostId);
+      }
 
-        <li className="PostDetails__list-item">
-          <button
-            type="button"
-            className="PostDetails__remove-button button"
-          >
-            X
-          </button>
-          <p>sad sds dfsadf asdf asdf</p>
-        </li>
-      </ul>
-    </section>
+      setPostDetails(postDetailsFromServer);
+    } catch {
+      setPostDetails(null);
+    }
+  };
 
-    <section>
-      <div className="PostDetails__form-wrapper">
-        <NewCommentForm />
-      </div>
-    </section>
-  </div>
-);
+  useEffect(
+    () => {
+      loadPostDetails();
+    },
+    [selectedPostId],
+  );
+
+  const loadPostComments = async () => {
+    try {
+      const commentsFromServer = await getPostComments(selectedPostId);
+
+      setPostComments(commentsFromServer);
+    } catch {
+      setPostComments([]);
+    }
+  };
+
+  useEffect(
+    () => {
+      loadPostComments();
+    },
+    [selectedPostId],
+  );
+
+  return (
+    <div className="PostDetails">
+      <h2>Post details:</h2>
+
+      {postDetails && (
+        <>
+          <section className="PostDetails__post">
+            <p>{postDetails.title}</p>
+          </section>
+
+          <section className="PostDetails__comments">
+            <button type="button" className="button">
+              {`Hide ${postComments.length} comments`}
+            </button>
+
+            <ul className="PostDetails__list">
+              {postComments.map((comment: Comment) => (
+                <li
+                  key={comment.id}
+                  className="PostDetails__list-item"
+                >
+                  <button
+                    type="button"
+                    className="PostDetails__remove-button button"
+                  >
+                    X
+                  </button>
+                  <p>{comment.body}</p>
+                </li>
+              ))}
+            </ul>
+          </section>
+
+          <section>
+            <div className="PostDetails__form-wrapper">
+              <NewCommentForm />
+            </div>
+          </section>
+        </>
+      )}
+
+    </div>
+  );
+};
