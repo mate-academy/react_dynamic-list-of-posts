dc217a50c54709e1ffe374af0ce6186b6ccb0fa2
Sardorbek Makhamadaliev <smakhamadaliev8@gmail.com>
2022-07-05 11:40:25 +0200

    initial commit

--- src/components/NewCommentForm/NewCommentForm.tsx
+++ src/components/NewCommentForm/NewCommentForm.tsx
@@ -1,39 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewCommentForm.scss';
 
-export const NewCommentForm: React.FC = () => (
-  <form className="NewCommentForm">
-    <div className="form-field">
-      <input
-        type="text"
-        name="name"
-        placeholder="Your name"
-        className="NewCommentForm__input"
-      />
-    </div>
+import { NewComment } from '../../react-app-env';
 
-    <div className="form-field">
-      <input
-        type="text"
-        name="email"
-        placeholder="Your email"
-        className="NewCommentForm__input"
-      />
-    </div>
+interface Props {
+  selectedPostId: number;
+  onAddComment: (newComment: NewComment) => void;
+}
 
-    <div className="form-field">
-      <textarea
-        name="body"
-        placeholder="Type comment here"
-        className="NewCommentForm__input"
-      />
-    </div>
+export const NewCommentForm: React.FC<Props> = ({
+  selectedPostId,
+  onAddComment,
+}) => {
+  const [newName, setNewName] = useState('');
+  const [newEmail, setNewEmail] = useState('');
+  const [newBody, setNewBody] = useState('');
 
-    <button
-      type="submit"
-      className="NewCommentForm__submit-button button"
-    >
-      Add a comment
-    </button>
-  </form>
-);
+  const clearForm = () => {
+    setNewName('');
+    setNewEmail('');
+    setNewBody('');
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const newComment: NewComment = {
+      postId: selectedPostId,
+      name: newName,
+      email: newEmail,
+      body: newBody,
+    };
+
+    onAddComment(newComment);
+    clearForm();
+  };
+
+  return (
+    <form className="NewCommentForm" onSubmit={handleSubmit}>
+      <div className="form-field">
+        <input
+          type="text"
+          name="name"
+          placeholder="Your name"
+          className="NewCommentForm__input"
+          required
+          value={newName}
+          onChange={event => setNewName(event.target.value)}
+        />
+      </div>
+
+      <div className="form-field">
+        <input
+          type="email"
+          name="email"
+          placeholder="Your email"
+          className="NewCommentForm__input"
+          required
+          value={newEmail}
+          onChange={event => setNewEmail(event.target.value)}
+        />
+      </div>
+
+      <div className="form-field">
+        <textarea
+          name="body"
+          placeholder="Type comment here"
+          className="NewCommentForm__input"
+          required
+          value={newBody}
+          onChange={event => setNewBody(event.target.value)}
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="NewCommentForm__submit-button button"
+      >
+        Add a comment
+      </button>
+    </form>
+  );
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="react-scripts" />
+
+interface Post {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface NewComment {
+  postId: number;
+  name:string;
+  email: string;
+  body: string;
+}
+
+export interface Comment extends NewComment {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+}
