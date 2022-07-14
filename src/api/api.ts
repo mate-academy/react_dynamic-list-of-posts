d188ab0ae290b9f2e00d011f97191d92862bb547
Rashaad Ebrahim <rmebrahim@gmail.com>
2022-07-14 07:44:47 +0200

    Task complete

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,7 @@
 export const BASE_URL = 'https://mate.academy/students-api';
+
+export const getUsers = async () => {
+  const response = await fetch(`${BASE_URL}/users/`);
+
+  return response.json();
+};
--- src/components/NewCommentForm/NewCommentForm.tsx
+++ src/components/NewCommentForm/NewCommentForm.tsx
@@ -1,39 +1,85 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import { postComment } from '../../api/comments';
 import './NewCommentForm.scss';
 
-export const NewCommentForm: React.FC = () => (
-  <form className="NewCommentForm">
-    <div className="form-field">
-      <input
-        type="text"
-        name="name"
-        placeholder="Your name"
-        className="NewCommentForm__input"
-      />
-    </div>
+type Props = {
+  selectedPostId: number;
+  loadPostComments: () => void;
+};
 
-    <div className="form-field">
-      <input
-        type="text"
-        name="email"
-        placeholder="Your email"
-        className="NewCommentForm__input"
-      />
-    </div>
+export const NewCommentForm: React.FC<Props> = ({
+  selectedPostId,
+  loadPostComments,
+}) => {
+  const [newName, setNewName] = useState('');
+  const [newEmail, setNewEmail] = useState('');
+  const [newBody, setNewBody] = useState('');
 
-    <div className="form-field">
-      <textarea
-        name="body"
-        placeholder="Type comment here"
-        className="NewCommentForm__input"
-      />
-    </div>
+  const submitHandler = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-    <button
-      type="submit"
-      className="NewCommentForm__submit-button button"
+    await postComment(
+      {
+        postId: selectedPostId,
+        name: newName,
+        email: newEmail,
+        body: newBody,
+      },
+    );
+
+    loadPostComments();
+    setNewName('');
+    setNewEmail('');
+    setNewBody('');
+  };
+
+  return (
+    <form
+      onSubmit={submitHandler}
+      className="NewCommentForm"
     >
-      Add a comment
-    </button>
-  </form>
-);
+      <div className="form-field">
+        <input
+          type="text"
+          name="name"
+          placeholder="Your name"
+          className="NewCommentForm__input"
+          value={newName}
+          onChange={(event) => setNewName(event.target.value)}
+          required
+        />
+      </div>
+
+      <div className="form-field">
+        <input
+          type="email"
+          name="email"
+          placeholder="Your email"
+          className="NewCommentForm__input"
+          value={newEmail}
+          onChange={(event) => setNewEmail(event.target.value)}
+          required
+        />
+      </div>
+
+      <div className="form-field">
+        <input
+          type="text"
+          name="name"
+          placeholder="Your name"
+          className="NewCommentForm__input"
+          value={newName}
+          onChange={(event) => setNewName(event.target.value)}
+          required
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="NewCommentForm__submit-button button"
+      >
+        Add a comment
+      </button>
+    </form>
+  );
+};
