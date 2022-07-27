005092516bf792c33bdc6161088a12582ff07339
Marta Fedoryshyn <fedoryshyn.marta@gmail.com>
2022-07-27 18:00:51 +0300

    list of posts was added

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,12 @@
-export const BASE_URL = 'https://mate.academy/students-api';
+const BASE_URL = 'https://mate.academy/students-api';
+
+// eslint-disable-next-line max-len
+export const request = async (url: string, options: {} | undefined = undefined) => {
+  const response = await fetch(`${BASE_URL}/${url}`, options);
+
+  if (!response.ok) {
+    throw new Error('Failed to load posts');
+  }
+
+  return response.json();
+};
