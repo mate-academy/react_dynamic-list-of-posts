c576939988d341fc14c7b65aab886c0e70fa73ea
Kostiantyn <havryshkostya@gmail.com>
2022-08-14 11:46:11 +0300

    figured it out

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ const App: React.FC = () => {
   const [currentUserId, setCurrentUserId] = useState(0);
   const [selectedPostId, setSelectedPostId] = useState(0);
   const [posts, setPosts] = useState<Post[] | null>([]);
+  const [isError, setIsError] = useState(false);
 
   const getUsersFromServer = async () => {
     try {
@@ -19,8 +20,7 @@ const App: React.FC = () => {
 
       setUsers(usersFromServer);
     } catch {
-      // setUsers([]);
-      throw new Error('Some error occured');
+      setIsError((prev) => !prev);
     }
   };
 
@@ -48,30 +48,35 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App__header">
-        <label>
-          Select a user: &nbsp;
+        {!isError ? (
+          <label>
+            Select a user: &nbsp;
 
-          <select
-            className="App__user-selector"
-            onChange={(event) => setCurrentUserId(+event.target.value)}
-          >
-            <option value="0">All users</option>
-            {users?.map((user) => {
-              if (user.name !== null) {
-                return (
-                  <option
-                    key={user.id}
-                    value={user.id}
-                  >
-                    {user.name}
-                  </option>
-                );
-              }
+            <select
+              className="App__user-selector"
+              onChange={(event) => setCurrentUserId(+event.target.value)}
+            >
+              <option value="0">All users</option>
+              {users?.map((user) => {
+                if (user.name !== null) {
+                  return (
+                    <option
+                      key={user.id}
+                      value={user.id}
+                    >
+                      {user.name}
+                    </option>
+                  );
+                }
 
-              return null;
-            })}
-          </select>
-        </label>
+                return null;
+              })}
+            </select>
+          </label>
+        ) : (
+          <span>Error occured</span>
+        )}
+
       </header>
 
       <main className="App__main">
