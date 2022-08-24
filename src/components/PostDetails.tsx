e6db71c5b4f19dfb7256d9cdf146dbdf03277dc3
PavloMarkov <r9markovpn@gmail.com>
2022-08-24 18:14:37 +0300

    App, PostList, PostDetails were implemented

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -1,58 +1,110 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Post } from '../types/Post';
+import { Comment } from '../types/Comment';
+import { client } from '../utils/fetchClient';
 import { Loader } from './Loader';
 import { NewCommentForm } from './NewCommentForm';
 
-export const PostDetails: React.FC = () => {
+type Props = {
+  post: Post;
+};
+
+export const PostDetails: React.FC<Props> = (props) => {
+  const { post } = props;
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [isLoadingComments, setIsLoadingComments] = useState(false);
+  const [hasLoadingError, setHasLoadingComments] = useState(false);
+  const [hasLoadingEnd, setHasLoadingEnd] = useState(false);
+
+  useEffect(
+    () => {
+      setIsLoadingComments(true);
+      client.get<Comment[]>(`/comments?postId=${post.id}`)
+        .then(result => {
+          setComments(result.map(comment => (
+            {
+              id: comment.id,
+              postId: comment.postId,
+              name: comment.name,
+              email: comment.email,
+              body: comment.body,
+            }
+          )));
+        })
+        .catch(() => {
+          setHasLoadingComments(true);
+        })
+        .finally(() => {
+          setIsLoadingComments(false);
+          setHasLoadingEnd(true);
+        });
+    },
+    [],
+  );
+
   return (
     <div className="content" data-cy="PostDetails">
       <div className="content" data-cy="PostDetails">
         <div className="block">
           <h2 data-cy="PostTitle">
-            #18: voluptate et itaque vero tempora molestiae
+            {`#${post.id}: ${post.title}`}
           </h2>
 
           <p data-cy="PostBody">
-            eveniet quo quis
-            laborum totam consequatur non dolor
-            ut et est repudiandae
-            est voluptatem vel debitis et magnam
+            {post.body}
           </p>
         </div>
 
         <div className="block">
-          <Loader />
+          {isLoadingComments && <Loader />}
 
-          <div className="notification is-danger" data-cy="CommentsError">
-            Something went wrong
-          </div>
-
-          <p className="title is-4" data-cy="NoCommentsMessage">
-            No comments yet
-          </p>
-
-          <p className="title is-4">Comments:</p>
-
-          <article className="message is-small" data-cy="Comment">
-            <div className="message-header">
-              <a href="mailto:misha@mate.academy" data-cy="CommentAuthor">
-                Misha Hrynko
-              </a>
-              <button
-                data-cy="CommentDelete"
-                type="button"
-                className="delete is-small"
-                aria-label="delete"
-              >
-                delete button
-              </button>
+          {hasLoadingError && (
+            <div
+              className="notification is-danger"
+              data-cy="CommentsError"
+            >
+              Something went wrong
             </div>
+          )}
 
-            <div className="message-body" data-cy="CommentBody">
-              Some comment
-            </div>
-          </article>
+          {hasLoadingEnd && comments.length === 0 && (
+            <p
+              className="title is-4"
+              data-cy="NoCommentsMessage"
+            >
+              No comments yet
+            </p>
+          )}
 
-          <article className="message is-small" data-cy="Comment">
+          {comments.length > 0 && (
+            <>
+              <p className="title is-4">Comments:</p>
+
+              {comments.map(comment => (
+                <article className="message is-small" data-cy="Comment">
+                  <div className="message-header">
+                    <a href={`mailto:${comment.email}`} data-cy="CommentAuthor">
+                      {comment.name}
+                    </a>
+                    <button
+                      data-cy="CommentDelete"
+                      type="button"
+                      className="delete is-small"
+                      aria-label="delete"
+                    >
+                      delete button
+                    </button>
+                  </div>
+
+                  <div className="message-body" data-cy="CommentBody">
+                    {comment.body}
+                  </div>
+                </article>
+              ))}
+            </>
+          )}
+
+          {/* <article className="message is-small" data-cy="Comment">
             <div className="message-header">
               <a
                 href="mailto:misha@mate.academy"
@@ -100,7 +152,7 @@ export const PostDetails: React.FC = () => {
             <div className="message-body" data-cy="CommentBody">
               {'Multi\nline\ncomment'}
             </div>
-          </article>
+          </article> */}
 
           <button
             data-cy="WriteCommentButton"
