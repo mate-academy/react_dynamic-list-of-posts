295fa789ae1043cb36b52c75e05323b0f44ebadd
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-10 22:17:31 +0300

    Added type for new comment data

--- src/api/comments.ts
+++ src/api/comments.ts
@@ -6,7 +6,10 @@ export const getPostComments = (postId: number) => (
   client.get<IComment[]>(`/comments?postId=${postId}`)
 );
 
-export const postNewComment = (comment: IComment) => (
+// eslint-disable-next-line max-len
+export type TCommentToPost = Pick<IComment, 'name' | 'email' | 'body' | 'postId'>;
+
+export const postNewComment = (comment: TCommentToPost) => (
   client.post<IComment>('/comments', comment)
 );
 
