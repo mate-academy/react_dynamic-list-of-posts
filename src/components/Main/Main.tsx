703a9f57989436347cd5e416c59a899d19da5373
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-10 17:39:06 +0300

    Implemented users loading, displaying and selecting

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,24 +1,43 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Loader } from '../Loader/Loader';
 import { Notification } from '../Notification/Notification';
 import { PostsList } from '../PostsList/PostsList';
 import { UserSelector } from '../UserSelector/UserSelector';
 
+import { TRootDispatch, TRootState } from '../../redux/store';
+import { fetchUsers } from '../../redux/slices/userSlice';
+
+import { EStatus } from '../../types/Status.enum';
+
 export const Main: React.FC = () => {
+  const {
+    currentUser,
+    status,
+  } = useSelector((state: TRootState) => state.users);
+
+  const dispatch: TRootDispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   return (
     <div className="tile is-parent">
       <div className="tile is-child box is-success">
-        <div className="block">
-          <UserSelector />
-        </div>
+        <UserSelector />
 
         <div className="block" data-cy="MainContent">
-          <p data-cy="NoSelectedUser">
-            No user selected
-          </p>
+          {status === EStatus.SUCCESS && !currentUser && (
+            <p data-cy="NoSelectedUser">
+              No user selected
+            </p>
+          )}
 
-          <Loader />
+          {status === EStatus.PENDING && <Loader />}
 
-          <Notification />
+          {status === EStatus.ERROR && <Notification />}
 
           <PostsList />
         </div>
--- src/components/UserSelector/UserSelector.tsx
+++ src/components/UserSelector/UserSelector.tsx
@@ -1,31 +1,69 @@
+import { useEffect, useState } from 'react';
+import classNames from 'classnames';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { setCurrentUser } from '../../redux/slices/userSlice';
+
+import { TRootDispatch, TRootState } from '../../redux/store';
+
+import './UserSelector.scss';
+
 export const UserSelector: React.FC = () => {
+  const { list, currentUser } = useSelector((state: TRootState) => state.users);
+  const [listVisible, setListVisible] = useState(false);
+
+  const dispatch: TRootDispatch = useDispatch();
+
+  useEffect(() => setListVisible(false), [currentUser]);
+
   return (
-    <div
-      data-cy="UserSelector"
-      className="dropdown is-active"
-    >
-      <div className="dropdown-trigger">
-        <button
-          type="button"
-          className="button"
-          aria-haspopup="true"
-          aria-controls="dropdown-menu"
-        >
-          <span>Choose a user</span>
+    <div className="block">
+      <div
+        data-cy="UserSelector"
+        className={classNames(
+          'dropdown',
+          {
+            'is-active': listVisible,
+          },
+        )}
+      >
+        <div className="dropdown-trigger">
+          <button
+            type="button"
+            className="button"
+            aria-haspopup="true"
+            aria-controls="dropdown-menu"
+            onClick={() => setListVisible(!listVisible)}
+          >
+            {currentUser ? (
+              <span>{currentUser.name}</span>
+            ) : (
+              <span>Choose a user</span>
+            )}
 
-          <span className="icon is-small">
-            <i className="fas fa-angle-down" aria-hidden="true" />
-          </span>
-        </button>
-      </div>
+            <span className="icon is-small">
+              <i className="fas fa-angle-down" aria-hidden="true" />
+            </span>
+          </button>
+        </div>
 
-      <div className="dropdown-menu" id="dropdown-menu" role="menu">
-        <div className="dropdown-content">
-          <a href="#user-1" className="dropdown-item">Leanne Graham</a>
-          <a href="#user-2" className="dropdown-item is-active">Ervin Howell</a>
-          <a href="#user-3" className="dropdown-item">Clementine Bauch</a>
-          <a href="#user-4" className="dropdown-item">Patricia Lebsack</a>
-          <a href="#user-5" className="dropdown-item">Chelsey Dietrich</a>
+        <div className="dropdown-menu" id="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+            {list.map(user => (
+              <a
+                href={`#${user.id}`}
+                key={user.id}
+                className="dropdown-item"
+                onClick={event => {
+                  event.preventDefault();
+
+                  dispatch(setCurrentUser(user));
+                }}
+              >
+                {user.name}
+              </a>
+            ))}
+          </div>
         </div>
       </div>
     </div>
