0a72be4086278fb6064d09d28c9989e437cd85e8
sergii-nosachenko <s.nosachenko@gmail.com>
2022-09-10 20:29:14 +0300

    Added comments slice for redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import userReducer from './slices/userSlice';
 import postReducer from './slices/postSlice';
+import commentReducer from './slices/commentSlice';
 
 export const store = configureStore({
   reducer: {
     users: userReducer,
     posts: postReducer,
+    comments: commentReducer,
   },
 });
 
