c6461203f1760e4f45c486af9ae384607008e9f8
Batick3737 <antonandriy@ukr.net>
2022-09-29 17:24:20 +0300

    write code

--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
+import { client } from '../utils/fetchClient';
+import { User } from '../types/User';
+import { Post } from '../types/Post';
 
-export const UserSelector: React.FC = () => {
+type Props = {
+  setSelectedUserId: (userId: number) => void;
+  setSelectedPost: (post: Post | null) => void;
+};
+
+export const UserSelector: React.FC<Props> = ({
+  setSelectedUserId,
+  setSelectedPost,
+}) => {
+  const [openList, setOpenList] = useState(false);
+  const [users, setUsers] = useState<User[]>([]);
+  const [selectUserName, setSelectUserName] = useState('');
+
+  useEffect(() => {
+    client.get<User[]>('/users').then(res => setUsers(res));
+  }, []);
+
   return (
     <div
       data-cy="UserSelector"
@@ -12,8 +32,9 @@ export const UserSelector: React.FC = () => {
           className="button"
           aria-haspopup="true"
           aria-controls="dropdown-menu"
+          onClick={() => setOpenList(boolean => !boolean)}
         >
-          <span>Choose a user</span>
+          <span>{selectUserName || 'Choose a user'}</span>
 
           <span className="icon is-small">
             <i className="fas fa-angle-down" aria-hidden="true" />
@@ -21,15 +42,30 @@ export const UserSelector: React.FC = () => {
         </button>
       </div>
 
-      <div className="dropdown-menu" id="dropdown-menu" role="menu">
-        <div className="dropdown-content">
-          <a href="#user-1" className="dropdown-item">Leanne Graham</a>
-          <a href="#user-2" className="dropdown-item is-active">Ervin Howell</a>
-          <a href="#user-3" className="dropdown-item">Clementine Bauch</a>
-          <a href="#user-4" className="dropdown-item">Patricia Lebsack</a>
-          <a href="#user-5" className="dropdown-item">Chelsey Dietrich</a>
+      {openList && (
+        <div className="dropdown-menu" id="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+            {users.map(user => (
+              <a
+                key={user.id}
+                onClick={() => {
+                  setSelectUserName(user.name);
+                  setSelectedUserId(user.id);
+                  setOpenList(false);
+                  setSelectedPost(null);
+                }}
+                href={`#user-${user.id}`}
+                className={classNames(
+                  'dropdown-item',
+                  { 'is-active': selectUserName === user.name },
+                )}
+              >
+                {user.name}
+              </a>
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
