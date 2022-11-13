d27abb6da494e6e60e81696573fcaa5ce13ab9f0
KristinaTar <kristina.tarashchuk@gmail.com>
2022-11-13 20:30:03 +0200

    ready task

--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -1,13 +1,11 @@
 const BASE_URL = 'https://mate.academy/students-api';
 
-// a promise resolved after a given delay
 function wait(delay: number) {
   return new Promise(resolve => {
     setTimeout(resolve, delay);
   });
 }
 
-// To have autocompletion and avoid mistypes
 type RequestMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
 
 function request<T>(
@@ -18,14 +16,12 @@ function request<T>(
   const options: RequestInit = { method };
 
   if (data) {
-    // We add body and Content-Type only for the requests with data
     options.body = JSON.stringify(data);
     options.headers = {
       'Content-Type': 'application/json; charset=UTF-8',
     };
   }
 
-  // for a demo purpose we emulate a delay to see if Loaders work
   return wait(300)
     .then(() => fetch(BASE_URL + url, options))
     .then(response => response.json());
