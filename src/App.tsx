a1212dc478c334299f6cd440723d4ed8ed144485
Matvii <admin@admins-MacBook-Air.local>
2022-12-08 11:42:32 +0100

    sol

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useEffect, useState } from 'react';
 import 'bulma/bulma.sass';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
@@ -8,8 +9,28 @@ import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
 import { Loader } from './components/Loader';
+import { client } from './utils/fetchClient';
+import { User } from './types/User';
 
 export const App: React.FC = () => {
+  const [selectedUserId, setSelectedUserId] = useState(-1);
+  const [users, setUsers] = useState<User[] | null>(null);
+  const [isErrorShown, setIsErrorShown] = useState(false);
+
+  useEffect(() => {
+    const getUsers = async (): Promise<void> => {
+      try {
+        const data: User[] = await client.get('/users');
+
+        setUsers(data);
+      } catch (error) {
+        setIsErrorShown(true);
+      }
+    };
+
+    getUsers();
+  }, []);
+
   return (
     <main className="section">
       <div className="container">
@@ -17,22 +38,32 @@ export const App: React.FC = () => {
           <div className="tile is-parent">
             <div className="tile is-child box is-success">
               <div className="block">
-                <UserSelector />
+                {users && (
+                  <UserSelector
+                    users={users}
+                    selectedUserId={selectedUserId}
+                    setSelectedUserId={setSelectedUserId}
+                  />
+                )}
               </div>
 
               <div className="block" data-cy="MainContent">
-                <p data-cy="NoSelectedUser">
-                  No user selected
-                </p>
+                {selectedUserId !== -1 && (
+                  <p data-cy="NoSelectedUser">
+                    No user selected
+                  </p>
+                )}
 
                 <Loader />
 
-                <div
-                  className="notification is-danger"
-                  data-cy="PostsLoadingError"
-                >
-                  Something went wrong!
-                </div>
+                {isErrorShown && (
+                  <div
+                    className="notification is-danger"
+                    data-cy="PostsLoadingError"
+                  >
+                    Something went wrong!
+                  </div>
+                )}
 
                 <div className="notification is-warning" data-cy="NoPostsYet">
                   No posts yet
--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+/* eslint-disable no-console */
+import classNames from 'classnames';
+import { useState } from 'react';
+import { User } from '../types/User';
 
-export const UserSelector: React.FC = () => {
+type Props = {
+  users: User[],
+  setSelectedUserId: (id: number) => void,
+  selectedUserId: number,
+};
+
+export const UserSelector: React.FC<Props> = ({
+  users,
+  setSelectedUserId,
+  selectedUserId,
+}) => {
+  const [isMenuDropped, setIsMenuDropped] = useState(false);
+
+  const selectUser = (user: User) => {
+    console.log(user);
+
+    if (user.id !== selectedUserId) {
+      setSelectedUserId(user.id);
+    }
+  };
+
+  console.log(selectedUserId);
+
   return (
     <div
       data-cy="UserSelector"
-      className="dropdown is-active"
+      className={classNames(
+        'dropdown',
+        { 'is-active': isMenuDropped },
+      )}
     >
       <div className="dropdown-trigger">
         <button
@@ -12,7 +40,10 @@ export const UserSelector: React.FC = () => {
           className="button"
           aria-haspopup="true"
           aria-controls="dropdown-menu"
+          onClick={() => setIsMenuDropped(prev => !prev)}
+          onBlur={() => setIsMenuDropped(false)}
         >
+
           <span>Choose a user</span>
 
           <span className="icon is-small">
@@ -23,11 +54,24 @@ export const UserSelector: React.FC = () => {
 
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
         <div className="dropdown-content">
-          <a href="#user-1" className="dropdown-item">Leanne Graham</a>
-          <a href="#user-2" className="dropdown-item is-active">Ervin Howell</a>
-          <a href="#user-3" className="dropdown-item">Clementine Bauch</a>
-          <a href="#user-4" className="dropdown-item">Patricia Lebsack</a>
-          <a href="#user-5" className="dropdown-item">Chelsey Dietrich</a>
+          {users.map((user) => {
+            return (
+              <a
+                key={user.id}
+                href={`#user-${user.id}`}
+                className={classNames(
+                  'dropdown-item',
+                  { 'is-active': selectedUserId === user.id },
+                )}
+                onClick={() => {
+                  console.log(user);
+                  selectUser(user);
+                }}
+              >
+                {user.name}
+              </a>
+            );
+          })}
         </div>
       </div>
     </div>
