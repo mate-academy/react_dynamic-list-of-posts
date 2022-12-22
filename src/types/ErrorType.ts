17e5fc73e42c13baa96e70e6f1a4bceff9f7588a
Andriy Nosov <nosovandriy@gmail.com>
2022-12-22 10:34:15 +0200

    Add ErrorType

---
+++ src/types/ErrorType.ts
@@ -0,0 +1,8 @@
+export enum ErrorType {
+  errorUsersLoad = 'Something went wrong with loading users',
+  errorUserPosts = 'Something went wrong with loading user posts',
+  errorPostComments = 'Something went wrong with loading post comments',
+  errorCommentDelete = 'Something went wrong with deleting comment',
+  errorCommentAdd = 'Something went wrong with adding comment',
+  usersLoad = 'Something went wrong with loading users',
+}
