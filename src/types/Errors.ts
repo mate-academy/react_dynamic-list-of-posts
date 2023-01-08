3a2ed6fb80967a2642264355862a0519deec9e18
Andrii Melnyk <andriimelnyq@gmail.com>
2023-01-08 16:47:34 +0200

    Solution

---
+++ src/types/Errors.ts
@@ -0,0 +1,8 @@
+export enum Errors {
+  NONE = '',
+  USERS = 'Unable to load users',
+  POSTS = 'Unable to load posts',
+  COMMENTS = 'Unable to load comments',
+  COMMENT_ADD = 'Unable to add comment',
+  COMMENT_DELETE = 'Unable to delete comment',
+}
