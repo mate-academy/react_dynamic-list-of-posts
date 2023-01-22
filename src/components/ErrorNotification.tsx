9960a8d56dc868e967fd568e9197fbdd04a40401
Oleksandr Filo <filo.sanya@gmail.com>
2023-01-22 12:37:12 +0200

    error while deploying was fixed

--- src/components/ErrorNotification.tsx
+++ src/components/ErrorNotification.tsx
@@ -5,30 +5,28 @@ type Props = {
   error: Errors;
 };
 
-export const ErrorNotification: React.FC<Props> = React.memo(
-  ({ error }) => {
-    let errorMessage = 'Something went wrong!';
+export const ErrorNotification: React.FC<Props> = React.memo(({ error }) => {
+  let errorMessage = 'Something went wrong!';
 
-    switch (error) {
-      case Errors.PostsLoadingError:
-        errorMessage = 'Can\'t load posts';
-        break;
+  switch (error) {
+    case Errors.PostsLoadingError:
+      errorMessage = 'Can\'t load posts';
+      break;
 
-      case Errors.CommentsError:
-        errorMessage = 'Can\'t load comments';
-        break;
+    case Errors.CommentsError:
+      errorMessage = 'Can\'t load comments';
+      break;
 
-      default:
-        break;
-    }
+    default:
+      break;
+  }
 
-    return (
-      <div
-        className="notification is-danger"
-        data-cy={error}
-      >
-        {errorMessage}
-      </div>
-    );
-  },
-);
+  return (
+    <div
+      className="notification is-danger"
+      data-cy={error}
+    >
+      {errorMessage}
+    </div>
+  );
+});
