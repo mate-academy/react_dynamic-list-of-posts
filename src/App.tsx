226fafbb12078559cdfb4d119cfb49aeb1c58bae
Serhii Yunnikov <yunnikov@icloud.com>
2023-02-20 20:35:49 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,151 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/bulma.sass';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-
 import classNames from 'classnames';
+
+import { User } from './types/User';
+import { Post } from './types/Post';
+import { Comment } from './types/Comment';
+import { Reset } from './types/Reset';
+
+import { getUsersList } from './api/users';
+import { getPosts } from './api/posts';
+import { getComments, postComment, deleteComment } from './api/comments';
+
 import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
 import { Loader } from './components/Loader';
 
 export const App: React.FC = () => {
+  const [userId, setUserId] = useState<number | null>(null);
+  const [userName, setUsername] = useState('Choose a user');
+  const [users, setUsers] = useState<User[] | null>(null);
+  const [posts, setPosts] = useState<Post [] | null>(null);
+  const [isPostLoading, setIsPostLoading] = useState(false);
+  const [isWrongPostResponse, setIsWrongPostResponse] = useState(false);
+  const [isActiveList, setIsActiveList] = useState(false);
+  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
+  const [comments, setComments] = useState<Comment[] | null>(null);
+  const [isWrongSidebarResponse, setIsWrongSidebarResponse] = useState(false);
+  const [isCommentsLoading, setIsCommentsLoading] = useState(false);
+  const [isCommentForm, setIsCommentForm] = useState(false);
+  const [isLoadingNewComment, setIsloadingNewComment] = useState(false);
+  const [isDangerSubmit, setIsDangerSubmit] = useState(false);
+  const [isWarningUpdate, setIsWarningUpdate] = useState(false);
+
+  useEffect(() => {
+    getUsersList()
+      .then(response => setUsers(response))
+      .catch(error => window.alert(error));
+  }, []);
+
+  const handleSelectButton = () => {
+    setIsActiveList(isCurrent => !isCurrent);
+  };
+
+  const handleUserSelect = (
+    selectedUserId: number,
+    selectedUserName: string,
+  ) => {
+    setUserId(selectedUserId);
+    setUsername(selectedUserName);
+    setIsPostLoading(true);
+    setIsActiveList(false);
+
+    if (isWrongPostResponse) {
+      setIsWrongPostResponse(false);
+    }
+
+    getPosts(selectedUserId)
+      .then(response => setPosts(response))
+      .catch(() => setIsWrongPostResponse(true))
+      .finally(() => setIsPostLoading(false));
+  };
+
+  const handleSidebarButton = (currentPost: Post) => {
+    setSelectedPost(current => {
+      return current === currentPost ? null : currentPost;
+    });
+    setIsCommentsLoading(true);
+    setIsCommentForm(false);
+
+    getComments(currentPost.id)
+      .then(response => setComments(response))
+      .catch(() => setIsWrongSidebarResponse(true))
+      .finally(() => setIsCommentsLoading(false));
+  };
+
+  const handleSubmitCommentForm = (
+    event: React.FormEvent<HTMLFormElement>,
+    reset: (inputType: Reset) => void,
+    name: string,
+    email: string,
+    body: string,
+    postId?: number,
+  ) => {
+    event.preventDefault();
+
+    if (!name || !email || !body) {
+      setIsDangerSubmit(true);
+
+      return;
+    }
+
+    setIsloadingNewComment(true);
+
+    const data = {
+      postId,
+      name,
+      email,
+      body,
+    };
+
+    const updateComments = (newComment: Comment) => {
+      setComments(currenList => {
+        return currenList
+          ? [
+            ...currenList,
+            newComment,
+          ]
+          : [newComment];
+      });
+
+      setIsDangerSubmit(false);
+      reset(Reset.comment);
+    };
+
+    setIsWarningUpdate(false);
+
+    postComment(data)
+      .then((response) => updateComments(response))
+      .catch(() => setIsWarningUpdate(true))
+      .finally(() => setIsloadingNewComment(false));
+  };
+
+  const canselDangerInput = () => {
+    setIsDangerSubmit(false);
+  };
+
+  const handleDeleteComment = (commentId: number) => {
+    const prevComments = comments;
+
+    setComments(currentList => {
+      if (currentList) {
+        const list = currentList?.filter(comment => comment.id !== commentId);
+
+        return list;
+      }
+
+      return currentList;
+    });
+
+    deleteComment(commentId)
+      .then()
+      .catch(() => setComments(prevComments));
+  };
+
   return (
     <main className="section">
       <div className="container">
@@ -17,28 +153,43 @@ export const App: React.FC = () => {
           <div className="tile is-parent">
             <div className="tile is-child box is-success">
               <div className="block">
-                <UserSelector />
+                <UserSelector
+                  users={users}
+                  userName={userName}
+                  isActiveList={isActiveList}
+                  onHandleUserSelect={handleUserSelect}
+                  onHandleSelectButton={handleSelectButton}
+                />
               </div>
 
               <div className="block" data-cy="MainContent">
-                <p data-cy="NoSelectedUser">
-                  No user selected
-                </p>
+                {!userId && (
+                  <p data-cy="Nopost">
+                    No user selected
+                  </p>
+                )}
 
-                <Loader />
+                {isPostLoading && <Loader />}
 
-                <div
-                  className="notification is-danger"
-                  data-cy="PostsLoadingError"
-                >
-                  Something went wrong!
-                </div>
+                {!isPostLoading
+                && userId
+                && !isWrongPostResponse
+                && (
+                  <PostsList
+                    posts={posts}
+                    onHandleSidebarButton={handleSidebarButton}
+                    selectedId={selectedPost?.id}
+                  />
+                )}
 
-                <div className="notification is-warning" data-cy="NoPostsYet">
-                  No posts yet
-                </div>
-
-                <PostsList />
+                {isWrongPostResponse && (
+                  <div
+                    className="notification is-danger"
+                    data-cy="PostsLoadingError"
+                  >
+                    Something went wrong!
+                  </div>
+                )}
               </div>
             </div>
           </div>
@@ -50,11 +201,26 @@ export const App: React.FC = () => {
               'is-parent',
               'is-8-desktop',
               'Sidebar',
-              'Sidebar--open',
+              { 'Sidebar--open': !!selectedPost },
             )}
           >
             <div className="tile is-child box is-success ">
-              <PostDetails />
+              <PostDetails
+                comments={comments}
+                postTitle={selectedPost?.title}
+                postBody={selectedPost?.body}
+                postId={selectedPost?.id}
+                isWrongPostResponse={isWrongSidebarResponse}
+                isPostLoading={isCommentsLoading}
+                isCommentForm={isCommentForm}
+                isLoadingNewComment={isLoadingNewComment}
+                isDangerSubmit={isDangerSubmit}
+                isWarningUpdate={isWarningUpdate}
+                setIsCommentForm={setIsCommentForm}
+                onHandleFormSubmit={handleSubmitCommentForm}
+                canselDangerInput={canselDangerInput}
+                onHandleDeleteComment={handleDeleteComment}
+              />
             </div>
           </div>
         </div>
--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -1,8 +1,64 @@
+import classNames from 'classnames';
 import React from 'react';
+import useInput from '../hooks/hooks';
+import { Reset } from '../types/Reset';
 
-export const NewCommentForm: React.FC = () => {
+type Props = {
+  onHandleFormSubmit: (
+    event: React.FormEvent<HTMLFormElement>,
+    reset: (inputType: Reset) => void,
+    name: string,
+    email: string,
+    body: string,
+    postId?: number,
+  ) => void,
+  canselDangerInput: () => void,
+  postId?: number,
+  isLoadingNewComment: boolean,
+  isDangerSubmit: boolean,
+  isWarningUpdate: boolean,
+};
+
+export const NewCommentForm: React.FC<Props> = ({
+  onHandleFormSubmit,
+  canselDangerInput,
+  postId,
+  isLoadingNewComment,
+  isWarningUpdate,
+  isDangerSubmit,
+}) => {
+  const name = useInput('', canselDangerInput);
+  const email = useInput('', canselDangerInput);
+  const comment = useInput('', canselDangerInput);
+  const isErrorName = isDangerSubmit && !name.value;
+  const isErrorEmail = isDangerSubmit && !email.value;
+  const isErrorComment = isDangerSubmit && !comment.value;
+
+  const reset = (inputs: Reset) => {
+    switch (inputs) {
+      case (Reset.all):
+        name.reset();
+        email.reset();
+        comment.reset();
+        break;
+      default:
+        comment.reset();
+        break;
+    }
+  };
+
   return (
-    <form data-cy="NewCommentForm">
+    <form
+      data-cy="NewCommentForm"
+      onSubmit={(event) => onHandleFormSubmit(
+        event,
+        reset,
+        name.value,
+        email.value,
+        comment.value,
+        postId,
+      )}
+    >
       <div className="field" data-cy="NameField">
         <label className="label" htmlFor="comment-author-name">
           Author Name
@@ -14,24 +70,32 @@ export const NewCommentForm: React.FC = () => {
             name="name"
             id="comment-author-name"
             placeholder="Name Surname"
-            className="input is-danger"
+            className={classNames(
+              'input',
+              { 'is-danger': isErrorName },
+            )}
+            {...name}
           />
 
           <span className="icon is-small is-left">
             <i className="fas fa-user" />
           </span>
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+          {isErrorName && (
+            <span
+              className="icon is-small is-right has-text-danger"
+              data-cy="ErrorIcon"
+            >
+              <i className="fas fa-exclamation-triangle" />
+            </span>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Name is required
-        </p>
+        {isErrorName && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Name is required
+          </p>
+        )}
       </div>
 
       <div className="field" data-cy="EmailField">
@@ -45,24 +109,32 @@ export const NewCommentForm: React.FC = () => {
             name="email"
             id="comment-author-email"
             placeholder="email@test.com"
-            className="input is-danger"
+            className={classNames(
+              'input',
+              { 'is-danger': isErrorEmail },
+            )}
+            {...email}
           />
 
           <span className="icon is-small is-left">
             <i className="fas fa-envelope" />
           </span>
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+          {isErrorEmail && (
+            <span
+              className="icon is-small is-right has-text-danger"
+              data-cy="ErrorIcon"
+            >
+              <i className="fas fa-exclamation-triangle" />
+            </span>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Email is required
-        </p>
+        {isErrorEmail && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Email is required
+          </p>
+        )}
       </div>
 
       <div className="field" data-cy="BodyField">
@@ -75,25 +147,47 @@ export const NewCommentForm: React.FC = () => {
             id="comment-body"
             name="body"
             placeholder="Type comment here"
-            className="textarea is-danger"
+            className={classNames(
+              'textarea',
+              { 'is-danger': isErrorComment },
+            )}
+            {...comment}
           />
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Enter some text
-        </p>
+        {isErrorComment && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Enter some text
+          </p>
+        )}
       </div>
 
       <div className="field is-grouped">
         <div className="control">
-          <button type="submit" className="button is-link is-loading">
+          <button
+            type="submit"
+            className={classNames(
+              'button',
+              'is-link',
+              { 'is-loading': isLoadingNewComment },
+            )}
+          >
             Add
           </button>
+          {isWarningUpdate && (
+            <p className="help is-danger" data-cy="ErrorMessage">
+              Try again
+            </p>
+          )}
         </div>
 
         <div className="control">
           {/* eslint-disable-next-line react/button-has-type */}
-          <button type="reset" className="button is-link is-light">
+          <button
+            type="reset"
+            className="button is-link is-light"
+            onClick={() => reset(Reset.all)}
+          >
             Clear
           </button>
         </div>
