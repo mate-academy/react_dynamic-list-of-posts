99ce657942b15d20760c17848d50c05d490747c7
Denis Bokov <denisbokov1703@gmail.com>
2023-03-11 14:28:44 +0100

    Solution `fix`

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -96,56 +96,60 @@ export const PostDetails: React.FC<Props> = React.memo(
               </div>
             )}
 
-            {!comments.length && (
-              <p className="title is-4" data-cy="NoCommentsMessage">
-                No comments yet
-              </p>
-            )}
-
-            {!!comments.length && (
-              <p className="title is-4">Comments:</p>
-            )}
-
-            {comments.map((comment) => (
+            {!isLoading && (
               <>
-                {post && (
-                  <article
-                    className="message is-small"
-                    data-cy="Comment"
-                    key={comment.id}
-                  >
-                    <div className="message-header">
-                      <a href={`mailto:${comment.email}`} data-cy="CommentAuthor">
-                        {comment.name}
-                      </a>
-                      <button
-                        data-cy="CommentDelete"
-                        type="button"
-                        className="delete is-small"
-                        aria-label="delete"
-                        onClick={() => onCommentDelete(comment.id)}
-
-                      >
-                        delete button
-                      </button>
-                    </div>
-
-                    <div className="message-body" data-cy="CommentBody">
-                      {comment.body}
-                    </div>
-                  </article>
+                {!comments.length && (
+                  <p className="title is-4" data-cy="NoCommentsMessage">
+                    No comments yet
+                  </p>
                 )}
-              </>
-            ))}
 
-            <button
-              data-cy="WriteCommentButton"
-              type="button"
-              className="button is-link"
-              onClick={() => setIsWriteComment(true)}
-            >
-              Write a comment
-            </button>
+                {!!comments.length && (
+                  <p className="title is-4">Comments:</p>
+                )}
+  
+                {comments.map((comment) => (
+                  <>
+                    {post && (
+                      <article
+                        className="message is-small"
+                        data-cy="Comment"
+                        key={comment.id}
+                      >
+                        <div className="message-header">
+                          <a href={`mailto:${comment.email}`} data-cy="CommentAuthor">
+                            {comment.name}
+                          </a>
+                          <button
+                            data-cy="CommentDelete"
+                            type="button"
+                            className="delete is-small"
+                            aria-label="delete"
+                            onClick={() => onCommentDelete(comment.id)}
+    
+                          >
+                            delete button
+                          </button>
+                        </div>
+    
+                        <div className="message-body" data-cy="CommentBody">
+                          {comment.body}
+                        </div>
+                      </article>
+                    )}
+                  </>
+                ))}
+
+                <button
+                  data-cy="WriteCommentButton"
+                  type="button"
+                  className="button is-link"
+                  onClick={() => setIsWriteComment(true)}
+                >
+                  Write a comment
+                </button>
+              </>
+            )}
           </div>
 
           {isWriteComment && (
