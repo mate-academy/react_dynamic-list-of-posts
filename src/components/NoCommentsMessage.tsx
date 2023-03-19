0e99c95af1751778d928610c9ed2a6e418a166d1
Vasyl Pavlenko <genlus@ukr.net>
2023-03-19 13:33:36 +0200

    Fixes

---
+++ src/components/NoCommentsMessage.tsx
@@ -0,0 +1,9 @@
+export const NoCommentsMessage = () => {
+  return (
+    <article className="message is-small">
+      <div className="message-body">
+        No comments yet.
+      </div>
+    </article>
+  );
+};
