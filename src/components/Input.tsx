edfc8b2472a8487607abba7d6082db5e7282a390
Zibi95 <sylwester.sob95@gmail.com>
2023-05-29 17:05:46 +0200

    Build Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,52 @@
+import classNames from 'classnames';
+
+type InputProps = {
+  label: string;
+  name: string;
+  id: string;
+  placeholder: string;
+  icon: string;
+  errorMsg: string;
+  isError: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const Input = ({
+  label, id, icon, errorMsg, isError, ...attr
+}: InputProps) => {
+  return (
+    <div className="field" data-cy="NameField">
+      <label className="label" htmlFor={id}>
+        {label}
+      </label>
+
+      <div className="control has-icons-left has-icons-right">
+        <input
+          type="text"
+          {...attr}
+          id={id}
+          className={classNames('input', { 'is-danger': isError })}
+        />
+
+        <span className="icon is-small is-left">
+          <i className={`fas ${icon}`} />
+        </span>
+
+        {isError && (
+          <span
+            className="icon is-small is-right has-text-danger"
+            data-cy="ErrorIcon"
+          >
+            <i className="fas fa-exclamation-triangle" />
+          </span>
+        )}
+      </div>
+
+      {isError && (
+        <p className="help is-danger" data-cy="ErrorMessage">
+          {errorMsg}
+        </p>
+      )}
+    </div>
+  );
+};
