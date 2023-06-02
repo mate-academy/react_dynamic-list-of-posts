00594565aa5ca590df8771ee01577716c158cac6
Paweł Smoliński <ski1993@o2.pl>
2023-06-02 13:23:12 +0200

    missing filies

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,19 +1,1 @@
 require('@mate-academy/cypress-tools/support');
-
-Cypress.Commands.add(
-  'byDataCy',
-  { prevSubject: 'optional' },
-  
-  (subject, name, text = '') => {
-    const target = subject || cy;
-    const selector = `[data-cy="${name}"]`;
-
-    if (text) {
-      return target.contain(selector, text);
-    }
-
-    return subject
-      ? subject.find(selector)
-      : cy.get(selector);
-  },
-);
