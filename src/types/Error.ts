04f882aed77d37c3e71cecd03a5afcc6ece94e36
Veronika Pastushkova <d9169393507@gmai.com>
2023-06-22 10:02:38 +0300

    add task solution

---
+++ src/types/Error.ts
@@ -0,0 +1,8 @@
+export enum Error {
+  NONE = '',
+  DOWNLOAD = 'Unable to download users',
+  ADD = 'Unable to add a todo',
+  DELETE = 'Unable to delete a todo',
+  UPDATE = 'Unable to update a todo',
+  NOTITLE = 'Title can\'t be empty',
+}
