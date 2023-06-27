810b7a1e2d18ffd3478d45082cbac5149b4da049
Yelyzaveta <molochiza@gmail.com>
2023-06-27 21:20:53 +0300

    solution

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -96,81 +96,83 @@ export const PostDetails: React.FC<Props> = ({ post }) => {
 
       <div className="block">
 
-        {isLoading && (
-          <Loader />
-        )}
-
         {loadError && (
           <div className="notification is-danger" data-cy="CommentsError">
             Something went wrong
           </div>
         )}
 
-        {!comments.length ? (
-          <p className="title is-4" data-cy="NoCommentsMessage">
-            No comments yet
-          </p>
+        {isLoading ? (
+          <Loader />
         ) : (
           <>
-            <p className="title is-4">Comments:</p>
+            {!comments.length ? (
+              <p className="title is-4" data-cy="NoCommentsMessage">
+                No comments yet
+              </p>
+            ) : (
+              <>
+                <p className="title is-4">Comments:</p>
 
-            {comments.map(comment => (
-              <article
-                key={comment.id}
-                className="message is-small"
-                data-cy="Comment"
-              >
-                <div className="message-header">
-                  <a href={`mailto:${comment.email}`} data-cy="CommentAuthor">
-                    {comment.name}
-                  </a>
-                  <button
-                    data-cy="CommentDelete"
-                    type="button"
-                    className="delete is-small"
-                    aria-label="delete"
-                    onClick={() => deleteSelectedComment(comment.id)}
+                {comments.map(comment => (
+                  <article
+                    key={comment.id}
+                    className="message is-small"
+                    data-cy="Comment"
                   >
-                    delete button
-                  </button>
-                </div>
+                    <div className="message-header">
+                      <a href={`mailto:${comment.email}`} data-cy="CommentAuthor">
+                        {comment.name}
+                      </a>
+                      <button
+                        data-cy="CommentDelete"
+                        type="button"
+                        className="delete is-small"
+                        aria-label="delete"
+                        onClick={() => deleteSelectedComment(comment.id)}
+                      >
+                        delete button
+                      </button>
+                    </div>
 
-                <div className="message-body" data-cy="CommentBody">
-                  {comment.body}
-                </div>
-              </article>
-            ))}
+                    <div className="message-body" data-cy="CommentBody">
+                      {comment.body}
+                    </div>
+                  </article>
+                ))}
+              </>
+            )}
+            {error && (
+              <div
+                className="notification is-danger"
+                data-cy="CommentsError"
+              >
+                Can&apos;t add new comment
+              </div>
+            )}
+
+            {deleteError && (
+              <div
+                className="notification is-danger"
+                data-cy="CommentsError"
+              >
+                Can&apos;t delete comment
+              </div>
+            )}
+
+            {!isFormVisible ? (
+              <button
+                data-cy="WriteCommentButton"
+                type="button"
+                className="button is-link"
+                onClick={() => setIsFormVisible(true)}
+              >
+                Write a comment
+              </button>
+            ) : (
+              <NewCommentForm onAddComment={addNewComment} />
+            )}
           </>
-        )}
-        {error && (
-          <div
-            className="notification is-danger"
-            data-cy="CommentsError"
-          >
-            Can&apos;t add new comment
-          </div>
-        )}
-
-        {deleteError && (
-          <div
-            className="notification is-danger"
-            data-cy="CommentsError"
-          >
-            Can&apos;t delete comment
-          </div>
-        )}
-
-        {!isFormVisible ? (
-          <button
-            data-cy="WriteCommentButton"
-            type="button"
-            className="button is-link"
-            onClick={() => setIsFormVisible(true)}
-          >
-            Write a comment
-          </button>
-        ) : (
-          <NewCommentForm onAddComment={addNewComment} />
         )}
       </div>
     </div>
