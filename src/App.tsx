28ef1cfda80f99384877ffd680a38597d22f4bb9
Nataliia Hrushanyk <nataligrushanyk@gmail.com>
2023-08-01 18:52:03 +0300

    hide sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -88,20 +88,22 @@ export const App: React.FC = () => {
             </div>
           </div>
 
-          <div
-            data-cy="Sidebar"
-            className={classNames(
-              'tile',
-              'is-parent',
-              'is-8-desktop',
-              'Sidebar',
-              'Sidebar--open',
-            )}
-          >
-            <div className="tile is-child box is-success ">
-              <PostDetails />
+          {selectedPost && (
+            <div
+              data-cy="Sidebar"
+              className={classNames(
+                'tile',
+                'is-parent',
+                'is-8-desktop',
+                'Sidebar',
+                'Sidebar--open',
+              )}
+            >
+              <div className="tile is-child box is-success ">
+                <PostDetails />
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </main>
