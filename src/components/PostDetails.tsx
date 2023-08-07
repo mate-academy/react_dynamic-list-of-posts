58026e94bb3ce9098703276e73315ce1470ae20c
Artem Kravets <kravetsartyom@gmail.com>
2023-08-07 21:01:57 +0300

    Solution

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -1,117 +1,96 @@
 import React from 'react';
 import { Loader } from './Loader';
+import { Comment } from '../types/Comment';
 import { NewCommentForm } from './NewCommentForm';
+import { Post } from '../types/Post';
+import { CommentsList } from './CommentsList';
+import { ErrorTypes } from '../constants';
 
-export const PostDetails: React.FC = () => {
+type Props = {
+  post: Post | null;
+  errorType: ErrorTypes | null;
+  comments: Comment[];
+  isFetching: boolean;
+  isCommentFormOpen: boolean;
+  setErrorType: React.Dispatch<React.SetStateAction<ErrorTypes | null>>;
+  setComments: React.Dispatch<React.SetStateAction<Comment[]>>;
+  openCommentsForm: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const PostDetails: React.FC<Props> = ({
+  post,
+  comments,
+  errorType,
+  isFetching,
+  setComments,
+  setErrorType,
+  openCommentsForm,
+  isCommentFormOpen,
+}) => {
+  const handleOpenCommentForm = () => {
+    openCommentsForm(true);
+  };
+
+  const errorTypes: ErrorTypes[] = [
+    ErrorTypes.ADD,
+    ErrorTypes.DELETE,
+    ErrorTypes.COMMENTS,
+  ];
+
   return (
     <div className="content" data-cy="PostDetails">
       <div className="content" data-cy="PostDetails">
         <div className="block">
           <h2 data-cy="PostTitle">
-            #18: voluptate et itaque vero tempora molestiae
+            {`#${post?.id}: ${post?.title}`}
           </h2>
 
           <p data-cy="PostBody">
-            eveniet quo quis
-            laborum totam consequatur non dolor
-            ut et est repudiandae
-            est voluptatem vel debitis et magnam
+            {post?.body}
           </p>
         </div>
 
         <div className="block">
-          <Loader />
+          {isFetching && (
+            <Loader />
+          )}
 
-          <div className="notification is-danger" data-cy="CommentsError">
-            Something went wrong
-          </div>
-
-          <p className="title is-4" data-cy="NoCommentsMessage">
-            No comments yet
-          </p>
-
-          <p className="title is-4">Comments:</p>
-
-          <article className="message is-small" data-cy="Comment">
-            <div className="message-header">
-              <a href="mailto:misha@mate.academy" data-cy="CommentAuthor">
-                Misha Hrynko
-              </a>
-              <button
-                data-cy="CommentDelete"
-                type="button"
-                className="delete is-small"
-                aria-label="delete"
-              >
-                delete button
-              </button>
-            </div>
-
-            <div className="message-body" data-cy="CommentBody">
-              Some comment
-            </div>
-          </article>
-
-          <article className="message is-small" data-cy="Comment">
-            <div className="message-header">
-              <a
-                href="mailto:misha@mate.academy"
-                data-cy="CommentAuthor"
-              >
-                Misha Hrynko
-              </a>
-
-              <button
-                data-cy="CommentDelete"
-                type="button"
-                className="delete is-small"
-                aria-label="delete"
-              >
-                delete button
-              </button>
-            </div>
+          {(errorType && errorTypes.includes(errorType)) && (
             <div
-              className="message-body"
-              data-cy="CommentBody"
+              className="notification is-danger"
+              data-cy="CommentsError"
             >
-              One more comment
+              { errorType }
             </div>
-          </article>
+          )}
 
-          <article className="message is-small" data-cy="Comment">
-            <div className="message-header">
-              <a
-                href="mailto:misha@mate.academy"
-                data-cy="CommentAuthor"
-              >
-                Misha Hrynko
-              </a>
+          {(errorType !== ErrorTypes.COMMENTS) && (
+            <CommentsList
+              comments={comments}
+              setErrorType={setErrorType}
+              setComments={setComments}
+            />
+          )}
 
-              <button
-                data-cy="CommentDelete"
-                type="button"
-                className="delete is-small"
-                aria-label="delete"
-              >
-                delete button
-              </button>
-            </div>
-
-            <div className="message-body" data-cy="CommentBody">
-              {'Multi\nline\ncomment'}
-            </div>
-          </article>
-
-          <button
-            data-cy="WriteCommentButton"
-            type="button"
-            className="button is-link"
-          >
-            Write a comment
-          </button>
+          {!isCommentFormOpen && (
+            <button
+              data-cy="WriteCommentButton"
+              type="button"
+              className="button is-link"
+              onClick={handleOpenCommentForm}
+            >
+              Write a comment
+            </button>
+          )}
         </div>
 
-        <NewCommentForm />
+        {isCommentFormOpen && (
+          <NewCommentForm
+            post={post}
+            setComments={setComments}
+            setErrorType={setErrorType}
+          />
+        )}
       </div>
     </div>
   );
