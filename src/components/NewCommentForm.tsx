bbeba64a124fc2c216cc5ea0f9304f7cdc9d30ea
AndruhaMan <andreysut12@gmail.com>
2023-08-12 00:26:15 +0300

    disable eslint to choose reset type of button

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -197,7 +197,7 @@ export const NewCommentForm: React.FC<Props> = ({
         </div>
 
         <div className="control">
-           {/* eslint-disable-next-line react/button-has-type */}
+          {/* eslint-disable-next-line react/button-has-type */}
           <button
             type="reset"
             className="button is-link is-light"
