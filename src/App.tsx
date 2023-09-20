e649a95384b225cafbd9c4c7843f34ea9079fd94
Viktoriia Tkachyk <viko66666vs@gmail.com>
2023-09-20 18:37:01 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bulma/bulma.sass';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
@@ -8,8 +8,143 @@ import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
 import { Loader } from './components/Loader';
+import { User } from './types/User';
+import { getUsers } from './Api/users';
+import { Post } from './types/Post';
+import { getPosts } from './Api/posts';
+import { deleteComment, getComments } from './Api/comments';
+import { Comment } from './types/Comment';
+import { ErrorMessage } from './types/ErrorMessage';
 
 export const App: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+  const [isUserLoading, setIsUserLoading] = useState(true);
+
+  const [posts, setPosts] = useState<Post[] | null>(null);
+  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
+
+  const [comments, setCcomments] = useState<Comment[]>([]);
+  const [isCommentsLoading, setIsCommentsLoading] = useState(false);
+
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [apiErrors, setApiErrors] = useState({
+    userError: false,
+    postError: false,
+    commentsError: false,
+    deleteCommentError: false,
+  });
+
+  const {
+    userError,
+    postError,
+    commentsError,
+    deleteCommentError,
+  } = apiErrors;
+
+  const deleteCommentFromList = (deletedId: number) => {
+    setCcomments(comments.filter(comment => comment.id !== deletedId));
+  };
+
+  const addCommentToList = (newComment: Comment) => {
+    setCcomments((prevComments) => [...prevComments, newComment]);
+  };
+
+  const removeComment = (commentId: number) => {
+    setApiErrors((prevState) => ({ ...prevState, deleteCommentError: false }));
+    deleteCommentFromList(commentId);
+    deleteComment(commentId)
+      .catch(() => {
+        setCcomments(comments);
+        setApiErrors(
+          (prevState) => ({ ...prevState, deleteCommentError: true }),
+        );
+      });
+  };
+
+  const handleSelectUser = (user: User) => {
+    if (selectedUser?.id !== user.id) {
+      setSelectedPost(null);
+    }
+
+    setSelectedUser(user);
+  };
+
+  const handleIsForm = (isForm: boolean) => {
+    setIsFormVisible(isForm);
+  };
+
+  const handleSelectPost = (post: Post | null) => {
+    if (selectedPost?.id === post?.id) {
+      setSelectedPost(null);
+
+      return;
+    }
+
+    setSelectedPost(post);
+    setIsFormVisible(false);
+    setApiErrors((prevState) => ({ ...prevState, deleteCommentError: false }));
+  };
+
+  const loadUsers = async () => {
+    try {
+      const usersFromServer = await getUsers();
+
+      setUsers(usersFromServer);
+    } catch {
+      setApiErrors((prevState) => ({ ...prevState, userError: true }));
+    }
+  };
+
+  const loadPosts = async () => {
+    if (selectedUser) {
+      setIsUserLoading(true);
+
+      setApiErrors((prevState) => ({ ...prevState, postError: false }));
+      try {
+        const postsFromServer = await getPosts(selectedUser.id);
+
+        setPosts(postsFromServer);
+      } catch {
+        setApiErrors((prevState) => ({ ...prevState, postError: true }));
+      } finally {
+        setIsUserLoading(false);
+      }
+    }
+  };
+
+  const loadComments = async () => {
+    if (selectedPost) {
+      setApiErrors((prevState) => ({ ...prevState, commentsError: false }));
+
+      setIsCommentsLoading(true);
+      setCcomments([]);
+
+      try {
+        const commentsFromServer = await getComments(selectedPost.id);
+
+        setCcomments(commentsFromServer);
+      } catch {
+        setApiErrors((prevState) => ({ ...prevState, commentsError: true }));
+      } finally {
+        setIsCommentsLoading(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  useEffect(() => {
+    loadPosts();
+  }, [selectedUser]);
+
+  useEffect(() => {
+    loadComments();
+  }, [selectedPost]);
+
   return (
     <main className="section">
       <div className="container">
@@ -17,45 +152,93 @@ export const App: React.FC = () => {
           <div className="tile is-parent">
             <div className="tile is-child box is-success">
               <div className="block">
-                <UserSelector />
+                <UserSelector
+                  users={users}
+                  selectedUser={selectedUser}
+                  onUserSelect={handleSelectUser}
+                />
               </div>
 
               <div className="block" data-cy="MainContent">
-                <p data-cy="NoSelectedUser">
-                  No user selected
-                </p>
+                {userError && (
+                  <div
+                    className="notification is-danger"
+                    data-cy="PostsLoadingError"
+                  >
+                    {ErrorMessage.USERS}
+                  </div>
+                )}
 
-                <Loader />
+                {!selectedUser && !userError && (
+                  <p data-cy="NoSelectedUser">
+                    No user selected
+                  </p>
+                )}
 
-                <div
-                  className="notification is-danger"
-                  data-cy="PostsLoadingError"
-                >
-                  Something went wrong!
-                </div>
+                {selectedUser && (
+                  <>
+                    {isUserLoading && (
+                      <Loader />
+                    )}
 
-                <div className="notification is-warning" data-cy="NoPostsYet">
-                  No posts yet
-                </div>
-
-                <PostsList />
+                    {postError ? (
+                      <div
+                        className="notification is-danger"
+                        data-cy="PostsLoadingError"
+                      >
+                        {ErrorMessage.POSTS}
+                      </div>
+                    ) : (
+                      <>
+                        {!isUserLoading && !posts?.length
+                            && (
+                              <div
+                                className="notification is-warning"
+                                data-cy="NoPostsYet"
+                              >
+                                No posts yet
+                              </div>
+                            )}
+                        {!isUserLoading && !!posts?.length && (
+                          <PostsList
+                            posts={posts}
+                            selectedPost={selectedPost}
+                            onPostSelect={handleSelectPost}
+                          />
+                        )}
+                      </>
+                    )}
+                  </>
+                )}
               </div>
             </div>
           </div>
-
           <div
             data-cy="Sidebar"
             className={classNames(
               'tile',
               'is-parent',
               'is-8-desktop',
-              'Sidebar',
-              'Sidebar--open',
+              'Sidebar', {
+                'Sidebar--open': selectedPost,
+              },
             )}
           >
-            <div className="tile is-child box is-success ">
-              <PostDetails />
-            </div>
+            {selectedPost && (
+              <div className="tile is-child box is-success ">
+                <PostDetails
+                  selectedPost={selectedPost}
+                  comments={comments}
+                  isCommentsError={commentsError}
+                  isLoading={isCommentsLoading}
+                  isFormVisible={isFormVisible}
+                  isDeleteError={deleteCommentError}
+                  onDelete={removeComment}
+                  onAdd={addCommentToList}
+                  showForm={handleIsForm}
+                />
+              </div>
+            )}
           </div>
         </div>
       </div>
--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -1,117 +1,121 @@
 import React from 'react';
+import { Post } from '../types/Post';
+import { Comment } from '../types/Comment';
 import { Loader } from './Loader';
 import { NewCommentForm } from './NewCommentForm';
+import { ErrorMessage } from '../types/ErrorMessage';
 
-export const PostDetails: React.FC = () => {
+type Props = {
+  selectedPost: Post | null;
+  comments: Comment[];
+  isCommentsError: boolean;
+  isLoading: boolean;
+  isFormVisible: boolean;
+  isDeleteError: boolean;
+  onDelete: (commentId: number) => void;
+  onAdd: (comment: Comment) => void;
+  showForm: (isForm: boolean) => void;
+};
+
+export const PostDetails: React.FC<Props> = ({
+  selectedPost,
+  comments,
+  isCommentsError,
+  isLoading,
+  isFormVisible,
+  isDeleteError,
+  onDelete,
+  onAdd,
+  showForm,
+}) => {
+  const showCommentForm = () => showForm(true);
+
   return (
     <div className="content" data-cy="PostDetails">
       <div className="content" data-cy="PostDetails">
         <div className="block">
           <h2 data-cy="PostTitle">
-            #18: voluptate et itaque vero tempora molestiae
+            {`#${selectedPost?.id}: ${selectedPost?.title}`}
           </h2>
 
           <p data-cy="PostBody">
-            eveniet quo quis
-            laborum totam consequatur non dolor
-            ut et est repudiandae
-            est voluptatem vel debitis et magnam
+            {selectedPost?.body}
           </p>
         </div>
 
         <div className="block">
-          <Loader />
-
-          <div className="notification is-danger" data-cy="CommentsError">
-            Something went wrong
-          </div>
-
-          <p className="title is-4" data-cy="NoCommentsMessage">
-            No comments yet
-          </p>
-
-          <p className="title is-4">Comments:</p>
-
-          <article className="message is-small" data-cy="Comment">
-            <div className="message-header">
-              <a href="mailto:misha@mate.academy" data-cy="CommentAuthor">
-                Misha Hrynko
-              </a>
-              <button
-                data-cy="CommentDelete"
-                type="button"
-                className="delete is-small"
-                aria-label="delete"
-              >
-                delete button
-              </button>
+          {isLoading && (
+            <Loader />
+          )}
+          {isCommentsError ? (
+            <div className="notification is-danger" data-cy="CommentsError">
+              {ErrorMessage.COMMENTS}
             </div>
-
-            <div className="message-body" data-cy="CommentBody">
-              Some comment
-            </div>
-          </article>
-
-          <article className="message is-small" data-cy="Comment">
-            <div className="message-header">
-              <a
-                href="mailto:misha@mate.academy"
-                data-cy="CommentAuthor"
-              >
-                Misha Hrynko
-              </a>
-
-              <button
-                data-cy="CommentDelete"
-                type="button"
-                className="delete is-small"
-                aria-label="delete"
-              >
-                delete button
-              </button>
-            </div>
-            <div
-              className="message-body"
-              data-cy="CommentBody"
-            >
-              One more comment
-            </div>
-          </article>
-
-          <article className="message is-small" data-cy="Comment">
-            <div className="message-header">
-              <a
-                href="mailto:misha@mate.academy"
-                data-cy="CommentAuthor"
-              >
-                Misha Hrynko
-              </a>
-
-              <button
-                data-cy="CommentDelete"
-                type="button"
-                className="delete is-small"
-                aria-label="delete"
-              >
-                delete button
-              </button>
-            </div>
-
-            <div className="message-body" data-cy="CommentBody">
-              {'Multi\nline\ncomment'}
-            </div>
-          </article>
-
-          <button
-            data-cy="WriteCommentButton"
-            type="button"
-            className="button is-link"
-          >
-            Write a comment
-          </button>
+          ) : (
+            <>
+              {(!comments.length && !isLoading) && (
+                <p className="title is-4" data-cy="NoCommentsMessage">
+                  No comments yet
+                </p>
+              )}
+              {(comments.length > 0 && !isLoading) && (
+                <p className="title is-4">Comments:</p>
+              )}
+              {comments.map(comment => (
+                <article
+                  key={comment.id}
+                  className="message is-small"
+                  data-cy="Comment"
+                >
+                  <div className="message-header">
+                    <a
+                      href={comment.email}
+                      data-cy="CommentAuthor"
+                    >
+                      {comment.name}
+                    </a>
+                    <button
+                      data-cy="CommentDelete"
+                      type="button"
+                      className="delete is-small"
+                      aria-label="delete"
+                      onClick={() => onDelete(comment.id)}
+                    >
+                      delete button
+                    </button>
+                  </div>
+                  <div className="message-body" data-cy="CommentBody">
+                    {comment.body}
+                  </div>
+                </article>
+              ))}
+              {isDeleteError && (
+                <div
+                  className="notification is-danger"
+                  data-cy="PostsLoadingError"
+                >
+                  {ErrorMessage.DELETE}
+                </div>
+              )}
+              {(!isLoading && !isFormVisible) && (
+                <button
+                  data-cy="WriteCommentButton"
+                  type="button"
+                  className="button is-link"
+                  onClick={showCommentForm}
+                >
+                  Write a comment
+                </button>
+              )}
+            </>
+          )}
         </div>
-
-        <NewCommentForm />
+        {isFormVisible && (
+          <NewCommentForm
+            selectedPost={selectedPost}
+            onAdd={onAdd}
+          />
+        )}
       </div>
     </div>
   );
