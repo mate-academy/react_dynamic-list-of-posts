26c12ab112487f519d83e68920edafa44f5c244b
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2023-11-11 12:34:50 +0200

    add task solution

--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import cn from 'classnames';
 
-export const UserSelector: React.FC = () => {
+import * as service from '../api/service';
+
+import { User } from '../types/User';
+
+type Props = {
+  onSelectUser: (userId: number) => void;
+  selectedUser: User | null;
+  setSelectedUser: React.Dispatch<React.SetStateAction<User | null>>;
+};
+
+export const UserSelector: React.FC<Props> = ({
+  onSelectUser,
+  selectedUser,
+  setSelectedUser,
+}) => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  useEffect(() => {
+    service.getUsers()
+      .then(usersFromServer => setUsers(usersFromServer))
+      .catch(() => new Error());
+  }, []);
+
+  const handleClick = (user: User) => {
+    setSelectedUser(user);
+
+    if (selectedUser?.id !== user.id) {
+      onSelectUser(user.id);
+    }
+  };
+
   return (
     <div
       data-cy="UserSelector"
-      className="dropdown is-active"
+      className={cn('dropdown', {
+        'is-active': isOpenMenu,
+      })}
     >
       <div className="dropdown-trigger">
         <button
@@ -12,8 +46,10 @@ export const UserSelector: React.FC = () => {
           className="button"
           aria-haspopup="true"
           aria-controls="dropdown-menu"
+          onClick={() => setIsOpenMenu(!isOpenMenu)}
+          onBlur={() => setIsOpenMenu(false)}
         >
-          <span>Choose a user</span>
+          <span>{selectedUser?.name || 'Choose a user'}</span>
 
           <span className="icon is-small">
             <i className="fas fa-angle-down" aria-hidden="true" />
@@ -23,11 +59,18 @@ export const UserSelector: React.FC = () => {
 
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
         <div className="dropdown-content">
-          <a href="#user-1" className="dropdown-item">Leanne Graham</a>
-          <a href="#user-2" className="dropdown-item is-active">Ervin Howell</a>
-          <a href="#user-3" className="dropdown-item">Clementine Bauch</a>
-          <a href="#user-4" className="dropdown-item">Patricia Lebsack</a>
-          <a href="#user-5" className="dropdown-item">Chelsey Dietrich</a>
+          {users.map(user => (
+            <a
+              key={user.id}
+              href={`#user-${user.id}`}
+              className={cn('dropdown-item', {
+                'is-active': selectedUser?.id === user.id,
+              })}
+              onMouseDown={() => handleClick(user)}
+            >
+              {user.name}
+            </a>
+          ))}
         </div>
       </div>
     </div>
--- src/types/Comment.ts
+++ src/types/Comment.ts
@@ -1,6 +1,6 @@
 export interface Comment {
   id: number;
-  postId: number;
+  postId: number | undefined; // костиль тре подумати як виправити!!!
   name: string;
   email: string;
   body: string;
