bbf0919744d5c3063df657a80647efe7920e4e59
newqou <137334905+newqou@users.noreply.github.com>
2023-11-22 19:46:52 +0200

    updated

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -75,8 +75,6 @@ export const NewCommentForm: React.FC<Props> = ({
 
   return (
     <form data-cy="NewCommentForm" onSubmit={onAddComment}>
-      {/* ... (other form elements) ... */}
-
       <div className="field" data-cy="NameField">
         <label className="label" htmlFor="comment-author-name">
           Author Name
@@ -112,7 +110,62 @@ export const NewCommentForm: React.FC<Props> = ({
         )}
       </div>
 
-      {/* ... (similar changes for other fields) ... */}
+      <div className="field" data-cy="EmailField">
+        <label className="label" htmlFor="comment-author-email">
+          Author Email
+        </label>
+        <div className="control has-icons-left has-icons-right">
+          <input
+            type="text"
+            name="email"
+            id="comment-author-email"
+            placeholder="email@test.com"
+            className={cn('input', { 'is-danger': errors.email })}
+            value={comment.email}
+            onChange={(e) => handleChange('email', e.target.value)}
+          />
+
+          <span className="icon is-small is-left">
+            <i className="fas fa-envelope" />
+          </span>
+          <span
+            className="icon is-small is-right has-text-danger"
+            data-cy="ErrorIcon"
+          >
+            {errors.email && (
+              <i className="fas fa-exclamation-triangle" />
+            )}
+          </span>
+        </div>
+
+        {errors.email && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Email is required
+          </p>
+        )}
+      </div>
+
+      <div className="field" data-cy="BodyField">
+        <label className="label" htmlFor="comment-body">
+          Comment Text
+        </label>
+        <div className="control">
+          <textarea
+            id="comment-body"
+            name="body"
+            placeholder="Type comment here"
+            className={cn('textarea', { 'is-danger': errors.body })}
+            value={comment.body}
+            onChange={(e) => handleChange('body', e.target.value)}
+          />
+        </div>
+
+        {errors.body && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Enter some text
+          </p>
+        )}
+      </div>
 
       <div className="field is-grouped">
         <div className="control">
