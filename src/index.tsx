b9ddf8d29b116bbb627118358b02208c7501a3dd
Stanislav Vdovychenko <vdovychenkostas@gmail.com>
2023-12-03 17:53:19 +0200

    solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { TodosProvider } from './TodoContext';
 
 createRoot(document.getElementById('root') as HTMLElement)
-  .render(<App />);
+  .render(
+    <TodosProvider>
+      <App />
+    </TodosProvider>,
+  );
