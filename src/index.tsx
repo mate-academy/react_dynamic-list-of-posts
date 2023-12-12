e37a0810cfef2ec998a0ce1f4316e0f029326e53
Yurii Shkrobut <georgebest2409@gmail.com>
2023-12-12 12:36:14 +0200

    Index fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
+
 import { App } from './App';
 
-createRoot(document.getElementById('root') as HTMLElement)
-  .render(<App />);
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
