68d1391d754df008b17ac11fb696aff080e33639
Natalia Maryshko <104643607+natalia-maryshko@users.noreply.github.com>
2023-12-19 12:30:31 -0600

    Update PostDetails.tsx

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -94,29 +94,31 @@ export const PostDetails: React.FC<Props> = ({
               <p className="title is-4">Comments:</p>
             ))}
 
-          {!isLoading && comments?.map(comment => (
+           {!isLoading && comments?.map(({
+            id, email, name, body,
+           }) => (
             <article
               className="message is-small"
               data-cy="Comment"
-              key={comment.id}
+              key={id}
             >
               <div className="message-header">
-                <a href={`mailto:${comment.email}`} data-cy="CommentAuthor">
-                  {comment.name}
+                <a href={`mailto:${email}`} data-cy="CommentAuthor">
+                  {name}
                 </a>
                 <button
                   data-cy="CommentDelete"
                   type="button"
                   className="delete is-small"
                   aria-label="delete"
-                  onClick={() => onDelete(comment.id)}
+                  onClick={() => onDelete(id)}
                 >
                   delete button
                 </button>
               </div>
 
               <div className="message-body" data-cy="CommentBody">
-                {comment.body}
+                {body}
               </div>
             </article>
           ))}
