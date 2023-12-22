9c5337bca175fea0b13dfab444fc5c947f31240e
Belia Anton <belja.anton@gmail.com>
2023-12-22 15:28:59 +0200

    add react dynamic list of posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/bulma.sass';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
@@ -8,8 +8,39 @@ import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
 import { Loader } from './components/Loader';
+import { User } from './types/User';
+import { Post } from './types/Post';
+import { getPosts, getUsers } from './api/posts';
 
 export const App: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
+  const [visibleForm, setVisibleForm] = useState(false);
+
+  useEffect(() => {
+    getUsers()
+      .then(usersFromApi => setUsers(usersFromApi));
+  }, []);
+
+  useEffect(() => {
+    if (selectedUser) {
+      setIsLoading(true);
+      setError(false);
+
+      getPosts(selectedUser.id)
+        .then(postsUser => setPosts(postsUser))
+        .catch(() => setError(true))
+        .finally(() => setIsLoading(false));
+    }
+  }, [selectedUser]);
+
+  const conditionForNoPostsYet = !isLoading && !error
+    && !posts?.length && selectedUser;
+
   return (
     <main className="section">
       <div className="container">
@@ -17,28 +48,46 @@ export const App: React.FC = () => {
           <div className="tile is-parent">
             <div className="tile is-child box is-success">
               <div className="block">
-                <UserSelector />
+                <UserSelector
+                  users={users}
+                  selectedUser={selectedUser}
+                  setSelectedUser={setSelectedUser}
+                  setSelectedPost={setSelectedPost}
+                />
               </div>
 
               <div className="block" data-cy="MainContent">
-                <p data-cy="NoSelectedUser">
-                  No user selected
-                </p>
+                {!selectedUser && (
+                  <p data-cy="NoSelectedUser">
+                    No user selected
+                  </p>
+                )}
 
-                <Loader />
+                {isLoading && <Loader />}
 
-                <div
-                  className="notification is-danger"
-                  data-cy="PostsLoadingError"
-                >
-                  Something went wrong!
-                </div>
+                {!isLoading && error && (
+                  <div
+                    className="notification is-danger"
+                    data-cy="PostsLoadingError"
+                  >
+                    Something went wrong!
+                  </div>
+                )}
 
-                <div className="notification is-warning" data-cy="NoPostsYet">
-                  No posts yet
-                </div>
+                {conditionForNoPostsYet && (
+                  <div className="notification is-warning" data-cy="NoPostsYet">
+                    No posts yet
+                  </div>
+                )}
 
-                <PostsList />
+                {!!posts?.length && (
+                  <PostsList
+                    posts={posts}
+                    selectedPost={selectedPost}
+                    setSelectedPost={setSelectedPost}
+                    setVisibleForm={setVisibleForm}
+                  />
+                )}
               </div>
             </div>
           </div>
@@ -50,11 +99,17 @@ export const App: React.FC = () => {
               'is-parent',
               'is-8-desktop',
               'Sidebar',
-              'Sidebar--open',
+              { 'Sidebar--open': selectedPost },
             )}
           >
             <div className="tile is-child box is-success ">
-              <PostDetails />
+              {selectedPost && (
+                <PostDetails
+                  post={selectedPost}
+                  visibleForm={visibleForm}
+                  setVisibleForm={setVisibleForm}
+                />
+              )}
             </div>
           </div>
         </div>
--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
+import { User } from '../types/User';
+import { Post } from '../types/Post';
 
-export const UserSelector: React.FC = () => {
+type Props = {
+  users: User[];
+  selectedUser: User | null;
+  setSelectedUser: (user: User | null) => void;
+  setSelectedPost: (post: Post | null) => void;
+};
+
+export const UserSelector: React.FC<Props> = ({
+  users,
+  selectedUser,
+  setSelectedUser,
+  setSelectedPost,
+}) => {
+  const [visibleDropdown, setVisibleDropdown] = useState(false);
+
+  const handleUserSelect = (user: User) => {
+    setSelectedUser(user);
+    setSelectedPost(null);
+  };
+
   return (
     <div
       data-cy="UserSelector"
-      className="dropdown is-active"
+      className={cn('dropdown', { 'is-active': visibleDropdown })}
     >
       <div className="dropdown-trigger">
         <button
@@ -12,8 +34,14 @@ export const UserSelector: React.FC = () => {
           className="button"
           aria-haspopup="true"
           aria-controls="dropdown-menu"
+          onClick={() => setVisibleDropdown(!visibleDropdown)}
+          onBlur={() => setVisibleDropdown(false)}
         >
-          <span>Choose a user</span>
+          <span>
+            {!selectedUser
+              ? 'Choose a user'
+              : selectedUser.name}
+          </span>
 
           <span className="icon is-small">
             <i className="fas fa-angle-down" aria-hidden="true" />
@@ -23,11 +51,17 @@ export const UserSelector: React.FC = () => {
 
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
         <div className="dropdown-content">
-          <a href="#user-1" className="dropdown-item">Leanne Graham</a>
-          <a href="#user-2" className="dropdown-item is-active">Ervin Howell</a>
-          <a href="#user-3" className="dropdown-item">Clementine Bauch</a>
-          <a href="#user-4" className="dropdown-item">Patricia Lebsack</a>
-          <a href="#user-5" className="dropdown-item">Chelsey Dietrich</a>
+          {users.map(user => (
+            <a
+              key={user.id}
+              href={`#user-${user.id}`}
+              className={cn('dropdown-item',
+                { 'is-active': selectedUser?.id === user.id })}
+              onMouseDown={() => handleUserSelect(user)}
+            >
+              {user.name}
+            </a>
+          ))}
         </div>
       </div>
     </div>
