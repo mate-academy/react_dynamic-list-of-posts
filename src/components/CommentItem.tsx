3198c16d1ddbaf6afac6a18ba777142323b96759
jacekkosiba <jac.kosiba@gmail.com>
2024-01-14 23:09:05 +0100

    deleting posts

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -1,11 +1,27 @@
 import { FC } from 'react';
 import { Comment } from '../types/Comment';
+import { client } from '../utils/fetchClient';
+import { useCommentContext } from '../context/CommentContext';
 
 type Props = {
   comment: Comment,
 };
 
 export const CommentItem: FC<Props> = ({ comment }) => {
+  const { setComments } = useCommentContext();
+
+  const handleCommentDelete = async () => {
+    try {
+      setComments(prev => prev
+        .filter(prevComment => prevComment.id !== comment.id));
+
+      await client.delete(`/comments/${comment.id}`);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
+  };
+
   return (
     <article className="message is-small" data-cy="Comment">
       <div className="message-header">
@@ -17,6 +33,7 @@ export const CommentItem: FC<Props> = ({ comment }) => {
           type="button"
           className="delete is-small"
           aria-label="delete"
+          onClick={handleCommentDelete}
         >
           delete button
         </button>
