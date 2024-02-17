5989846b53ca6f9ea72283a1101f4a28610f2186
Yevhen Baiev <baev838@gmail.com>
2024-02-17 10:53:51 +0200

    Initial commit

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,86 +1,70 @@
 import React from 'react';
+import cn from 'classnames';
+import { Post } from '../types/Post';
 
-export const PostsList: React.FC = () => (
-  <div data-cy="PostsList">
-    <p className="title">Posts:</p>
+type Props = {
+  posts: Post[];
+  setSelectedPost: React.Dispatch<React.SetStateAction<Post | null>>;
+  selectedPost: Post | null;
+};
 
-    <table className="table is-fullwidth is-striped is-hoverable is-narrow">
-      <thead>
-        <tr className="has-background-link-light">
-          <th>#</th>
-          <th>Title</th>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <th> </th>
-        </tr>
-      </thead>
+export const PostsList: React.FC<Props> = ({
+  posts,
+  setSelectedPost,
+  selectedPost,
+}) => {
+  const handleOnPostButtonClick = (post: Post) => {
+    setSelectedPost(prev => {
+      if (prev?.id === post.id) {
+        return null;
+      }
 
-      <tbody>
-        <tr data-cy="Post">
-          <td data-cy="PostId">17</td>
+      return post;
+    });
+  };
 
-          <td data-cy="PostTitle">
-            fugit voluptas sed molestias voluptatem provident
-          </td>
+  return (
+    <div data-cy="PostsList">
+      <p className="title">Posts:</p>
 
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
+      <table className="table is-fullwidth is-striped is-hoverable is-narrow">
+        <thead>
+          <tr className="has-background-link-light">
+            <th>#</th>
+            <th>Title</th>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <th> </th>
+          </tr>
+        </thead>
 
-        <tr data-cy="Post">
-          <td data-cy="PostId">18</td>
+        <tbody>
+          {posts.map(post => {
+            const { id, title } = post;
+            const selected = selectedPost?.id !== id;
 
-          <td data-cy="PostTitle">
-            voluptate et itaque vero tempora molestiae
-          </td>
+            return (
+              <tr data-cy="Post" key={id}>
+                <td data-cy="PostId">{id}</td>
 
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link"
-            >
-              Close
-            </button>
-          </td>
-        </tr>
+                <td data-cy="PostTitle">
+                  {title}
+                </td>
 
-        <tr data-cy="Post">
-          <td data-cy="PostId">19</td>
-          <td data-cy="PostTitle">adipisci placeat illum aut reiciendis qui</td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
-
-        <tr data-cy="Post">
-          <td data-cy="PostId">20</td>
-          <td data-cy="PostTitle">doloribus ad provident suscipit at</td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+                <td className="has-text-right is-vcentered">
+                  <button
+                    type="button"
+                    data-cy="PostButton"
+                    className={cn('button is-link', { 'is-light': selected })}
+                    onClick={() => handleOnPostButtonClick(post)}
+                  >
+                    {selected ? 'Open' : 'Close'}
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { User } from '../types/User';
+import { UserInfo } from './UserInfo';
 
-export const UserSelector: React.FC = () => {
+type Props = {
+  setSelectedUser: React.Dispatch<React.SetStateAction<User | null>>;
+  users: User[];
+  selectedUser: User | null;
+};
+
+export const UserSelector: React.FC<Props> = ({
+  selectedUser, users, setSelectedUser,
+}) => {
+  const [isActive, setIsActive] = useState(false);
+  const buttonText = selectedUser ? selectedUser.name : 'Choose a user';
+
   return (
     <div
       data-cy="UserSelector"
@@ -12,8 +25,10 @@ export const UserSelector: React.FC = () => {
           className="button"
           aria-haspopup="true"
           aria-controls="dropdown-menu"
+          onBlur={() => setIsActive(false)}
+          onClick={() => setIsActive(prev => !prev)}
         >
-          <span>Choose a user</span>
+          <span>{buttonText}</span>
 
           <span className="icon is-small">
             <i className="fas fa-angle-down" aria-hidden="true" />
@@ -21,15 +36,20 @@ export const UserSelector: React.FC = () => {
         </button>
       </div>
 
-      <div className="dropdown-menu" id="dropdown-menu" role="menu">
-        <div className="dropdown-content">
-          <a href="#user-1" className="dropdown-item">Leanne Graham</a>
-          <a href="#user-2" className="dropdown-item is-active">Ervin Howell</a>
-          <a href="#user-3" className="dropdown-item">Clementine Bauch</a>
-          <a href="#user-4" className="dropdown-item">Patricia Lebsack</a>
-          <a href="#user-5" className="dropdown-item">Chelsey Dietrich</a>
+      {isActive && (
+        <div className="dropdown-menu" id="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+            {users.map(user => (
+              <UserInfo
+                key={user.id}
+                setSelectedUser={setSelectedUser}
+                selectedUser={selectedUser}
+                user={user}
+              />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
