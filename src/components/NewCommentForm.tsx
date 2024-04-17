ffc668c8d706ecb963656c9700e4fdf4dd60e3e9
Ivan <tixehack@gmail.com>
2024-04-17 12:58:38 +0300

    fix issues by mentor 2

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -126,7 +126,7 @@ export const NewCommentForm: React.FC = () => {
 
         <div className="control has-icons-left has-icons-right">
           <input
-            type="text"
+            type="email"
             name="email"
             id="comment-author-email"
             placeholder="email@test.com"
