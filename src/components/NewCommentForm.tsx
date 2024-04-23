644fc4af374611cba6ef6789547ca38eb19ecf6a
deezhizyu <kalenskuja@gmail.com>
2024-04-23 16:55:41 +0200

    Fix email input type

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -128,7 +128,7 @@ export const NewCommentForm: React.FC<Props> = ({
 
         <div className="control has-icons-left has-icons-right">
           <input
-            type="text"
+            type="email"
             name="email"
             id="comment-author-email"
             placeholder="email@test.com"
