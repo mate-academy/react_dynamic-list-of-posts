dfc3e760c9a150cb335c75598b91db80e9c7a95a
mrsvolodya <vovamursky@gmail.com>
2024-04-27 20:10:00 +0300

    skipped three errors

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -1,8 +1,62 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/indent */
+import React, { useContext, useState } from 'react';
+import { getComments, postComment } from '../api/lists';
+import { ContextList } from './ListProvider/ListProvider';
+import classNames from 'classnames';
+
+const initialFormState = { name: '', email: '', body: '' };
 
 export const NewCommentForm: React.FC = () => {
+  const { selectPost, setComments } = useContext(ContextList);
+  const [comment, setComment] = useState(initialFormState);
+  const [error, setError] = useState(false);
+  const [spinner, setSpinner] = useState(false);
+
+  const onReset = () => {
+    setError(false);
+    setComment(initialFormState);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    setError(!error);
+    setSpinner(true);
+    e.preventDefault();
+
+    if (!comment.name.trim() || !comment.email.trim() || !comment.body.trim()) {
+      setSpinner(false);
+
+      return;
+    }
+
+    const newComment = {
+      ...comment,
+      postId: selectPost?.id,
+    };
+
+    if (selectPost) {
+      postComment(newComment)
+        .then(() => getComments(selectPost.id))
+        .then(setComments)
+        .finally(() => {
+          setSpinner(false);
+          setComment({ ...comment, body: '' });
+          setError(false);
+        });
+    }
+  };
+
+  const handlerInput = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
+
+    setComment(prevData => ({ ...prevData, [name]: value }));
+  };
+
   return (
-    <form data-cy="NewCommentForm">
+    <form data-cy="NewCommentForm" onSubmit={onSubmit}>
       <div className="field" data-cy="NameField">
         <label className="label" htmlFor="comment-author-name">
           Author Name
@@ -14,24 +68,31 @@ export const NewCommentForm: React.FC = () => {
             name="name"
             id="comment-author-name"
             placeholder="Name Surname"
-            className="input is-danger"
+            className={classNames('input', {
+              'is-danger': !comment.name && error,
+            })}
+            value={comment.name}
+            onChange={handlerInput}
           />
 
           <span className="icon is-small is-left">
             <i className="fas fa-user" />
           </span>
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+          {!comment.name && error && (
+            <span
+              className="icon is-small is-right has-text-danger"
+              data-cy="ErrorIcon"
+            >
+              <i className="fas fa-exclamation-triangle" />
+            </span>
+          )}
         </div>
-
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Name is required
-        </p>
+        {!comment.name && error && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Name is required
+          </p>
+        )}
       </div>
 
       <div className="field" data-cy="EmailField">
@@ -45,24 +106,37 @@ export const NewCommentForm: React.FC = () => {
             name="email"
             id="comment-author-email"
             placeholder="email@test.com"
-            className="input is-danger"
+            className={classNames('input', {
+              'is-danger': !comment.email && error,
+            })}
+            value={comment.email}
+            onChange={handlerInput}
           />
 
           <span className="icon is-small is-left">
             <i className="fas fa-envelope" />
           </span>
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+          {!comment.email && error && (
+            <span
+              className="icon is-small is-right has-text-danger"
+              data-cy="ErrorIcon"
+            >
+              <i className="fas fa-exclamation-triangle" />
+            </span>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Email is required
-        </p>
+        {!comment.email && error && (
+          <p
+            className={classNames('help', {
+              'is-danger': !comment.email && error,
+            })}
+            data-cy="ErrorMessage"
+          >
+            Email is required
+          </p>
+        )}
       </div>
 
       <div className="field" data-cy="BodyField">
@@ -75,25 +149,43 @@ export const NewCommentForm: React.FC = () => {
             id="comment-body"
             name="body"
             placeholder="Type comment here"
-            className="textarea is-danger"
+            className={classNames('textarea', {
+              'is-danger': !comment.body && error,
+            })}
+            value={comment.body}
+            onChange={handlerInput}
           />
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Enter some text
-        </p>
+        {!comment.body && error && (
+          <p
+            className={classNames('help', {
+              'is-danger': !comment.body && error,
+            })}
+            data-cy="ErrorMessage"
+          >
+            Enter some text
+          </p>
+        )}
       </div>
 
       <div className="field is-grouped">
         <div className="control">
-          <button type="submit" className="button is-link is-loading">
+          <button
+            type="submit"
+            className={classNames('button is-link ', { 'is-loading': spinner })}
+          >
+            {' '}
             Add
           </button>
         </div>
 
         <div className="control">
-          {/* eslint-disable-next-line react/button-has-type */}
-          <button type="reset" className="button is-link is-light">
+          <button
+            type="reset"
+            className="button is-link is-light"
+            onClick={onReset}
+          >
             Clear
           </button>
         </div>
--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { ContextList } from './ListProvider/ListProvider';
+import cn from 'classnames';
 
 export const UserSelector: React.FC = () => {
+  const { usersList, selectedUser, setSelectPost, handlePostsOfSelectUser } =
+    useContext(ContextList);
+
+  const [isActive, setIsActive] = useState(false);
+
+  const handleDropdown = () => {
+    setSelectPost(null);
+    setIsActive(!isActive);
+  };
+
   return (
-    <div data-cy="UserSelector" className="dropdown is-active">
-      <div className="dropdown-trigger">
+    <div
+      data-cy="UserSelector"
+      className={cn('dropdown', { 'is-active': isActive })}
+      onBlur={() => setIsActive(false)}
+    >
+      <div className="dropdown-trigger" onClick={handleDropdown}>
         <button
           type="button"
           className="button"
           aria-haspopup="true"
           aria-controls="dropdown-menu"
         >
-          <span>Choose a user</span>
+          <span>{selectedUser ? selectedUser.name : 'Choose a user'}</span>
 
           <span className="icon is-small">
             <i className="fas fa-angle-down" aria-hidden="true" />
@@ -20,21 +36,18 @@ export const UserSelector: React.FC = () => {
 
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
         <div className="dropdown-content">
-          <a href="#user-1" className="dropdown-item">
-            Leanne Graham
-          </a>
-          <a href="#user-2" className="dropdown-item is-active">
-            Ervin Howell
-          </a>
-          <a href="#user-3" className="dropdown-item">
-            Clementine Bauch
-          </a>
-          <a href="#user-4" className="dropdown-item">
-            Patricia Lebsack
-          </a>
-          <a href="#user-5" className="dropdown-item">
-            Chelsey Dietrich
-          </a>
+          {usersList.map(user => (
+            <a
+              key={user.id}
+              href={`#user-${user.id}`}
+              className={cn('dropdown-item', {
+                'is-active': user.id === selectedUser?.id,
+              })}
+              onMouseDown={() => handlePostsOfSelectUser(user)}
+            >
+              {user.name}
+            </a>
+          ))}
         </div>
       </div>
     </div>
