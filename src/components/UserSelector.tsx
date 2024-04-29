a74129c5d7149be03174550e9da92fa53c367c9b
Vladyslav Zakharov <vladyslavzakharovdk@gmail.com>
2024-04-29 23:20:24 +0300

    solution

--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { User } from '../types/User';
+import classNames from 'classnames';
+import { UserContext } from './UsersContext';
 
-export const UserSelector: React.FC = () => {
+interface Props {
+  activeUser: User | null;
+  onChange: (value: User) => void;
+}
+
+export const UserSelector: React.FC<Props> = ({ activeUser, onChange }) => {
+  const [expanded, setExpanded] = useState(false);
+  const users = useContext(UserContext);
+
+  useEffect(() => {
+    if (!expanded) {
+      return;
+    }
+
+    const handleDocumentClick = () => {
+      setExpanded(false);
+    };
+
+    document.addEventListener('click', handleDocumentClick);
+
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+    };
+  }, [expanded]);
+
   return (
-    <div data-cy="UserSelector" className="dropdown is-active">
+    <div
+      data-cy="UserSelector"
+      className={classNames('dropdown', { 'is-active': expanded })}
+    >
       <div className="dropdown-trigger">
         <button
           type="button"
           className="button"
           aria-haspopup="true"
           aria-controls="dropdown-menu"
+          onClick={e => {
+            e.stopPropagation();
+            setExpanded(current => !current);
+          }}
         >
-          <span>Choose a user</span>
+          <span>{activeUser?.name || 'Choose a user'}</span>
 
           <span className="icon is-small">
             <i className="fas fa-angle-down" aria-hidden="true" />
@@ -20,21 +54,18 @@ export const UserSelector: React.FC = () => {
 
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
         <div className="dropdown-content">
-          <a href="#user-1" className="dropdown-item">
-            Leanne Graham
-          </a>
-          <a href="#user-2" className="dropdown-item is-active">
-            Ervin Howell
-          </a>
-          <a href="#user-3" className="dropdown-item">
-            Clementine Bauch
-          </a>
-          <a href="#user-4" className="dropdown-item">
-            Patricia Lebsack
-          </a>
-          <a href="#user-5" className="dropdown-item">
-            Chelsey Dietrich
-          </a>
+          {users.map(user => (
+            <a
+              href={`#user-${user.id}`}
+              className={classNames('dropdown-item', {
+                'is-active': user.id === activeUser?.id,
+              })}
+              key={user.id}
+              onClick={() => onChange(user)}
+            >
+              {user.name}
+            </a>
+          ))}
         </div>
       </div>
     </div>
