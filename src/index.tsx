e3351cf48775da96f4d151a52299395e4ad30e59
Mykyta  Romanishyn <mromanishyn@medenvoyglobal.com>
2024-08-25 20:10:32 +0200

    modules_added

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { SelectedUserProvider } from './providers/UserProvider';
 import { SelectedPostProvider } from './providers/PostProvider';
+import React from 'react';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <SelectedUserProvider>
