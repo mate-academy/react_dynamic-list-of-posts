9eec5b8bf4fc95d200fb8a66a58f51a74b912f7c
Marina Avramenko <marina.avramenko123@gmail.com>
2024-08-29 15:13:40 +0300

    add task solution

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -1,103 +1,219 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import { Comment } from '../types/Comment';
+import { addComment } from '../utils/fetchClient';
+import cn from 'classnames';
 
-export const NewCommentForm: React.FC = () => {
+type Props = {
+  postId: number;
+  updateComent: React.Dispatch<React.SetStateAction<Comment[]>>;
+};
+
+export const NewCommentForm: React.FC<Props> = ({ postId, updateComent }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [body, setBody] = useState('');
+  const [isError, setIsError] = useState(false);
+  const [hasNameError, setHasNameError] = useState(false);
+  const [hasEmailError, setHasEmailError] = useState(false);
+  const [hasBodyError, setHasBodyError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getCorrectValue = (value: string) => {
+    return value.trim();
+  };
+
+  const handleOnChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    if (event.target.name === 'name') {
+      setName(event.target.value);
+      setHasNameError(false);
+    }
+
+    if (event.target.name === 'email') {
+      setEmail(event.target.value);
+      setHasEmailError(false);
+    }
+
+    if (event.target.name === 'body') {
+      setBody(event.target.value);
+      setHasBodyError(false);
+    }
+  };
+
+  const resetForm = () => {
+    setName('');
+    setEmail('');
+    setBody('');
+    setHasNameError(false);
+    setHasEmailError(false);
+    setHasBodyError(false);
+  };
+
+  const handleAddComment = (event: FormEvent) => {
+    event.preventDefault();
+
+    const correctName = getCorrectValue(name);
+    const correctEmail = getCorrectValue(email);
+    const correctBody = getCorrectValue(body);
+
+    setHasNameError(!correctName);
+    setHasEmailError(!correctEmail);
+    setHasBodyError(!correctBody);
+
+    if (!correctName || !correctEmail || !correctBody) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    addComment({
+      postId,
+      name: correctName,
+      email: correctEmail,
+      body: correctBody,
+    })
+      .then(comment => {
+        updateComent(currentComment => [...currentComment, comment]);
+        setBody('');
+      })
+      .catch(() => {
+        setIsError(true);
+        setTimeout(() => {
+          setIsError(false);
+        }, 2000);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
   return (
-    <form data-cy="NewCommentForm">
-      <div className="field" data-cy="NameField">
-        <label className="label" htmlFor="comment-author-name">
-          Author Name
-        </label>
+    <>
+      {isError && (
+        <div className="notification is-danger" data-cy="AddCommentError">
+          Something went wrong!
+        </div>
+      )}
 
-        <div className="control has-icons-left has-icons-right">
-          <input
-            type="text"
-            name="name"
-            id="comment-author-name"
-            placeholder="Name Surname"
-            className="input is-danger"
-          />
+      <form
+        data-cy="NewCommentForm"
+        onSubmit={handleAddComment}
+        onReset={resetForm}
+      >
+        <div className="field" data-cy="NameField">
+          <label className="label" htmlFor="comment-author-name">
+            Author Name
+          </label>
 
-          <span className="icon is-small is-left">
-            <i className="fas fa-user" />
-          </span>
+          <div className="control has-icons-left has-icons-right">
+            <input
+              type="text"
+              name="name"
+              value={name}
+              id="comment-author-name"
+              placeholder="Name Surname"
+              className={cn('input', { 'is-danger': hasNameError })}
+              onChange={handleOnChange}
+            />
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+            <span className="icon is-small is-left">
+              <i className="fas fa-user" />
+            </span>
+
+            {hasNameError && (
+              <span
+                className="icon is-small is-right has-text-danger"
+                data-cy="ErrorIcon"
+              >
+                <i className="fas fa-exclamation-triangle" />
+              </span>
+            )}
+          </div>
+
+          {hasNameError && (
+            <p className="help is-danger" data-cy="ErrorMessage">
+              Name is required
+            </p>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Name is required
-        </p>
-      </div>
+        <div className="field" data-cy="EmailField">
+          <label className="label" htmlFor="comment-author-email">
+            Author Email
+          </label>
 
-      <div className="field" data-cy="EmailField">
-        <label className="label" htmlFor="comment-author-email">
-          Author Email
-        </label>
+          <div className="control has-icons-left has-icons-right">
+            <input
+              type="text"
+              value={email}
+              name="email"
+              id="comment-author-email"
+              placeholder="email@test.com"
+              className={cn('input', { 'is-danger': hasEmailError })}
+              onChange={handleOnChange}
+            />
 
-        <div className="control has-icons-left has-icons-right">
-          <input
-            type="text"
-            name="email"
-            id="comment-author-email"
-            placeholder="email@test.com"
-            className="input is-danger"
-          />
+            <span className="icon is-small is-left">
+              <i className="fas fa-envelope" />
+            </span>
 
-          <span className="icon is-small is-left">
-            <i className="fas fa-envelope" />
-          </span>
+            {hasEmailError && (
+              <span
+                className="icon is-small is-right has-text-danger"
+                data-cy="ErrorIcon"
+              >
+                <i className="fas fa-exclamation-triangle" />
+              </span>
+            )}
+          </div>
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+          {hasEmailError && (
+            <p className="help is-danger" data-cy="ErrorMessage">
+              Email is required
+            </p>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Email is required
-        </p>
-      </div>
+        <div className="field" data-cy="BodyField">
+          <label className="label" htmlFor="comment-body">
+            Comment Text
+          </label>
 
-      <div className="field" data-cy="BodyField">
-        <label className="label" htmlFor="comment-body">
-          Comment Text
-        </label>
+          <div className="control">
+            <textarea
+              id="comment-body"
+              value={body}
+              name="body"
+              placeholder="Type comment here"
+              className={cn('textarea', { 'is-danger': hasEmailError })}
+              onChange={handleOnChange}
+            />
+          </div>
 
-        <div className="control">
-          <textarea
-            id="comment-body"
-            name="body"
-            placeholder="Type comment here"
-            className="textarea is-danger"
-          />
+          {hasBodyError && (
+            <p className="help is-danger" data-cy="ErrorMessage">
+              Enter some text
+            </p>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Enter some text
-        </p>
-      </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="submit"
+              className={cn('button is-link', { 'is-loading': isLoading })}
+            >
+              Add
+            </button>
+          </div>
 
-      <div className="field is-grouped">
-        <div className="control">
-          <button type="submit" className="button is-link is-loading">
-            Add
-          </button>
+          <div className="control">
+            {/* eslint-disable-next-line react/button-has-type */}
+            <button type="reset" className="button is-link is-light">
+              Clear
+            </button>
+          </div>
         </div>
-
-        <div className="control">
-          {/* eslint-disable-next-line react/button-has-type */}
-          <button type="reset" className="button is-link is-light">
-            Clear
-          </button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,86 +1,65 @@
 import React from 'react';
+import { Post } from '../types/Post';
+import cn from 'classnames';
 
-export const PostsList: React.FC = () => (
-  <div data-cy="PostsList">
-    <p className="title">Posts:</p>
+type Props = {
+  selectedUserPost: Post[];
+  selectedPost: Post | null;
+  setSelectedPost: (post: Post | null) => void;
+};
 
-    <table className="table is-fullwidth is-striped is-hoverable is-narrow">
-      <thead>
-        <tr className="has-background-link-light">
-          <th>#</th>
-          <th>Title</th>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <th> </th>
-        </tr>
-      </thead>
+export const PostsList: React.FC<Props> = ({
+  selectedUserPost,
+  selectedPost,
+  setSelectedPost,
+}) => {
+  const handleSelectedUserPost = (post: Post) => {
+    if (selectedPost?.id === post.id) {
+      setSelectedPost(null);
+    } else {
+      setSelectedPost(post);
+    }
+  };
 
-      <tbody>
-        <tr data-cy="Post">
-          <td data-cy="PostId">17</td>
+  return (
+    <div data-cy="PostsList">
+      <p className="title">Posts:</p>
 
-          <td data-cy="PostTitle">
-            fugit voluptas sed molestias voluptatem provident
-          </td>
+      <table className="table is-fullwidth is-striped is-hoverable is-narrow">
+        <thead>
+          <tr className="has-background-link-light">
+            <th>#</th>
+            <th>Title</th>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <th> </th>
+          </tr>
+        </thead>
 
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
+        <tbody>
+          {selectedUserPost.map(userPost => {
+            const { id, title } = userPost;
 
-        <tr data-cy="Post">
-          <td data-cy="PostId">18</td>
-
-          <td data-cy="PostTitle">
-            voluptate et itaque vero tempora molestiae
-          </td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link"
-            >
-              Close
-            </button>
-          </td>
-        </tr>
-
-        <tr data-cy="Post">
-          <td data-cy="PostId">19</td>
-          <td data-cy="PostTitle">adipisci placeat illum aut reiciendis qui</td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
-
-        <tr data-cy="Post">
-          <td data-cy="PostId">20</td>
-          <td data-cy="PostTitle">doloribus ad provident suscipit at</td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+            return (
+              <tr data-cy="Post" key={id}>
+                <td data-cy="PostId">{id}</td>
+                <td data-cy="PostTitle"> {title}</td>
+                <td className="has-text-right is-vcentered">
+                  <button
+                    type="button"
+                    data-cy="PostButton"
+                    className={cn('button is-link', {
+                      'is-light': selectedPost?.id !== id,
+                    })}
+                    onClick={() => handleSelectedUserPost(userPost)}
+                  >
+                    {selectedPost?.id === id ? 'Close' : 'Open'}
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
