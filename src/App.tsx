a2102dab86651f4f8e582f16fc2be9919a9933a4
Pavlo Pukalo <misterkatlet@gmail.com>
2024-10-09 23:40:49 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -8,53 +8,96 @@ import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
 import { Loader } from './components/Loader';
+import { useEffect, useState } from 'react';
+import { Post } from './types/Post';
+import { User } from './types/User';
+import { getUsersPosts } from './components/api/users';
 
-export const App = () => (
-  <main className="section">
-    <div className="container">
-      <div className="tile is-ancestor">
-        <div className="tile is-parent">
-          <div className="tile is-child box is-success">
-            <div className="block">
-              <UserSelector />
-            </div>
+export const App = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isErrorPosts, setIsErrorPosts] = useState(false);
+  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
 
-            <div className="block" data-cy="MainContent">
-              <p data-cy="NoSelectedUser">No user selected</p>
+  const isNoPosts =
+    selectedUser && posts.length === 0 && !isLoading && !isErrorPosts;
 
-              <Loader />
+  useEffect(() => {
+    setIsErrorPosts(false);
+    if (selectedUser) {
+      setIsLoading(true);
+      getUsersPosts(selectedUser.id)
+        .then(setPosts)
+        .catch(() => setIsErrorPosts(true))
+        .finally(() => setIsLoading(false));
+    }
+  }, [selectedUser]);
 
-              <div
-                className="notification is-danger"
-                data-cy="PostsLoadingError"
-              >
-                Something went wrong!
+  return (
+    <main className="section">
+      <div className="container">
+        <div className="tile is-ancestor">
+          <div className="tile is-parent">
+            <div className="tile is-child box is-success">
+              <div className="block">
+                <UserSelector
+                  selectedUser={selectedUser}
+                  onSelectedUser={setSelectedUser}
+                  onSelectedPost={setSelectedPost}
+                />
               </div>
 
-              <div className="notification is-warning" data-cy="NoPostsYet">
-                No posts yet
-              </div>
+              <div className="block" data-cy="MainContent">
+                {!selectedUser && !isErrorPosts && (
+                  <p data-cy="NoSelectedUser">No user selected</p>
+                )}
 
-              <PostsList />
+                {isLoading && <Loader />}
+
+                {isErrorPosts && (
+                  <div
+                    className="notification is-danger"
+                    data-cy="PostsLoadingError"
+                  >
+                    Something went wrong!
+                  </div>
+                )}
+
+                {posts.length > 0 && !isLoading && !isErrorPosts && (
+                  <PostsList
+                    posts={posts}
+                    selectedPost={selectedPost}
+                    onSelectedPost={setSelectedPost}
+                  />
+                )}
+                {isNoPosts && (
+                  <div className="notification is-warning" data-cy="NoPostsYet">
+                    No posts yet
+                  </div>
+                )}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div
-          data-cy="Sidebar"
-          className={classNames(
-            'tile',
-            'is-parent',
-            'is-8-desktop',
-            'Sidebar',
-            'Sidebar--open',
-          )}
-        >
-          <div className="tile is-child box is-success ">
-            <PostDetails />
+          <div
+            data-cy="Sidebar"
+            className={classNames(
+              'tile',
+              'is-parent',
+              'is-8-desktop',
+              'Sidebar',
+              { 'Sidebar--open': selectedPost },
+            )}
+          >
+            {selectedPost && (
+              <div className="tile is-child box is-success ">
+                <PostDetails selectedPost={selectedPost} />
+              </div>
+            )}
           </div>
         </div>
       </div>
-    </div>
-  </main>
-);
+    </main>
+  );
+};
--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import { Comment } from '../types/Comment';
+import classNames from 'classnames';
+import { Post } from '../types/Post';
 
-export const NewCommentForm: React.FC = () => {
+type Props = {
+  onAdd: (val: Omit<Comment, 'id'>) => Promise<void>;
+  selectedPost: Post | null;
+};
+
+export const NewCommentForm: React.FC<Props> = ({ onAdd, selectedPost }) => {
+  const [isSubmiting, setIsSubmiting] = useState(false);
+
+  const [name, setName] = useState('');
+  const [isErrorName, setIsErrorName] = useState(false);
+
+  const [email, setEmail] = useState('');
+  const [isErrorEmail, setIsErrorEmail] = useState(false);
+
+  const [comment, setComment] = useState('');
+  const [isErrorComment, setIsErrorComment] = useState(false);
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+    setIsErrorName(false);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setIsErrorEmail(false);
+  };
+
+  const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+    setIsErrorComment(false);
+  };
+
+  const handleReset = () => {
+    setComment('');
+    setEmail('');
+    setName('');
+    setIsErrorComment(false);
+    setIsErrorEmail(false);
+    setIsErrorName(false);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    setIsErrorName(!name);
+    setIsErrorEmail(!email);
+    setIsErrorComment(!comment);
+
+    if (!name || !email || !comment) {
+      return;
+    }
+
+    setIsSubmiting(true);
+    onAdd({
+      name: name.trim(),
+      email: email.trim(),
+      body: comment.trim(),
+      postId: selectedPost?.id || 0,
+    }).finally(() => {
+      setComment('');
+      setIsSubmiting(false);
+      setIsErrorComment(false);
+      setIsErrorEmail(false);
+      setIsErrorName(false);
+    });
+  };
+
   return (
-    <form data-cy="NewCommentForm">
+    <form data-cy="NewCommentForm" onSubmit={handleSubmit}>
       <div className="field" data-cy="NameField">
         <label className="label" htmlFor="comment-author-name">
           Author Name
@@ -14,24 +83,30 @@ export const NewCommentForm: React.FC = () => {
             name="name"
             id="comment-author-name"
             placeholder="Name Surname"
-            className="input is-danger"
+            className={classNames('input', { 'is-danger': isErrorName })}
+            value={name}
+            onChange={handleNameChange}
           />
 
           <span className="icon is-small is-left">
             <i className="fas fa-user" />
           </span>
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+          {isErrorName && (
+            <span
+              className="icon is-small is-right has-text-danger"
+              data-cy="ErrorIcon"
+            >
+              <i className="fas fa-exclamation-triangle" />
+            </span>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Name is required
-        </p>
+        {isErrorName && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Name is required
+          </p>
+        )}
       </div>
 
       <div className="field" data-cy="EmailField">
@@ -45,24 +120,30 @@ export const NewCommentForm: React.FC = () => {
             name="email"
             id="comment-author-email"
             placeholder="email@test.com"
-            className="input is-danger"
+            className={classNames('input', { 'is-danger': isErrorEmail })}
+            value={email}
+            onChange={handleEmailChange}
           />
 
           <span className="icon is-small is-left">
             <i className="fas fa-envelope" />
           </span>
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+          {isErrorEmail && (
+            <span
+              className="icon is-small is-right has-text-danger"
+              data-cy="ErrorIcon"
+            >
+              <i className="fas fa-exclamation-triangle" />
+            </span>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Email is required
-        </p>
+        {isErrorEmail && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Email is required
+          </p>
+        )}
       </div>
 
       <div className="field" data-cy="BodyField">
@@ -75,25 +156,39 @@ export const NewCommentForm: React.FC = () => {
             id="comment-body"
             name="body"
             placeholder="Type comment here"
-            className="textarea is-danger"
+            className={classNames('textarea', { 'is-danger': isErrorComment })}
+            value={comment}
+            onChange={handleCommentChange}
           />
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Enter some text
-        </p>
+        {isErrorComment && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Enter some text
+          </p>
+        )}
       </div>
 
       <div className="field is-grouped">
         <div className="control">
-          <button type="submit" className="button is-link is-loading">
+          <button
+            type="submit"
+            className={classNames('button is-link', {
+              'is-loading': isSubmiting,
+            })}
+          >
             Add
           </button>
         </div>
 
         <div className="control">
           {/* eslint-disable-next-line react/button-has-type */}
-          <button type="reset" className="button is-link is-light">
+          <button
+            type="reset"
+            className="button is-link is-light"
+            disabled={isSubmiting}
+            onClick={handleReset}
+          >
             Clear
           </button>
         </div>
