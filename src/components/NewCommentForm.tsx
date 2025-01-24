6d885679824336d89eb3f437bdd1a509c4748463
«Polina <polinabondar4@gmail.com>
2025-01-24 12:05:11 +0200

    add task solution

--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -1,8 +1,86 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { FormEvent, useState } from 'react';
 
-export const NewCommentForm: React.FC = () => {
+type Props = {
+  addComment: (name: string, email: string, body: string) => Promise<void>;
+};
+
+const getTrimmedValue = (value: string): string => {
+  return value.trim();
+};
+
+const isTrimmedInputEmpty = (value: string): boolean => {
+  return getTrimmedValue(value) === '';
+};
+
+const isValidEmail = (email: string): boolean => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  return emailRegex.test(getTrimmedValue(email));
+};
+
+export const NewCommentForm: React.FC<Props> = ({ addComment }) => {
+  const [isNameError, setIsNameError] = useState(false);
+  const [isEmailError, setIsEmailError] = useState(false);
+  const [isBodyError, setIsBodyError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [bodyText, setBodyText] = useState('');
+
+  const clearInputs = () => {
+    setName('');
+    setEmail('');
+    setBodyText('');
+  };
+
+  const clearErrors = () => {
+    setIsNameError(false);
+    setIsEmailError(false);
+    setIsBodyError(false);
+  };
+
+  const handleSubmitComment = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    let hasValidationErrors = false;
+
+    if (isTrimmedInputEmpty(name)) {
+      setIsNameError(true);
+      hasValidationErrors = true;
+    }
+
+    if (isTrimmedInputEmpty(email) || !isValidEmail(email)) {
+      setIsEmailError(true);
+      hasValidationErrors = true;
+    }
+
+    if (isTrimmedInputEmpty(bodyText)) {
+      setIsBodyError(true);
+      hasValidationErrors = true;
+    }
+
+    if (hasValidationErrors) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    addComment(name, email, bodyText)
+      .then(() => {
+        setBodyText('');
+        clearErrors();
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  const handleClearForm = () => {
+    clearInputs();
+    clearErrors();
+  };
+
   return (
-    <form data-cy="NewCommentForm">
+    <form data-cy="NewCommentForm" onSubmit={handleSubmitComment}>
       <div className="field" data-cy="NameField">
         <label className="label" htmlFor="comment-author-name">
           Author Name
@@ -11,27 +89,33 @@ export const NewCommentForm: React.FC = () => {
         <div className="control has-icons-left has-icons-right">
           <input
             type="text"
+            value={name}
             name="name"
             id="comment-author-name"
             placeholder="Name Surname"
-            className="input is-danger"
+            className={classNames('input', { 'is-danger': isNameError })}
+            onChange={event => setName(event.target.value)}
           />
 
           <span className="icon is-small is-left">
             <i className="fas fa-user" />
           </span>
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+          {isNameError && (
+            <span
+              className="icon is-small is-right has-text-danger"
+              data-cy="ErrorIcon"
+            >
+              <i className="fas fa-exclamation-triangle" />
+            </span>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Name is required
-        </p>
+        {isNameError && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Name is required
+          </p>
+        )}
       </div>
 
       <div className="field" data-cy="EmailField">
@@ -43,26 +127,32 @@ export const NewCommentForm: React.FC = () => {
           <input
             type="text"
             name="email"
+            value={email}
             id="comment-author-email"
             placeholder="email@test.com"
-            className="input is-danger"
+            className={classNames('input', { 'is-danger': isEmailError })}
+            onChange={event => setEmail(event.target.value)}
           />
 
           <span className="icon is-small is-left">
             <i className="fas fa-envelope" />
           </span>
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+          {isEmailError && (
+            <span
+              className="icon is-small is-right has-text-danger"
+              data-cy="ErrorIcon"
+            >
+              <i className="fas fa-exclamation-triangle" />
+            </span>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Email is required
-        </p>
+        {isEmailError && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Email is required
+          </p>
+        )}
       </div>
 
       <div className="field" data-cy="BodyField">
@@ -73,27 +163,40 @@ export const NewCommentForm: React.FC = () => {
         <div className="control">
           <textarea
             id="comment-body"
+            value={bodyText}
             name="body"
             placeholder="Type comment here"
-            className="textarea is-danger"
+            className={classNames('input', { 'is-danger': isBodyError })}
+            onChange={event => setBodyText(event.target.value)}
           />
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Enter some text
-        </p>
+        {isBodyError && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Enter some text
+          </p>
+        )}
       </div>
 
       <div className="field is-grouped">
         <div className="control">
-          <button type="submit" className="button is-link is-loading">
+          <button
+            type="submit"
+            className={classNames('button is-link', {
+              'is-loading': isLoading,
+            })}
+          >
             Add
           </button>
         </div>
 
         <div className="control">
           {/* eslint-disable-next-line react/button-has-type */}
-          <button type="reset" className="button is-link is-light">
+          <button
+            type="reset"
+            className="button is-link is-light"
+            onClick={handleClearForm}
+          >
             Clear
           </button>
         </div>
--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -1,106 +1,178 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/indent */
+import React, { useEffect, useState } from 'react';
 import { Loader } from './Loader';
 import { NewCommentForm } from './NewCommentForm';
+import { Post } from '../types/Post';
+import { Comment } from '../types/Comment';
+import * as commentMethods from '../api/comment';
 
-export const PostDetails: React.FC = () => {
+type Props = {
+  post?: Post;
+};
+
+export const PostDetails: React.FC<Props> = ({ post }) => {
+  const [isPostCommentsLoading, setisPostCommentsLoading] = useState(false);
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isWriteCommentButtonVisible, setIsWriteCommentButtonVisible] =
+    useState(true);
+  const [isNewCommentFormVisible, setIsNewCommentFormVisible] = useState(false);
+  const hasNoError = errorMessage === '';
+
+  useEffect(() => {
+    if (!post) {
+      return;
+    }
+
+    setisPostCommentsLoading(true);
+
+    commentMethods
+      .getComments(post.id)
+      .then(setComments)
+      .catch(error => {
+        setErrorMessage('Something went wrong!');
+        throw error;
+      })
+      .finally(() => {
+        setisPostCommentsLoading(false);
+      });
+  }, [post]);
+
+  const handleWriteCommentButtonClick = () => {
+    setIsWriteCommentButtonVisible(false);
+    setIsNewCommentFormVisible(true);
+  };
+
+  useEffect(() => {
+    if (post) {
+      setIsNewCommentFormVisible(false);
+      setIsWriteCommentButtonVisible(true);
+    }
+  }, [post]);
+
+  const addComment = async (
+    name: string,
+    email: string,
+    body: string,
+  ): Promise<void> => {
+    if (!post) {
+      return;
+    }
+
+    const postId = post.id;
+
+    try {
+      const newComment = await commentMethods.createComent({
+        postId,
+        name,
+        email,
+        body,
+      });
+
+      setComments(currentComments => [...currentComments, newComment]);
+    } catch (error) {
+      setErrorMessage('Something went wrong!');
+      throw error;
+    }
+  };
+
+  const deleteComment = async (commentId: number): Promise<void> => {
+    try {
+      await commentMethods.deleteComment(commentId);
+
+      setComments(currentComments =>
+        currentComments.filter(comment => comment.id !== commentId),
+      );
+    } catch (error) {
+      setErrorMessage('Something went wrong!');
+      throw error;
+    }
+  };
+
+  if (!post) {
+    return null;
+  }
+
   return (
     <div className="content" data-cy="PostDetails">
       <div className="content" data-cy="PostDetails">
         <div className="block">
-          <h2 data-cy="PostTitle">
-            #18: voluptate et itaque vero tempora molestiae
-          </h2>
+          <h2 data-cy="PostTitle">{`#${post?.id}: ${post?.title}`}</h2>
 
-          <p data-cy="PostBody">
-            eveniet quo quis laborum totam consequatur non dolor ut et est
-            repudiandae est voluptatem vel debitis et magnam
-          </p>
+          <p data-cy="PostBody">{post?.body}</p>
         </div>
 
         <div className="block">
-          <Loader />
+          {post && (
+            <>
+              {isPostCommentsLoading ? (
+                <Loader />
+              ) : comments.length === 0 && hasNoError ? (
+                <p className="title is-4" data-cy="NoCommentsMessage">
+                  No comments yet
+                </p>
+              ) : (
+                <>
+                  <p className="title is-4">Comments:</p>
+                  {comments.map(comment => {
+                    return (
+                      <article
+                        className="message is-small"
+                        data-cy="Comment"
+                        key={comment.id}
+                      >
+                        <div className="message-header">
+                          <a
+                            href={`mailto:${comment.email}`}
+                            data-cy="CommentAuthor"
+                          >
+                            {comment.name}
+                          </a>
+                          <button
+                            data-cy="CommentDelete"
+                            type="button"
+                            className="delete is-small"
+                            aria-label="delete"
+                            onClick={() => deleteComment(comment.id)}
+                          >
+                            delete button
+                          </button>
+                        </div>
 
-          <div className="notification is-danger" data-cy="CommentsError">
-            Something went wrong
-          </div>
+                        <div className="message-body" data-cy="CommentBody">
+                          {comment.body}
+                        </div>
+                      </article>
+                    );
+                  })}
+                </>
+              )}
+            </>
+          )}
 
-          <p className="title is-4" data-cy="NoCommentsMessage">
-            No comments yet
-          </p>
+          {!hasNoError && (
+            <div className="notification is-danger" data-cy="CommentsError">
+              Something went wrong
+            </div>
+          )}
 
-          <p className="title is-4">Comments:</p>
-
-          <article className="message is-small" data-cy="Comment">
-            <div className="message-header">
-              <a href="mailto:misha@mate.academy" data-cy="CommentAuthor">
-                Misha Hrynko
-              </a>
+          {!isPostCommentsLoading &&
+            isWriteCommentButtonVisible &&
+            hasNoError && (
               <button
-                data-cy="CommentDelete"
+                data-cy="WriteCommentButton"
                 type="button"
-                className="delete is-small"
-                aria-label="delete"
+                className="button is-link"
+                onClick={handleWriteCommentButtonClick}
               >
-                delete button
+                Write a comment
               </button>
-            </div>
-
-            <div className="message-body" data-cy="CommentBody">
-              Some comment
-            </div>
-          </article>
-
-          <article className="message is-small" data-cy="Comment">
-            <div className="message-header">
-              <a href="mailto:misha@mate.academy" data-cy="CommentAuthor">
-                Misha Hrynko
-              </a>
-
-              <button
-                data-cy="CommentDelete"
-                type="button"
-                className="delete is-small"
-                aria-label="delete"
-              >
-                delete button
-              </button>
-            </div>
-            <div className="message-body" data-cy="CommentBody">
-              One more comment
-            </div>
-          </article>
-
-          <article className="message is-small" data-cy="Comment">
-            <div className="message-header">
-              <a href="mailto:misha@mate.academy" data-cy="CommentAuthor">
-                Misha Hrynko
-              </a>
-
-              <button
-                data-cy="CommentDelete"
-                type="button"
-                className="delete is-small"
-                aria-label="delete"
-              >
-                delete button
-              </button>
-            </div>
-
-            <div className="message-body" data-cy="CommentBody">
-              {'Multi\nline\ncomment'}
-            </div>
-          </article>
-
-          <button
-            data-cy="WriteCommentButton"
-            type="button"
-            className="button is-link"
-          >
-            Write a comment
-          </button>
+            )}
         </div>
 
-        <NewCommentForm />
+        {isNewCommentFormVisible && hasNoError && (
+          <NewCommentForm addComment={addComment} />
+        )}
       </div>
     </div>
   );
