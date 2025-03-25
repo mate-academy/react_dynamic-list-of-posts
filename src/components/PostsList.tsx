922cac089ee18f169f8e4db64c91dff6a930252b
Heny <jackbalageru@yahoo.com>
2025-03-25 13:56:41 +0300

    Solution

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,86 +1,58 @@
 import React from 'react';
+import { Post } from '../types/Post';
 
-export const PostsList: React.FC = () => (
-  <div data-cy="PostsList">
-    <p className="title">Posts:</p>
+interface PostListProps {
+  posts: Post[];
+  onPostSelect: (post: Post | null) => void;
+  selectedPost: Post | null;
+}
 
-    <table className="table is-fullwidth is-striped is-hoverable is-narrow">
-      <thead>
-        <tr className="has-background-link-light">
-          <th>#</th>
-          <th>Title</th>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <th> </th>
-        </tr>
-      </thead>
+export const PostsList: React.FC<PostListProps> = ({
+  posts,
+  onPostSelect,
+  selectedPost,
+}) => {
+  return (
+    <div data-cy="PostsList">
+      <p className="title">Posts:</p>
 
-      <tbody>
-        <tr data-cy="Post">
-          <td data-cy="PostId">17</td>
+      <table className="table is-fullwidth is-striped is-hoverable is-narrow">
+        <thead>
+          <tr className="has-background-link-light">
+            <th>#</th>
+            <th>Title</th>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <th> </th>
+          </tr>
+        </thead>
 
-          <td data-cy="PostTitle">
-            fugit voluptas sed molestias voluptatem provident
-          </td>
+        <tbody>
+          {posts?.map(p => {
+            const isSelected = selectedPost?.id === p.id;
 
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
+            return (
+              <tr key={p.id} data-cy="Post">
+                <td data-cy="PostId">{p.id}</td>
 
-        <tr data-cy="Post">
-          <td data-cy="PostId">18</td>
+                <td data-cy="PostTitle">{p.title}</td>
 
-          <td data-cy="PostTitle">
-            voluptate et itaque vero tempora molestiae
-          </td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link"
-            >
-              Close
-            </button>
-          </td>
-        </tr>
-
-        <tr data-cy="Post">
-          <td data-cy="PostId">19</td>
-          <td data-cy="PostTitle">adipisci placeat illum aut reiciendis qui</td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
-
-        <tr data-cy="Post">
-          <td data-cy="PostId">20</td>
-          <td data-cy="PostTitle">doloribus ad provident suscipit at</td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+                <td className="has-text-right is-vcentered">
+                  <button
+                    type="button"
+                    data-cy="PostButton"
+                    className={`button is-link ${isSelected ? '' : 'is-light'}`}
+                    onClick={() => onPostSelect(isSelected ? null : p)}
+                    // className="button is-link is-light"
+                    // onClick={() => onPostSelect(p)}
+                  >
+                    {isSelected ? 'Close' : 'Open'}
+                  </button>
+                </td>
+              </tr>
+            );
+          }) ?? null}
+        </tbody>
+      </table>
+    </div>
+  );
+};
