0be3ce185c5cb17ea022ff498b6476d3b064af98
NataliaDavida <nataliadavida88@gmail.com>
2025-07-11 12:44:05 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -7,54 +7,108 @@ import './App.scss';
 import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
-import { Loader } from './components/Loader';
+import { useEffect, useState } from 'react';
+import { User } from './types/User';
+import * as service from './services/servises';
+import { Post } from './types/Post';
 
-export const App = () => (
-  <main className="section">
-    <div className="container">
-      <div className="tile is-ancestor">
-        <div className="tile is-parent">
-          <div className="tile is-child box is-success">
-            <div className="block">
-              <UserSelector />
-            </div>
+export const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
 
-            <div className="block" data-cy="MainContent">
-              <p data-cy="NoSelectedUser">No user selected</p>
+  const [, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
 
-              <Loader />
+  const handleError = (message: string) => {
+    setErrorMsg(message);
 
-              <div
-                className="notification is-danger"
-                data-cy="PostsLoadingError"
-              >
-                Something went wrong!
+    setTimeout(() => {
+      setErrorMsg('');
+    }, 3000);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+
+    service
+      .getUsers()
+      .then(setUsers)
+      .catch(error => {
+        handleError('Something went wrong!');
+        throw error;
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <main className="section">
+      <div className="container">
+        <div className="tile is-ancestor">
+          <div className="tile is-parent">
+            <div className="tile is-child box is-success">
+              <div className="block">
+                <UserSelector
+                  users={users}
+                  onUserSelect={user => {
+                    setSelectedUser(user);
+                    setSelectedPost(null);
+                  }}
+                  selectedUser={selectedUser}
+                />
               </div>
+              <div className="block" data-cy="MainContent">
+                {!selectedUser ? (
+                  <p data-cy="NoSelectedUser">No user selected</p>
+                ) : (
+                  <>
+                    {errorMsg && (
+                      <div
+                        className="notification is-danger"
+                        data-cy="PostsLoadingError"
+                      >
+                        {errorMsg}
+                      </div>
+                    )}
 
-              <div className="notification is-warning" data-cy="NoPostsYet">
-                No posts yet
+                    {selectedUser && (
+                      <PostsList
+                        user={selectedUser.id}
+                        setLoading={setLoading}
+                        errorMsg={errorMsg}
+                        setErrorMsg={setErrorMsg}
+                        selectedPost={selectedPost}
+                        setSelectedPost={setSelectedPost}
+                      />
+                    )}
+                  </>
+                )}
               </div>
-
-              <PostsList />
             </div>
           </div>
-        </div>
 
-        <div
-          data-cy="Sidebar"
-          className={classNames(
-            'tile',
-            'is-parent',
-            'is-8-desktop',
-            'Sidebar',
-            'Sidebar--open',
-          )}
-        >
-          <div className="tile is-child box is-success ">
-            <PostDetails />
+          <div
+            data-cy="Sidebar"
+            className={classNames(
+              'tile',
+              'is-parent',
+              'is-8-desktop',
+              'Sidebar',
+              { 'Sidebar--open': selectedPost },
+            )}
+          >
+            {selectedPost && (
+              <div className="tile is-child box is-success ">
+                <PostDetails
+                  post={selectedPost}
+                  setLoading={setLoading}
+                  setErrorMsg={setErrorMsg}
+                />
+              </div>
+            )}
           </div>
         </div>
       </div>
-    </div>
-  </main>
-);
+    </main>
+  );
+};
--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,86 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Post } from '../types/Post';
+import * as service from '../services/servises';
+import { Loader } from './Loader';
+import classNames from 'classnames';
 
-export const PostsList: React.FC = () => (
-  <div data-cy="PostsList">
-    <p className="title">Posts:</p>
+type Props = {
+  user: number;
+  setLoading: (value: boolean) => void;
+  errorMsg: string;
+  setErrorMsg: (message: string) => void;
+  selectedPost: Post | null;
+  setSelectedPost: (post: Post | null) => void;
+};
 
-    <table className="table is-fullwidth is-striped is-hoverable is-narrow">
-      <thead>
-        <tr className="has-background-link-light">
-          <th>#</th>
-          <th>Title</th>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <th> </th>
-        </tr>
-      </thead>
+export const PostsList: React.FC<Props> = ({
+  user,
+  setLoading,
+  errorMsg,
+  setErrorMsg,
+  selectedPost,
+  setSelectedPost,
+}) => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [localLoading, setLocalLoading] = useState(true);
 
-      <tbody>
-        <tr data-cy="Post">
-          <td data-cy="PostId">17</td>
+  useEffect(() => {
+    setLoading(true);
+    setLocalLoading(true);
 
-          <td data-cy="PostTitle">
-            fugit voluptas sed molestias voluptatem provident
-          </td>
+    service
+      .getUserPosts(user)
+      .then(setPosts)
+      .catch(() => setErrorMsg('Something went wrong!'))
+      .finally(() => {
+        setLoading(false);
+        setLocalLoading(false);
+      });
+  }, [user]);
 
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
+  if (localLoading) {
+    return <Loader />;
+  }
 
-        <tr data-cy="Post">
-          <td data-cy="PostId">18</td>
+  if (user && !localLoading && posts.length === 0 && !errorMsg) {
+    return (
+      <div className="notification is-warning" data-cy="NoPostsYet">
+        No posts yet
+      </div>
+    );
+  }
 
-          <td data-cy="PostTitle">
-            voluptate et itaque vero tempora molestiae
-          </td>
+  return (
+    <div data-cy="PostsList">
+      <p className="title">Posts:</p>
+      <table className="table is-fullwidth is-striped is-hoverable is-narrow">
+        <thead>
+          <tr className="has-background-link-light">
+            <th>#</th>
+            <th>Title</th>
+            <th> </th>
+          </tr>
+        </thead>
 
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link"
-            >
-              Close
-            </button>
-          </td>
-        </tr>
-
-        <tr data-cy="Post">
-          <td data-cy="PostId">19</td>
-          <td data-cy="PostTitle">adipisci placeat illum aut reiciendis qui</td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
-
-        <tr data-cy="Post">
-          <td data-cy="PostId">20</td>
-          <td data-cy="PostTitle">doloribus ad provident suscipit at</td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+        <tbody>
+          {posts.map(post => (
+            <tr key={post.id} data-cy="Post">
+              <td data-cy="PostId">{post.id}</td>
+              <td data-cy="PostTitle">{post.title}</td>
+              <td className="has-text-right is-vcentered">
+                <button
+                  type="button"
+                  data-cy="PostButton"
+                  className={classNames('button is-link', {
+                    'is-light': selectedPost?.id !== post.id,
+                  })}
+                  onClick={() => {
+                    if (selectedPost?.id === post.id) {
+                      setSelectedPost(null);
+                    } else {
+                      setSelectedPost(post);
+                    }
+                  }}
+                >
+                  {selectedPost?.id === post.id ? 'Close' : 'Open'}
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
