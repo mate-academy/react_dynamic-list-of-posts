7e820856b83f719a6c04bab7f7e738953e0fad30
Yuliia Fito <jylia.fito2004@gmail.com>
2025-08-11 14:04:09 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -8,53 +8,131 @@ import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
 import { Loader } from './components/Loader';
+import { useEffect, useState } from 'react';
+import { User } from './types/User';
+import { getUserPosts, getUsers } from './api/api';
+import { Post } from './types/Post';
 
-export const App = () => (
-  <main className="section">
-    <div className="container">
-      <div className="tile is-ancestor">
-        <div className="tile is-parent">
-          <div className="tile is-child box is-success">
-            <div className="block">
-              <UserSelector />
-            </div>
+export const App = () => {
+  const [users, setUsers] = useState<User[] | null>(null);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [userPosts, setUserPosts] = useState<Post[] | null>(null);
+  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
+  const [isErrorShown, setIsErrorShown] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
-            <div className="block" data-cy="MainContent">
-              <p data-cy="NoSelectedUser">No user selected</p>
+  const loadUsers = async () => {
+    setIsErrorShown(false);
 
-              <Loader />
+    try {
+      const usersFromServer = await getUsers();
 
-              <div
-                className="notification is-danger"
-                data-cy="PostsLoadingError"
-              >
-                Something went wrong!
+      if (usersFromServer.length === 0) {
+        setIsErrorShown(true);
+      }
+
+      setUsers(usersFromServer);
+    } catch (err) {
+      setIsErrorShown(true);
+    }
+  };
+
+  const loadUserPosts = async (userId: number) => {
+    setIsErrorShown(false);
+    setIsLoading(true);
+
+    try {
+      const userPostsFromServer = await getUserPosts(userId);
+
+      setUserPosts(userPostsFromServer);
+    } catch (err) {
+      setIsErrorShown(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  useEffect(() => {
+    setSelectedPost(null);
+    setUserPosts(null);
+
+    if (selectedUser) {
+      loadUserPosts(selectedUser.id);
+    }
+  }, [selectedUser]);
+
+  return (
+    <main className="section">
+      <div className="container">
+        <div className="tile is-ancestor">
+          <div className="tile is-parent">
+            <div className="tile is-child box is-success">
+              <div className="block">
+                <UserSelector
+                  users={users}
+                  selectedUser={selectedUser}
+                  onUserSelect={setSelectedUser}
+                />
               </div>
 
-              <div className="notification is-warning" data-cy="NoPostsYet">
-                No posts yet
-              </div>
+              <div className="block" data-cy="MainContent">
+                {!selectedUser && (
+                  <p data-cy="NoSelectedUser">No user selected</p>
+                )}
 
-              <PostsList />
+                {isLoading && <Loader />}
+
+                {isErrorShown && (
+                  <div
+                    className="notification is-danger"
+                    data-cy="PostsLoadingError"
+                  >
+                    Something went wrong!
+                  </div>
+                )}
+
+                {userPosts?.length === 0 && !isErrorShown && !isLoading && (
+                  <div className="notification is-warning" data-cy="NoPostsYet">
+                    No posts yet
+                  </div>
+                )}
+
+                {userPosts && !isLoading && !isErrorShown && (
+                  <PostsList
+                    userPosts={userPosts}
+                    selectedPost={selectedPost}
+                    onPostSelect={setSelectedPost}
+                  />
+                )}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div
-          data-cy="Sidebar"
-          className={classNames(
-            'tile',
-            'is-parent',
-            'is-8-desktop',
-            'Sidebar',
-            'Sidebar--open',
-          )}
-        >
-          <div className="tile is-child box is-success ">
-            <PostDetails />
+          <div
+            data-cy="Sidebar"
+            className={classNames(
+              'tile',
+              'is-parent',
+              'is-8-desktop',
+              'Sidebar',
+              { 'Sidebar--open': !!selectedPost },
+            )}
+          >
+            <div className="tile is-child box is-success ">
+              <PostDetails
+                userPost={selectedPost}
+                isFormVisible={isFormVisible}
+                setIsFormVisible={setIsFormVisible}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </main>
-);
+    </main>
+  );
+};
