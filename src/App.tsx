91a6e45d02d0ad9a33213d9245d24b40dddcaf92
1umamaster <lukasholeg02@gmail.com>
2026-03-01 13:36:30 +0200

    The solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,57 +4,130 @@ import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
 
+import { getUsers } from './api/users';
+import { client } from './utils/fetchClient';
+
+import { useEffect, useState } from 'react';
+
+import { User } from './types/User';
+import { Post } from './types/Post';
+
 import { PostsList } from './components/PostsList';
 import { PostDetails } from './components/PostDetails';
 import { UserSelector } from './components/UserSelector';
 import { Loader } from './components/Loader';
 
-export const App = () => (
-  <main className="section">
-    <div className="container">
-      <div className="tile is-ancestor">
-        <div className="tile is-parent">
-          <div className="tile is-child box is-success">
-            <div className="block">
-              <UserSelector />
-            </div>
+enum PostState {
+  Loading = 'loading',
+  LoadingError = 'error',
+  LoadingSuccess = 'success',
+}
 
-            <div className="block" data-cy="MainContent">
-              <p data-cy="NoSelectedUser">No user selected</p>
+export const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [activeUser, setActiveUser] = useState<number | null>(null);
+  const [activePost, setActivePost] = useState<Post | null>(null);
+  const [postState, setPostState] = useState<PostState | null>(null);
 
-              <Loader />
+  useEffect(() => {
+    getUsers()
+      .then(loadedUsers => {
+        setUsers(loadedUsers);
+      })
+      .catch(() => {
+        // Handle error here
+      });
+  }, []);
 
-              <div
-                className="notification is-danger"
-                data-cy="PostsLoadingError"
-              >
-                Something went wrong!
+  useEffect(() => {
+    setActivePost(null);
+
+    if (activeUser === null || activeUser < 1) {
+      setPostState(null);
+      setPosts([]);
+
+      return;
+    }
+
+    setPostState(PostState.Loading);
+
+    client
+      .get<Post[]>(`/posts?userId=${activeUser}`)
+      .then(loadedPosts => {
+        setPosts(loadedPosts);
+        setPostState(PostState.LoadingSuccess);
+      })
+      .catch(() => {
+        setPostState(PostState.LoadingError);
+      });
+  }, [activeUser]);
+
+  return (
+    <main className="section">
+      <div className="container">
+        <div className="tile is-ancestor">
+          <div className="tile is-parent">
+            <div className="tile is-child box is-success">
+              <div className="block">
+                <UserSelector
+                  users={users}
+                  activeUser={activeUser}
+                  setActiveUser={setActiveUser}
+                />
               </div>
 
-              <div className="notification is-warning" data-cy="NoPostsYet">
-                No posts yet
-              </div>
+              <div className="block" data-cy="MainContent">
+                {activeUser === null && (
+                  <p data-cy="NoSelectedUser">No user selected</p>
+                )}
 
-              <PostsList />
+                {postState === PostState.LoadingSuccess &&
+                  (posts.length > 0 ? (
+                    <PostsList
+                      posts={posts}
+                      activePost={activePost}
+                      setActivePost={setActivePost}
+                    />
+                  ) : (
+                    <div
+                      className="notification is-warning"
+                      data-cy="NoPostsYet"
+                    >
+                      No posts yet
+                    </div>
+                  ))}
+
+                {postState === PostState.Loading && <Loader />}
+
+                {postState === PostState.LoadingError && (
+                  <div
+                    className="notification is-danger"
+                    data-cy="PostsLoadingError"
+                  >
+                    Something went wrong!
+                  </div>
+                )}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div
-          data-cy="Sidebar"
-          className={classNames(
-            'tile',
-            'is-parent',
-            'is-8-desktop',
-            'Sidebar',
-            'Sidebar--open',
-          )}
-        >
-          <div className="tile is-child box is-success ">
-            <PostDetails />
+          <div
+            data-cy="Sidebar"
+            className={classNames(
+              'tile',
+              'is-parent',
+              'is-8-desktop',
+              'Sidebar',
+              { 'Sidebar--open': activePost !== null },
+            )}
+          >
+            <div className="tile is-child box is-success ">
+              {activePost !== null && <PostDetails post={activePost} />}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </main>
-);
+    </main>
+  );
+};
--- src/components/NewCommentForm.tsx
+++ src/components/NewCommentForm.tsx
@@ -1,8 +1,64 @@
 import React from 'react';
+import { CommentData, Comment } from '../types/Comment';
+import { client } from '../utils/fetchClient';
+import classNames from 'classnames';
 
-export const NewCommentForm: React.FC = () => {
+interface Props {
+  postId: number;
+  setComments: React.Dispatch<React.SetStateAction<Comment[]>>;
+}
+
+export const NewCommentForm: React.FC<Props> = ({ postId, setComments }) => {
+  const [isAdding, setIsAdding] = React.useState(false);
+  const [newCommentData, setNewCommentData] = React.useState<CommentData>({
+    name: '',
+    email: '',
+    body: '',
+  });
+  const [errors, setErrors] = React.useState({
+    name: false,
+    email: false,
+    body: false,
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const newErrors = {
+      name: !newCommentData.name.trim(),
+      email: !newCommentData.email.trim(),
+      body: !newCommentData.body.trim(),
+    };
+
+    setErrors(newErrors);
+
+    if (newErrors.name || newErrors.email || newErrors.body) {
+      return;
+    }
+
+    setIsAdding(true);
+    client
+      .post<Comment>(`/comments`, { postId, ...newCommentData })
+      .then(newComment => {
+        setComments(prevComments => [...prevComments, newComment]);
+        setNewCommentData({ ...newCommentData, body: '' });
+      })
+      .catch(() => {
+        // Handle error here
+      })
+      .finally(() => {
+        setIsAdding(false);
+      });
+  };
+
+  const handleClear = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setNewCommentData({ name: '', email: '', body: '' });
+    setErrors({ name: false, email: false, body: false });
+  };
+
   return (
-    <form data-cy="NewCommentForm">
+    <form data-cy="NewCommentForm" onSubmit={handleSubmit}>
       <div className="field" data-cy="NameField">
         <label className="label" htmlFor="comment-author-name">
           Author Name
@@ -14,24 +70,32 @@ export const NewCommentForm: React.FC = () => {
             name="name"
             id="comment-author-name"
             placeholder="Name Surname"
-            className="input is-danger"
+            className={classNames('input', { 'is-danger': errors.name })}
+            value={newCommentData.name}
+            onChange={e =>
+              setNewCommentData({ ...newCommentData, name: e.target.value })
+            }
           />
 
           <span className="icon is-small is-left">
             <i className="fas fa-user" />
           </span>
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+          {errors.name && (
+            <span
+              className="icon is-small is-right has-text-danger"
+              data-cy="ErrorIcon"
+            >
+              <i className="fas fa-exclamation-triangle" />
+            </span>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Name is required
-        </p>
+        {errors.name && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Name is required
+          </p>
+        )}
       </div>
 
       <div className="field" data-cy="EmailField">
@@ -45,24 +109,32 @@ export const NewCommentForm: React.FC = () => {
             name="email"
             id="comment-author-email"
             placeholder="email@test.com"
-            className="input is-danger"
+            className={classNames('input', { 'is-danger': errors.email })}
+            value={newCommentData.email}
+            onChange={e =>
+              setNewCommentData({ ...newCommentData, email: e.target.value })
+            }
           />
 
           <span className="icon is-small is-left">
             <i className="fas fa-envelope" />
           </span>
 
-          <span
-            className="icon is-small is-right has-text-danger"
-            data-cy="ErrorIcon"
-          >
-            <i className="fas fa-exclamation-triangle" />
-          </span>
+          {errors.email && (
+            <span
+              className="icon is-small is-right has-text-danger"
+              data-cy="ErrorIcon"
+            >
+              <i className="fas fa-exclamation-triangle" />
+            </span>
+          )}
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Email is required
-        </p>
+        {errors.email && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Email is required
+          </p>
+        )}
       </div>
 
       <div className="field" data-cy="BodyField">
@@ -75,25 +147,38 @@ export const NewCommentForm: React.FC = () => {
             id="comment-body"
             name="body"
             placeholder="Type comment here"
-            className="textarea is-danger"
+            className={classNames('textarea', { 'is-danger': errors.body })}
+            value={newCommentData.body}
+            onChange={e =>
+              setNewCommentData({ ...newCommentData, body: e.target.value })
+            }
           />
         </div>
 
-        <p className="help is-danger" data-cy="ErrorMessage">
-          Enter some text
-        </p>
+        {errors.body && (
+          <p className="help is-danger" data-cy="ErrorMessage">
+            Enter some text
+          </p>
+        )}
       </div>
 
       <div className="field is-grouped">
         <div className="control">
-          <button type="submit" className="button is-link is-loading">
+          <button
+            type="submit"
+            className={classNames('button is-link', { 'is-loading': isAdding })}
+          >
             Add
           </button>
         </div>
 
         <div className="control">
           {/* eslint-disable-next-line react/button-has-type */}
-          <button type="reset" className="button is-link is-light">
+          <button
+            type="reset"
+            className="button is-link is-light"
+            onClick={handleClear}
+          >
             Clear
           </button>
         </div>
