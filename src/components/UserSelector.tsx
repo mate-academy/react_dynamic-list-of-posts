62ed547914fd1fbf25d3b8c81d31fdddd010fb39
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2026-03-04 14:38:22 +0200

    done Dynamic list of posts task

--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { User } from '../types/User';
+import classNames from 'classnames';
 
-export const UserSelector: React.FC = () => {
+interface Props {
+  users: User[];
+  activeUser: number | null;
+  setActiveUser: (userId: number) => void;
+}
+
+export const UserSelector: React.FC<Props> = ({
+  users,
+  activeUser,
+  setActiveUser,
+}) => {
+  const [focused, setFocused] = useState(false);
+
+  const selectedUserName =
+    users.find(u => u.id === activeUser)?.name || 'Choose a user';
+
   return (
-    <div data-cy="UserSelector" className="dropdown is-active">
+    <div
+      data-cy="UserSelector"
+      className={classNames('dropdown', { 'is-active': focused })}
+      onBlur={() => setFocused(false)}
+    >
       <div className="dropdown-trigger">
         <button
           type="button"
           className="button"
           aria-haspopup="true"
           aria-controls="dropdown-menu"
+          onClick={() => setFocused(!focused)}
         >
-          <span>Choose a user</span>
+          <span>{selectedUserName}</span>
 
           <span className="icon is-small">
             <i className="fas fa-angle-down" aria-hidden="true" />
@@ -20,21 +42,21 @@ export const UserSelector: React.FC = () => {
 
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
         <div className="dropdown-content">
-          <a href="#user-1" className="dropdown-item">
-            Leanne Graham
-          </a>
-          <a href="#user-2" className="dropdown-item is-active">
-            Ervin Howell
-          </a>
-          <a href="#user-3" className="dropdown-item">
-            Clementine Bauch
-          </a>
-          <a href="#user-4" className="dropdown-item">
-            Patricia Lebsack
-          </a>
-          <a href="#user-5" className="dropdown-item">
-            Chelsey Dietrich
-          </a>
+          {users.map(user => (
+            <a
+              key={user.id}
+              className={classNames('dropdown-item', {
+                'is-active': activeUser === user.id,
+              })}
+              onMouseDown={e => {
+                e.preventDefault();
+                setActiveUser(user.id);
+                setFocused(false);
+              }}
+            >
+              {user.name}
+            </a>
+          ))}
         </div>
       </div>
     </div>
