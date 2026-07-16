565d6739a9a8b97b7e00378a5122354a78f7b1ab
Maksym <maxim.golya@gmail.com>
2026-07-16 18:32:43 +0300

    peremoga

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,86 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Loader } from './Loader';
+import { client } from '../utils/fetchClient';
+import { Post } from '../types/Post';
 
-export const PostsList: React.FC = () => (
-  <div data-cy="PostsList">
-    <p className="title">Posts:</p>
+interface Props {
+  selectedUserId: number;
+  selectedPost: Post | null;
+  onPostSelect: (post: Post | null) => void;
+}
 
-    <table className="table is-fullwidth is-striped is-hoverable is-narrow">
-      <thead>
-        <tr className="has-background-link-light">
-          <th>#</th>
-          <th>Title</th>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <th> </th>
-        </tr>
-      </thead>
+export const PostsList: React.FC<Props> = ({
+  selectedUserId,
+  selectedPost,
+  onPostSelect,
+}) => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
-      <tbody>
-        <tr data-cy="Post">
-          <td data-cy="PostId">17</td>
+  useEffect(() => {
+    setIsLoading(true);
+    setHasError(false);
+    onPostSelect(null);
+    client
+      .get<Post[]>(`/posts?userId=${selectedUserId}`)
+      .then(postsFromServer => {
+        setPosts(postsFromServer);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setHasError(true);
+        setIsLoading(false);
+      });
+  }, [onPostSelect, selectedUserId]);
 
-          <td data-cy="PostTitle">
-            fugit voluptas sed molestias voluptatem provident
-          </td>
+  return (
+    <>
+      {isLoading && <Loader />}
+      {!isLoading && hasError && (
+        <div className="notification is-danger" data-cy="PostsLoadingError">
+          Something went wrong!
+        </div>
+      )}
+      {!isLoading && !hasError && posts.length === 0 && (
+        <div className="notification is-warning" data-cy="NoPostsYet">
+          No posts yet
+        </div>
+      )}
+      {!isLoading && !hasError && posts.length > 0 && (
+        <div data-cy="PostsList">
+          <p className="title">Posts:</p>
+          {/* eslint-disable-next-line max-len */}
+          <table className="table is-fullwidth is-striped is-hoverable is-narrow">
+            <thead>
+              <tr className="has-background-link-light">
+                <th>#</th>
+                <th>Title</th>
+                {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+                <th> </th>
+              </tr>
+            </thead>
 
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
+            <tbody>
+              {posts.map(post => (
+                <tr data-cy="Post" key={post.id}>
+                  <td data-cy="PostId">{post.id}</td>
+                  <td data-cy="PostTitle">{post.title}</td>
 
-        <tr data-cy="Post">
-          <td data-cy="PostId">18</td>
-
-          <td data-cy="PostTitle">
-            voluptate et itaque vero tempora molestiae
-          </td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link"
-            >
-              Close
-            </button>
-          </td>
-        </tr>
-
-        <tr data-cy="Post">
-          <td data-cy="PostId">19</td>
-          <td data-cy="PostTitle">adipisci placeat illum aut reiciendis qui</td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
-
-        <tr data-cy="Post">
-          <td data-cy="PostId">20</td>
-          <td data-cy="PostTitle">doloribus ad provident suscipit at</td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+                  <td className="has-text-right is-vcentered">
+                    {selectedPost?.id === post.id ? (
+                      <button
+                        type="button"
+                        data-cy="PostButton"
+                        className="button is-link"
+                        onClick={() => onPostSelect(null)}
+                      >
+                        Close
+                      </button>
+                    ) : (
+                      <button
+                        type="button"
+                        data-cy="PostButton"
+                        className="button is-link is-light"
+                        onClick={() => onPostSelect(post)}
+                      >
+                        Open
+                      </button>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
+  );
+};
--- src/components/UserSelector.tsx
+++ src/components/UserSelector.tsx
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { User } from '../types/User';
+import classNames from 'classnames';
 
-export const UserSelector: React.FC = () => {
+interface Props {
+  users: User[];
+  selectedUserId: number;
+  setSelectedUserId(userId: number): void;
+}
+
+export const UserSelector: React.FC<Props> = ({
+  users,
+  selectedUserId,
+  setSelectedUserId,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const selectedUser = users.find(user => user.id === selectedUserId);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
-    <div data-cy="UserSelector" className="dropdown is-active">
+    <div
+      data-cy="UserSelector"
+      className={classNames('dropdown', { 'is-active': isOpen })}
+      ref={dropdownRef}
+    >
       <div className="dropdown-trigger">
         <button
           type="button"
           className="button"
           aria-haspopup="true"
           aria-controls="dropdown-menu"
+          onClick={() => setIsOpen(!isOpen)}
         >
-          <span>Choose a user</span>
+          <span>{selectedUser ? selectedUser.name : 'Choose a user'}</span>
 
           <span className="icon is-small">
             <i className="fas fa-angle-down" aria-hidden="true" />
@@ -20,21 +60,20 @@ export const UserSelector: React.FC = () => {
 
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
         <div className="dropdown-content">
-          <a href="#user-1" className="dropdown-item">
-            Leanne Graham
-          </a>
-          <a href="#user-2" className="dropdown-item is-active">
-            Ervin Howell
-          </a>
-          <a href="#user-3" className="dropdown-item">
-            Clementine Bauch
-          </a>
-          <a href="#user-4" className="dropdown-item">
-            Patricia Lebsack
-          </a>
-          <a href="#user-5" className="dropdown-item">
-            Chelsey Dietrich
-          </a>
+          {users.map(user => (
+            <a
+              className={classNames('dropdown-item', {
+                'is-active': user.id === selectedUserId,
+              })}
+              key={user.id}
+              onClick={() => {
+                setSelectedUserId(user.id);
+                setIsOpen(false);
+              }}
+            >
+              {user.name}
+            </a>
+          ))}
         </div>
       </div>
     </div>
